2d1a967865d60f732ca8ff92b51e7f59a7fa90ef
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-11 16:08:46 -0700

    feat: migrate constants

---
+++ src/simulation/utils/constants.ts
@@ -0,0 +1,10 @@
+const GRAV_CONST = 6.674e-11; // Gravitational Constant m^3/kg/s^2;
+const AU = 1.495978707e11; // Astronomical Unit in m
+const SOLAR_MASS = 1.989e30; // Solar Mass in kg
+const DAY = 86400; // Day in seconds
+const YEAR = 360.25; // Year in Days
+const DIST_MULT = 1e10; // Distance multiplier (10 unit = 1x10^11m = 100,000,000km)
+const KM_TO_M = 1000; // User enters value in km and this converts it to m
+const MASS_MULT = 1e24;
+
+export { GRAV_CONST, AU, SOLAR_MASS, DAY, YEAR, DIST_MULT, KM_TO_M, MASS_MULT };
