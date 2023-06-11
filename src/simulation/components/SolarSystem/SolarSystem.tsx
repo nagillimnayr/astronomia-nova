e0503fe7ef267b4b9fe970372b01b1a9394bb6c8
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-11 16:35:29 -0700

    fix: fix import statement

--- src/simulation/components/SolarSystem/SolarSystem.tsx
+++ src/simulation/components/SolarSystem/SolarSystem.tsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useRef, useState } from "react";
 import { useLoader, useFrame } from "@react-three/fiber";
-import { type DynamicBody } from "../../Classes/Dynamics";
+import { type DynamicBody } from "../../classes/Dynamics";
 import Body from "../Body/Body";
 import loadBodyPreset from "../../utils/loadBodyPreset";
-import type KeplerBody from "../../Classes/KeplerBody";
-import { traverseTree } from "../../Classes/KeplerBody";
+import type KeplerBody from "../../classes/KeplerBody";
+import { traverseTree } from "../../classes/KeplerBody";
 import KeplerTreeContext from "../../context/KeplerTreeContext";
 import { useEventListener } from "@react-hooks-library/core";
 import { useTexture } from "@react-three/drei";
