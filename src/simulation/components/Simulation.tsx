dc087a52cc99a8907bf5dcbdd16d773646e5c61d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-11 16:20:24 -0700

    feat: migrate Simulation component

---
+++ src/simulation/components/Simulation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SolarSystem from './SolarSystem/SolarSystem';
+
+const Simulation = () => {
+  return (
+    <group>
+      <polarGridHelper />
+      <SolarSystem />
+    </group>
+  );
+};
