6a0283e194ff242bc633539fc6d187a87038cb22
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-12 15:30:28 -0700

    feat: add PauseButton component

---
+++ src/simulation/components/Time/PauseButton.tsx
@@ -0,0 +1,10 @@
+import { PlayIcon } from '@heroicons/react/24/solid';
+const PauseButton = () => {
+  return (
+    <button className="rounded-full border-2 border-white">
+      <PlayIcon />
+    </button>
+  );
+};
+
+export default PauseButton;
