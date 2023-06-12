c592aa64c0c8f6cf450f9ee6ad4c20b751e0309f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-11 20:52:31 -0700

    build: organize drei imports

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,13 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import SpinCube from "./SpinCube";
 import TrailSphere from "./TrailSphere";
-import { OrbitControls } from "~/drei-imports/OrbitControls";
+import { OrbitControls } from "~/drei-imports/controls/OrbitControls";
 import Simulation from "~/simulation/components/Simulation";
 
 const Scene = () => {
   return (
     <Canvas>
-      <perspectiveCamera position={[0, 0, 5]}>
+      <perspectiveCamera position={[0, 0, 5]} fov={50}>
         <spotLight />
       </perspectiveCamera>
       <ambientLight intensity={0.1} />
