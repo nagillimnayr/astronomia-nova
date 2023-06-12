3a4b72646fb5a8ab64796f9db73848e3d1d529ab
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-12 00:16:07 -0700

    feat: add EARTH_RADIUS

--- src/simulation/utils/constants.ts
+++ src/simulation/utils/constants.ts
@@ -6,5 +6,16 @@ const YEAR = 360.25; // Year in Days
 const DIST_MULT = 1e10; // Distance multiplier (10 unit = 1x10^11m = 100,000,000km)
 const KM_TO_M = 1000; // User enters value in km and this converts it to m
 const MASS_MULT = 1e24;
+const EARTH_RADIUS = 6371.0084e3;
 
-export { GRAV_CONST, AU, SOLAR_MASS, DAY, YEAR, DIST_MULT, KM_TO_M, MASS_MULT };
+export {
+  GRAV_CONST,
+  AU,
+  SOLAR_MASS,
+  DAY,
+  YEAR,
+  DIST_MULT,
+  KM_TO_M,
+  MASS_MULT,
+  EARTH_RADIUS,
+};
