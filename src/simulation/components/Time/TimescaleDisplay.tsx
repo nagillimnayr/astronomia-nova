0dcac013001ae837b2bffa7a09c2b8bffcb1411f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-13 16:40:04 -0700

    feat: set timescale text to white and center

--- src/simulation/components/Time/TimescaleDisplay.tsx
+++ src/simulation/components/Time/TimescaleDisplay.tsx
@@ -1,6 +1,6 @@
 const TimescaleDisplay = () => {
   return (
-    <span className="w-min-fit flex flex-row whitespace-nowrap">
+    <span className="w-min-fit flex flex-row justify-center whitespace-nowrap text-white">
       <span></span> Days / second
     </span>
   );
