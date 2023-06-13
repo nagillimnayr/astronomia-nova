ec60e3d64b4e635f333d1221318cdf51896dd788
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-12 19:48:34 -0700

    feat: center icons in buttons

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -4,7 +4,7 @@ type IconBtnProps = {
 
 const IconButton = (props: IconBtnProps) => {
   return (
-    <button className="aspect-square w-10  rounded-full border-2 border-white bg-transparent p-1 text-white">
+    <button className="flex aspect-square  w-10 items-center justify-center rounded-full border-2 border-white bg-transparent p-0 text-white">
       {props.children}
     </button>
   );
