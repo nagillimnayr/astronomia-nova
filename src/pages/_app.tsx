c3080f776b7ba4fce0efdca0eabf98bcc0ac1e5a
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-12 18:24:22 -0700

    feat: add Roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import "~/styles/globals.css";
+import { type AppType } from 'next/dist/shared/lib/utils';
+import '~/styles/globals.css';
+import { Roboto } from 'next/font/google';
+const roboto = Roboto({ weight: '400', subsets: ['latin'] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
