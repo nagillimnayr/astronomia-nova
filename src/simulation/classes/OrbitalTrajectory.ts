f7f706e04307b8e2106b1452411dbfd7ff9bd7ea
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-14 13:19:40 -0700

    fix: fix typo in 'constructor'

--- src/simulation/classes/OrbitalTrajectory.ts
+++ src/simulation/classes/OrbitalTrajectory.ts
@@ -3,7 +3,8 @@ import Vec3 from '../types/Vec3';
 export class OrbitalTrajectory extends Object3D {
   private _resolution!: number; // number of points
   private _points!: Vec3[]; // array of points
-  cosntructor() {
+  constructor() {
+    super();
     this._resolution = 0;
     this._points = [];
   }
