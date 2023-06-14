ba0d9741202329f02bcc518a0922bd584d4eeb22
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-13 19:21:09 -0700

    feat: add Perf monitor to Scene

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -7,6 +7,7 @@ import { useRef } from 'react';
 import { TimeContext } from '~/simulation/context/TimeContext';
 import TimeDisplay from '~/simulation/components/Time/TimeDisplay';
 import TimePanel from '~/simulation/components/Time/TimePanel';
+import { Perf } from 'r3f-perf';
 
 const Scene = () => {
   const timerRef = useRef<HTMLSpanElement>(null!);
@@ -38,6 +39,7 @@ const Scene = () => {
             <OrbitControls />
             <Simulation />
             <Stats />
+            <Perf />
           </Canvas>
         </div>
 
