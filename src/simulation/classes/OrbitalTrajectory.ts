84b43d7d665dc40868f5db9a4ac9cafa48014985
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-14 11:39:34 -0700

    feat: add points array to OrbitalTrajectory class

--- src/simulation/classes/OrbitalTrajectory.ts
+++ src/simulation/classes/OrbitalTrajectory.ts
@@ -1,9 +1,10 @@
 import { Object3D } from 'three';
-
+import Vec3 from '../types/Vec3';
 export class OrbitalTrajectory extends Object3D {
   private _resolution!: number; // number of points
-
+  private _points!: Vec3[]; // array of points
   cosntructor() {
     this._resolution = 0;
+    this._points = [];
   }
 }
