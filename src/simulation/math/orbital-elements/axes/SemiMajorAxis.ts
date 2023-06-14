ee7c58978864b63d1798bf6fbe2d75e62be0b2b7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-14 12:23:24 -0700

    feat: add function to calculate semiMajorAxis from period

--- src/simulation/math/orbital-elements/axes/SemiMajorAxis.ts
+++ src/simulation/math/orbital-elements/axes/SemiMajorAxis.ts
@@ -1,3 +1,10 @@
+import { nthRoot, square } from 'mathjs';
+import {
+  GRAV_CONST,
+  PI_SQUARED,
+  SOLAR_MASS,
+} from '~/simulation/utils/constants';
+
 export const SemiMajorAxis = {
   getFromPeriapsis: getFromPeriapsis,
 
@@ -30,4 +37,37 @@ function getFromApoapsis(eccentricity: number, apoapsis: number): number {
   const semiMajorAxis = apoapsis / (1 + eccentricity);
 
   return semiMajorAxis;
+}
+
+function getFromLinearEccentricity() {}
+
+function getFromSpecificOrbitalEnergy(
+  centralMass: number,
+  specificOrbitalEnergy: number
+) {
+  const semiMajorAxis = 
+}
+
+
+
+
+/**
+ * @description
+ * @author Ryan Milligan
+ * @date 14/06/2023
+ * @param {number} period
+ * @param {number} centralMass
+ * @returns {*}  {number} semiMajorAxis
+ */
+function getFromPeriod(period: number, centralMass: number): number {
+  // cube root of (G*M * T^2) / (4PI^2)
+  const semiMajorAxis = nthRoot(
+    ((GRAV_CONST * centralMass * square(period)) / 4.0) * PI_SQUARED,
+    3
+  ) as number;
+  return (semiMajorAxis);
+}
+
+function getFromApsides(apoapsis: number, periapsis: number) {
+  return (periapsis + apoapsis) / 2.0;
 }
