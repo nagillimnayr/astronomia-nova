d9b81eeb09c1422e05588fe6b78fcae0f0f369bb
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-14 17:31:56 -0700

    feat: create CelestialSphere component

---
+++ src/simulation/components/CelestialSphere.tsx
@@ -0,0 +1,40 @@
+import { useLoader } from '@react-three/fiber';
+import { BackSide, DoubleSide, FrontSide } from 'three';
+import { Wireframe } from '~/drei-imports/misc/Wireframe';
+import { Stars } from '~/drei-imports/staging/Stars';
+import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js';
+
+type CelestialSphereProps = {
+  children: React.ReactNode;
+};
+export const CelestialSphere = (props: CelestialSphereProps) => {
+  const starmap = useLoader(
+    EXRLoader,
+    'assets/textures/stars/starmap_2020_4k.exr'
+  );
+  return (
+    <group>
+      <mesh>
+        <sphereGeometry args={[500]} />
+        <meshBasicMaterial
+          args={[
+            {
+              map: starmap,
+              side: BackSide,
+            },
+          ]}
+        />
+        {props.children}
+      </mesh>
+      <Stars
+        radius={240}
+        depth={1}
+        count={5000}
+        factor={10}
+        saturation={0}
+        fade
+        speed={1}
+      />
+    </group>
+  );
+};
