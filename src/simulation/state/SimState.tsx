a2091d6d053177bd69a77ddc2168e7c8e05c3dc1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-15 21:33:26 -0700

    feat: add root and controls to global state

--- src/simulation/state/SimState.tsx
+++ src/simulation/state/SimState.tsx
@@ -1,13 +1,15 @@
 import { Object3D } from 'three';
 import { proxy } from 'valtio';
 import KeplerBody from '../classes/KeplerBody';
+import { OrbitControls } from 'three-stdlib';
 
 type SimStateObj = {
+  root: KeplerBody;
   selected: KeplerBody | null;
+  controls: OrbitControls;
+
+  update: (deltaTime: number) => void;
 };
-export const SimState = proxy<SimStateObj>({
-  selected: null,
-});
 
 export const select = (newSelection: KeplerBody) => {
   if (!newSelection) {
@@ -18,7 +20,27 @@ export const select = (newSelection: KeplerBody) => {
   SimState.selected = newSelection;
 
   console.log(`New selection: ${newSelection.name}:`, SimState.selected);
+
+  SimState.controls.target = newSelection.position;
+  SimState.controls.object.lookAt(newSelection.position);
+  SimState.controls.update();
+  SimState.controls.object.updateProjectionMatrix();
 };
 export const unselect = () => {
   SimState.selected = null;
 };
+
+export const setRoot = (root: KeplerBody) => {
+  SimState.root = root;
+};
+
+const updateFn = (deltaTime: number) => {
+  return;
+};
+
+export const SimState = proxy<SimStateObj>({
+  root: null!, // root of the Kepler Tree
+  selected: null, // current selection
+  controls: null!,
+  update: updateFn,
+});
