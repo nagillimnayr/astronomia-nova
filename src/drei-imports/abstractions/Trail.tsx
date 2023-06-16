17ccdc9e583c68b67a0616191eaf0143945c0571
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-16 09:43:41 -0700

    build: delete drei imports

--- src/drei-imports/abstractions/Trail.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const Trail = dynamic(
-  () => import("@react-three/drei/core/Trail").then((mod) => mod.Trail),
-  { ssr: false }
-);
-
-export { Trail };
--- src/drei-imports/shapes/ScreenQuad.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-export const ScreenQuad = dynamic(
-  () =>
-    import('@react-three/drei/core/ScreenQuad').then((mod) => {
-      return mod.ScreenQuad;
-    }),
-  { ssr: false }
-);
