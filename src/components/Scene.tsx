fe03fd1b0001764660e8484a40f573254127cc34
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-16 16:28:57 -0700

    feat: set container to relative positioning

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -6,14 +6,13 @@ import { VRButton, ARButton, XR } from '@react-three/xr';
 import { Suspense } from 'react';
 import { LoadingFallback } from './LoadingFallback';
 import { CameraControls, PerspectiveCamera } from '@react-three/drei';
-import { simState } from '~/simulation/state/SimState';
-import { useEventListener } from 'usehooks-ts';
 import { HUD } from '~/simulation/components/HUD/HUD';
 import { camState } from '~/simulation/state/CamState';
 
 const Scene = () => {
   return (
-    <div className="h-min-[42rem] flex h-[42rem] w-full flex-col justify-center">
+    <div className="h-min-[42rem] relative flex h-[42rem] w-full flex-col justify-center">
+      <HUD />
       <div className=" absolute bottom-0 right-1 h-24 w-40 select-none whitespace-nowrap border-2 border-green-400">
         <VRButton />
       </div>
@@ -31,9 +30,7 @@ const Scene = () => {
 
                 camState.setControls(controls);
               }}
-            >
-              <HUD />
-            </CameraControls>
+            />
             <Simulation />
             {/* <Stats /> */}
             {/* <Perf /> */}
