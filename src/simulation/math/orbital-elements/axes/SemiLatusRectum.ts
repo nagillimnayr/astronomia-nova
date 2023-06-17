32b039703d8ccff46f4b0b5ec77eaefa6b1bb56f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-16 20:06:54 -0700

    feat: implement calculations for semiLatusRectum

--- src/simulation/math/orbital-elements/axes/SemiLatusRectum.ts
+++ src/simulation/math/orbital-elements/axes/SemiLatusRectum.ts
@@ -1,1 +1,13 @@
-export const SemiLatusRectum = {};
+import { square } from 'mathjs';
+
+const fromEccentricity = (semiMajorAxis: number, eccentricity: number) => {
+  return semiMajorAxis * (1.0 - square(eccentricity));
+};
+const fromApsides = (apoapsis: number, periapsis: number) => {
+  return (2.0 * apoapsis * periapsis) / (apoapsis + periapsis);
+};
+
+export const SemiLatusRectum = {
+  fromEccentricity,
+  fromApsides,
+};
