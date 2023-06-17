aa050d1fe06b6b3097c9ae7a8173d8155d2f9671
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-16 20:05:56 -0700

    feat: implement calculations for semiMinorAxis

--- src/simulation/math/orbital-elements/axes/SemiMinorAxis.ts
+++ src/simulation/math/orbital-elements/axes/SemiMinorAxis.ts
@@ -1,1 +1,33 @@
-export const SemiMinorAxis = {};
+import { sqrt } from 'mathjs';
+
+/**
+ * @description
+ * @author Ryan Milligan
+ * @date 16/06/2023
+ * @param {number} semiMajorAxis (meters)
+ * @param {number} semiLatusRectum (meters)
+ * @returns {*}  {number} semiMinorAxis (m)
+ */
+const fromSemiLatusRectum = (
+  semiMajorAxis: number,
+  semiLatusRectum: number
+): number => {
+  return sqrt(semiMajorAxis * semiLatusRectum) as number;
+};
+
+/**
+ * @description
+ * @author Ryan Milligan
+ * @date 16/06/2023
+ * @param {number} apoapsis (meters)
+ * @param {number} periapsis (meters)
+ * @returns {*}  {number} semiMinorAxis (m)
+ */
+const fromApsides = (apoapsis: number, periapsis: number): number => {
+  return sqrt(apoapsis * periapsis) as number;
+};
+
+export const SemiMinorAxis = {
+  fromSemiLatusRectum,
+  fromApsides,
+};
