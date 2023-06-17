1c1dff770079b47da48a28058992f8b84d4a06ed
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-17 08:23:42 -0700

    feat: nest Body and Trajectory in Orbit

--- src/simulation/components/Orbit/Orbit.tsx
+++ src/simulation/components/Orbit/Orbit.tsx
@@ -1,25 +1,63 @@
 import { OrbitalElements } from '~/simulation/classes/OrbitalElements';
 import { Trajectory } from './Trajectory/Trajectory';
-import { useMemo } from 'react';
+import { useCallback, useContext, useMemo } from 'react';
 import loadBodyPreset, { PresetKey } from '~/simulation/utils/loadBodyPreset';
-import { BodyAttributes } from '../Body/Body';
+import Body, { BodyAttributes } from '../Body/Body';
+import KeplerBody from '~/simulation/classes/KeplerBody';
+import KeplerTreeContext from '~/simulation/context/KeplerTreeContext';
+import { CentralMassContext } from '~/simulation/context/CentralMassContext';
+import { calculateOrbitFromPeriapsis } from '~/simulation/math/orbit/calculateOrbit';
 
 type OrbitProps = {
   children: React.ReactNode;
   name: PresetKey;
+  texturePath: string;
 };
 
 export const Orbit = (props: OrbitProps) => {
+  const centralMass = useContext(CentralMassContext);
+
+  // Note: create usePreset to get memoized preset data
   const preset = useMemo<BodyAttributes>(
     () => loadBodyPreset(props.name),
     [props.name]
   );
 
+  const elements = useMemo(
+    () =>
+      calculateOrbitFromPeriapsis(
+        preset.initialPosition,
+        preset.initialVelocity,
+        centralMass
+      ),
+    []
+  );
+
+  // callback function to be passed down to children via context provider
+  // the child will call it within a callback ref and pass their reference
+  // as the argument, where it will be used to construct the Kepler Tree
+  const addChildToTree = useCallback((body: KeplerBody) => {
+    if (!body) {
+      return;
+    }
+
+    // setup attachment to parent
+    const parent: KeplerBody = body.parent as KeplerBody;
+    console.assert(parent, 'failed to cast to parent');
+    parent.addOrbitingBody(body);
+  }, []);
+
   return (
     <>
-      <object3D>
-        <Trajectory />
-      </object3D>
+      <KeplerTreeContext.Provider value={addChildToTree}>
+        <Body args={preset} texturePath={props.texturePath}>
+          {props.children}
+        </Body>
+      </KeplerTreeContext.Provider>
+      <Trajectory
+        semiMajorAxis={elements.semiMajorAxis}
+        semiMinorAxis={elements.semiMinorAxis}
+      />
     </>
   );
 };
