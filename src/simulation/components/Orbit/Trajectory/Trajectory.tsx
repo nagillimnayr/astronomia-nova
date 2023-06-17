7839c87c2d9c0873512cf995eef16f679f2ed790
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-17 08:24:41 -0700

    feat: create elliptical curve in Trajectory

--- src/simulation/components/Orbit/Trajectory/Trajectory.tsx
+++ src/simulation/components/Orbit/Trajectory/Trajectory.tsx
@@ -1,7 +1,34 @@
+import { CatmullRomLine, Line } from '@react-three/drei';
+import { useMemo } from 'react';
+import { EllipseCurve } from 'three';
+import { LinearEccentricity } from '~/simulation/math/orbital-elements/LinearEccentricity';
+
 type TrajectoryProps = {
   semiMajorAxis: number;
   semiMinorAxis: number;
+  linearEccentricity?: number;
 };
 export const Trajectory = (props: TrajectoryProps) => {
-  return <></>;
+  const points = useMemo(() => {
+    const linearEccentricity =
+      props.linearEccentricity ??
+      LinearEccentricity.fromAxes(props.semiMajorAxis, props.semiMinorAxis);
+    return new EllipseCurve(
+      -linearEccentricity,
+      0,
+      props.semiMajorAxis,
+      props.semiMinorAxis
+    ).getSpacedPoints(64);
+  }, [props.semiMajorAxis, props.semiMinorAxis, props.linearEccentricity]);
+
+  return (
+    <>
+      <CatmullRomLine
+        points={points}
+        color={'white'}
+        curveType="catmullrom"
+        lineWidth={0.1}
+      />
+    </>
+  );
 };
