4f29cd3ef6a91e9aaabe9c18adac3f9442872911
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-16 20:28:48 -0700

    feat: implement calculations for orbital elements

---
+++ src/simulation/math/orbit/calculateOrbit.ts
@@ -0,0 +1,64 @@
+import { SpecificOrbitalEnergy } from '../orbital-elements/SpecificOrbitalEnergy';
+import { Apoapsis } from '../orbital-elements/apsides/Apoapsis';
+import { SemiMajorAxis } from '../orbital-elements/axes/SemiMajorAxis';
+import { LinearEccentricity } from '../orbital-elements/LinearEccentricity';
+import { Eccentricity } from '../orbital-elements/Eccentricity';
+import { SemiLatusRectum } from '../orbital-elements/axes/SemiLatusRectum';
+import { SemiMinorAxis } from '../orbital-elements/axes/SemiMinorAxis';
+
+export const calculateOrbitFromPeriapsis = (
+  periapsis: number,
+  maxOrbitalSpeed: number,
+  centralMass: number
+) => {
+  // calculate specificOrbitalEnergy
+  const specificOrbitalEnergy = SpecificOrbitalEnergy.calculate(
+    maxOrbitalSpeed,
+    centralMass,
+    periapsis
+  );
+
+  // calculate semiMajorAxis
+  const semiMajorAxis = SemiMajorAxis.getFromSpecificOrbitalEnergy(
+    centralMass,
+    specificOrbitalEnergy
+  );
+
+  // calculate apoapsis
+  const apoapsis = Apoapsis.fromPeriapsis(semiMajorAxis, periapsis);
+
+  // linear eccentricity
+  const linearEccentricity = LinearEccentricity.fromApsis(
+    semiMajorAxis,
+    apoapsis
+  );
+
+  // eccentricity
+  const eccentricity = Eccentricity.fromLinearEccentricity(
+    semiMajorAxis,
+    linearEccentricity
+  );
+
+  // semiLatusRectum
+  const semiLatusRectum = SemiLatusRectum.fromEccentricity(
+    semiMajorAxis,
+    eccentricity
+  );
+
+  // semiMinorAxis
+  const semiMinorAxis = SemiMinorAxis.fromSemiLatusRectum(
+    semiMajorAxis,
+    semiLatusRectum
+  );
+
+  return {
+    periapsis,
+    apoapsis,
+    semiMajorAxis,
+    semiMinorAxis,
+    semiLatusRectum,
+    specificOrbitalEnergy,
+    eccentricity,
+    linearEccentricity,
+  };
+};
