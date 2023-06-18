9ffcbdbcb020af81020a68127369ed48fe2f5595
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-18 16:09:59 -0700

    build: create About page stub

---
+++ src/pages/about.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+        <meta name="description" content="" />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/favicon/lightmode/favicon.svg"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/lightmode/favicon.png"
+        />
+      </Head>
+    </>
+  );
+};
