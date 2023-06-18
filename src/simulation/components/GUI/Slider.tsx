0e7a0c287d52fba083a816db475f4015bde3ccf4
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-18 12:27:28 -0700

    feat: enable pointer events on Slider component

--- src/simulation/components/GUI/Slider.tsx
+++ src/simulation/components/GUI/Slider.tsx
@@ -13,7 +13,7 @@ export const Slider = () => {
         <RadixSlider.Track className="relative h-[3px] grow rounded-full bg-blackA10  ">
           <RadixSlider.Range className="absolute h-full  rounded-full bg-white" />
         </RadixSlider.Track>
-        <RadixSlider.Thumb className="block aspect-square h-5 w-5 rounded-full bg-white shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_6px] focus:shadow-blackA8 focus:outline-none" />
+        <RadixSlider.Thumb className="pointer-events-auto block aspect-square h-5 w-5 rounded-full bg-white shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_6px] focus:shadow-blackA8 focus:outline-none" />
       </RadixSlider.Root>
     </div>
   );
