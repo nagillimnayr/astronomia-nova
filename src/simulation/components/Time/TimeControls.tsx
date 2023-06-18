e500c19461c343ca85038485581f6edc88c820ef
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-18 12:31:21 -0700

    feat: add Slider to TimeControls

--- src/simulation/components/Time/TimeControls.tsx
+++ src/simulation/components/Time/TimeControls.tsx
@@ -1,14 +1,20 @@
+import { Slider } from '../GUI/Slider';
 import BackwardButton from './buttons/BackwardButton';
 import ForwardButton from './buttons/ForwardButton';
 import PauseButton from './buttons/PauseButton';
 
 const TimeControls = () => {
   return (
-    <div className="pointer-events-auto mt-2 flex w-40 flex-row justify-between self-center ">
-      <BackwardButton />
-      <PauseButton />
-      <ForwardButton />
-    </div>
+    <>
+      <div className="pointer-events-auto mt-2 flex w-40 flex-row justify-between self-center ">
+        <BackwardButton />
+        <PauseButton />
+        <ForwardButton />
+      </div>
+      <div className="pointer-events-auto my-2 flex w-40 flex-row justify-between self-center ">
+        <Slider />
+      </div>
+    </>
   );
 };
 
