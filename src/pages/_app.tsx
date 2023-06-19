e26d4a01138955e596f597cbf4e696fb449e4aa1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-19 11:00:11 -0700

    feat: add MDXProvider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import { type AppType } from 'next/dist/shared/lib/utils';
 import '~/styles/globals.css';
+import { MDXProvider } from '@mdx-js/react';
 import { Roboto } from 'next/font/google';
 const roboto = Roboto({ weight: '400', subsets: ['latin'] });
 
+const components = {};
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className="min-w-screen m-0 flex h-fit min-h-screen w-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c] p-0 text-white">
-      <Component {...pageProps} />
-    </div>
+    <MDXProvider components={components}>
+      <div className="min-w-screen m-0 flex h-fit min-h-screen w-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c] p-0 text-white">
+        <Component {...pageProps} />
+      </div>
+    </MDXProvider>
   );
 };
 
