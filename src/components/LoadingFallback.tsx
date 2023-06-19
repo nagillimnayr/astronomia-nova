b6cded8e82997f0795d9ac9e48fe9c092f31b6b3
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-19 12:44:36 -0700

    feat: remove text from LoadingFallback

--- src/components/LoadingFallback.tsx
+++ src/components/LoadingFallback.tsx
@@ -2,8 +2,6 @@ export const LoadingFallback = () => {
   return (
     <div className="flex w-full flex-row items-center justify-center">
       <span className="loading loading-spinner loading-lg" />
-      <h2 className="mx-2 place-self-center text-3xl text-white">Loading...</h2>
-      <span className="loading loading-spinner loading-lg" />
     </div>
   );
 };
