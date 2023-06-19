1dede5cc1adf9ba55dfa9a12e8cfb33c7ede4b0f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-19 12:43:47 -0700

    feat: add NavBar to _app layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type AppType } from 'next/dist/shared/lib/utils';
 import '~/styles/globals.css';
 import { MDXProvider } from '@mdx-js/react';
 import { Roboto } from 'next/font/google';
+import NavBar from '~/components/gui/Navigation/NavBar';
 const roboto = Roboto({ weight: '400', subsets: ['latin'] });
 
 const components = {};
@@ -10,6 +11,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <MDXProvider components={components}>
       <div className="min-w-screen m-0 flex h-fit min-h-screen w-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c] p-0 text-white">
+        <NavBar />
         <Component {...pageProps} />
       </div>
     </MDXProvider>
