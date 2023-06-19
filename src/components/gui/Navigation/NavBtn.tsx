b48c9e22259ab29bd7d67bbdaf909bb09e34110d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-19 13:01:12 -0700

    refactor: condense bg opacity into bg-color

--- src/components/gui/Navigation/NavBtn.tsx
+++ src/components/gui/Navigation/NavBtn.tsx
@@ -6,7 +6,7 @@ type NavBtnProps = {
 };
 const NavBtn = (props: NavBtnProps) => {
   return (
-    <button className="mx-0 my-0  px-4 hover:bg-neutral-300 hover:bg-opacity-20">
+    <button className="mx-0 my-0  px-4 hover:bg-neutral-300/10">
       <Link href={props.href}>
         <h3 className="text-3xl ">{props.children}</h3>
       </Link>
