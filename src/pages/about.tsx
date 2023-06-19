2de84c3ed24ad78817c2a1c930327882fb01e98c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-19 12:44:08 -0700

    feat: remove bg color

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -18,7 +18,7 @@ const About: NextPage = () => {
           href="/favicon/lightmode/favicon.png"
         />
       </Head>
-      <div className="min-w-screen h-fit min-h-screen bg-slate-700">
+      <div className="h-fit min-h-screen min-w-full border-2 border-white">
         <header className="pl-6 pt-4">
           <h1 className="text-5xl text-white ">About</h1>
         </header>
