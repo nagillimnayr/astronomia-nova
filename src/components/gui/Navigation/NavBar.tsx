ee4e7b3c580d9719564d0a6d357078316ca3232d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-19 14:19:08 -0700

    feat: add home links to NavBar

--- src/components/gui/Navigation/NavBar.tsx
+++ src/components/gui/Navigation/NavBar.tsx
@@ -5,10 +5,14 @@ const NavBar = () => {
   return (
     <header className="navbar sticky top-0 flex min-w-full flex-row items-stretch justify-start border-2 border-white py-0 pl-6 pr-36">
       <div className="flex-start mr-auto flex flex-row">
-        <div className="aspect-square w-24 border-2 border-white"></div>
-        <h2 className="mx-6 text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Placeholder <span className="text-[hsl(280,100%,70%)]">Title</span>
-        </h2>
+        <Link className="h-full w-full" href="/">
+          <div className="aspect-square w-24 border-2 border-white"></div>
+        </Link>
+        <Link className="whitespace-nowrap" href="/">
+          <h2 className="mx-6 text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Placeholder <span className="text-[hsl(280,100%,70%)]">Title</span>
+          </h2>
+        </Link>
       </div>
 
       <NavBtn href="about">About</NavBtn>
--- src/components/gui/Navigation/NavBtn.tsx
+++ src/components/gui/Navigation/NavBtn.tsx
@@ -7,7 +7,10 @@ type NavBtnProps = {
 const NavBtn = (props: NavBtnProps) => {
   return (
     <button className="mx-0 my-0  px-4 hover:bg-neutral-300/10">
-      <Link href={props.href}>
+      <Link
+        className="flex h-full w-full items-center justify-center"
+        href={props.href}
+      >
         <h3 className="text-3xl ">{props.children}</h3>
       </Link>
     </button>
