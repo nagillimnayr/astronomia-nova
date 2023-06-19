ee626eb970f51f54dc34ee0b8274a2df434b9a81
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-18 18:31:02 -0700

    feat: set text color to white

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ const roboto = Roboto({ weight: '400', subsets: ['latin'] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className="min-w-screen m-0 flex h-fit min-h-screen w-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c] p-0">
+    <div className="min-w-screen m-0 flex h-fit min-h-screen w-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c] p-0 text-white">
       <Component {...pageProps} />
     </div>
   );
