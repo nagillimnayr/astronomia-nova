7244ee8b5fac1204f48d84c8126ad2d5cd35694e
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-19 11:23:08 -0700

    feat: center LoadingFallback

--- src/components/LoadingFallback.tsx
+++ src/components/LoadingFallback.tsx
@@ -1,7 +1,9 @@
 export const LoadingFallback = () => {
   return (
-    <div>
-      <h2 className="place-self-center text-2xl text-white">Loading...</h2>
+    <div className="flex w-full flex-row items-center justify-center">
+      <span className="loading loading-spinner loading-lg" />
+      <h2 className="mx-2 place-self-center text-3xl text-white">Loading...</h2>
+      <span className="loading loading-spinner loading-lg" />
     </div>
   );
 };
