16d221a7e28f63eed1ef1fefa757d2184d0c08f9
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-19 17:40:07 -0700

    feat: position Footer at bottom of page

--- src/components/gui/Footer/Footer.tsx
+++ src/components/gui/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0">
+    <footer className="relative bottom-0 mt-auto h-60 w-screen shrink-0 grow-0 border-2 border-white">
       <div></div>
     </footer>
   );
