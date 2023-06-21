064dc9ea55149031ef6df1e055551bd22bde58cc
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-20 20:41:24 -0700

    feat: add showArrows to debugState

--- src/simulation/state/DebugState.tsx
+++ src/simulation/state/DebugState.tsx
@@ -2,7 +2,9 @@ import { proxy } from 'valtio';
 
 type DebugStateObj = {
   showBoundingBoxes: boolean;
+  showArrows: boolean;
 };
 export const debugState = proxy<DebugStateObj>({
   showBoundingBoxes: false,
+  showArrows: false,
 });
