93fc0f41eb4b33fdd6e7ad2c447031c1e07919cf
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-21 12:06:57 -0700

    feat: disable tooltip on slider

--- src/simulation/components/Time/TimescaleSlider.tsx
+++ src/simulation/components/Time/TimescaleSlider.tsx
@@ -42,13 +42,13 @@ export const TimescaleSlider = () => {
         <RadixSlider.Track className="relative h-[3px] grow rounded-full bg-blackA10  ">
           <RadixSlider.Range className="absolute h-full  rounded-full bg-white" />
         </RadixSlider.Track>
-        <TimescaleTooltip show={isHovered}>
-          <RadixSlider.Thumb
-            className="pointer-events-auto block aspect-square h-5 w-5 cursor-pointer rounded-full bg-white shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_6px] focus:shadow-blackA8 focus:outline-none"
-            onPointerEnter={handlePointerEnter}
-            onPointerLeave={handlePointerLeave}
-          />
-        </TimescaleTooltip>
+        {/* <TimescaleTooltip show={isHovered}> */}
+        <RadixSlider.Thumb
+          className="pointer-events-auto block aspect-square h-5 w-5 cursor-pointer rounded-full bg-white shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_6px] focus:shadow-blackA8 focus:outline-none"
+          onPointerEnter={handlePointerEnter}
+          onPointerLeave={handlePointerLeave}
+        />
+        {/* </TimescaleTooltip> */}
       </RadixSlider.Root>
     </form>
   );
