495f069f2eeca0ccd5e7a3d6314e9b0200bad0e5
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-21 15:42:04 -0700

    feat: switch back to full SolarSystem

--- src/simulation/components/Simulation.tsx
+++ src/simulation/components/Simulation.tsx
@@ -48,8 +48,8 @@ const Simulation = () => {
     <>
       <group>
         {/* <polarGridHelper args={[24, 16, 24, 64]} /> */}
-        {/* <SolarSystem /> */}
-        <EarthMars />
+        <SolarSystem />
+        {/* <EarthMars /> */}
       </group>
       {/* <DebugPanel /> */}
     </>
