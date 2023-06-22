7c1f3c8318e9b202e2014b9ee8fcdeaeb8c236f1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-21 23:54:00 -0700

    feat: add CameraTarget

---
+++ src/simulation/components/Camera/CameraTarget.tsx
@@ -0,0 +1,32 @@
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { Object3D, Vector3 } from 'three';
+import { camState } from '~/simulation/state/CamState';
+import Vec3 from '~/simulation/types/Vec3';
+
+export const CameraTarget = () => {
+  const ref = useRef<Object3D>(null!);
+
+  // useFrame(() => {
+  //   if (!ref.current) return;
+
+  //   const worldPos = new Vector3();
+  //   ref.current.getWorldPosition(worldPos);
+  //   const targetPos: Vec3 = worldPos.toArray();
+  //   camState.controls
+  //     .lookInDirectionOf(...ref.current.position.toArray(), false)
+  //     .catch((reason) => {
+  //       console.log('promise rejected: ', reason);
+  //     });
+  // });
+
+  return (
+    <object3D
+      ref={(obj) => {
+        if (!obj) return;
+        ref.current = obj;
+        camState.marker = obj;
+      }}
+    ></object3D>
+  );
+};
