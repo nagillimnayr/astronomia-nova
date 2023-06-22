3c4c48afaa28975683b34b7f722169c0abf7701c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-21 23:53:14 -0700

    feat: add camera to target

--- src/simulation/state/CamState.tsx
+++ src/simulation/state/CamState.tsx
@@ -2,10 +2,12 @@ import { CameraControls } from '@react-three/drei';
 import { simState } from './SimState';
 import { proxy } from 'valtio';
 import Vec3 from '../types/Vec3';
-import { Object3D, Vector3 } from 'three';
+import { Camera, Object3D, Vector3 } from 'three';
+import { OrbitControls } from 'three-stdlib';
 
 type CamStateObj = {
   controls: CameraControls;
+  marker: Object3D;
   focusTarget: Object3D | null;
 
   isTransitioning: boolean;
@@ -17,17 +19,18 @@ type CamStateObj = {
 
 const setControls = (controls: CameraControls) => {
   camState.controls = controls;
-  controls.mouseButtons.right = 1; // disable pan (set to rotate on right mouse button instead)
-  controls.addEventListener('update', camState.updateControls);
-  camState.updateControls();
+  // controls.mouseButtons.right = 1; // disable pan (set to rotate on right mouse button instead)
+  // controls.addEventListener('update', camState.updateControls);
+  // camState.updateControls();
 };
 
+let isTransitioning = false;
 const updateControls = () => {
   if (!camState.controls) {
     return;
   }
   if (!camState.focusTarget) return;
-
+  // if (isTransitioning) return;
   const worldPos = new Vector3();
   camState.focusTarget.getWorldPosition(worldPos);
   const targetPos: Vec3 = worldPos.toArray();
@@ -39,15 +42,35 @@ const updateControls = () => {
 };
 
 const setFocus = (target: Object3D) => {
-  camState.focusTarget = target;
   if (!target) return;
 
-  target.attach(camState.controls.camera);
+  const body = target.parent!.parent!;
+  camState.focusTarget = body;
+  // attach camera to target
+  // body.add(camState.controls.camera);
+  body.add(camState.controls.camera);
+
+  // update controls to follow target
+  if (!camState.focusTarget) return;
+  const worldPos = new Vector3();
+  camState.focusTarget.getWorldPosition(worldPos);
+  const targetPos: Vec3 = worldPos.toArray();
+  isTransitioning = true;
+  camState.controls
+    .setTarget(...targetPos, true)
+    .then(() => {
+      isTransitioning = false;
+    })
+    .catch((reason) => {
+      console.log('promise rejected: ', reason);
+    });
+
   camState.updateControls();
 };
 
 export const camState = proxy<CamStateObj>({
   controls: null!,
+  marker: null!,
   focusTarget: null,
 
   isTransitioning: false,
