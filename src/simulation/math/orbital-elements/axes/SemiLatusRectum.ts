82c60ab281ff32097f7177c3e5058d735c82c75c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-26 11:57:32 -0700

    refactor: change arrow functions to regular functions

--- src/simulation/math/orbital-elements/axes/SemiLatusRectum.ts
+++ src/simulation/math/orbital-elements/axes/SemiLatusRectum.ts
@@ -1,12 +1,22 @@
-export const getSemiLatusRectumFromEccentricity = (
+export function getSemiLatusRectumFromEccentricity(
   semiMajorAxis: number,
   eccentricity: number
-) => {
+) {
   return semiMajorAxis * (1.0 - eccentricity ** 2);
-};
-export const getSemiLatusRectumFromApsides = (
+}
+
+/**
+ * @description
+ * @author Ryan Milligan
+ * @date 26/06/2023
+ * @export
+ * @param {number} apoapsis
+ * @param {number} periapsis
+ * @returns {*}
+ */
+export function getSemiLatusRectumFromApsides(
   apoapsis: number,
   periapsis: number
-) => {
+) {
   return (2.0 * apoapsis * periapsis) / (apoapsis + periapsis);
-};
+}
