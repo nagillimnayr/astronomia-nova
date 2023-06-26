195acc1e009916ca6a916cb6e8995dcf75c068a7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-26 14:26:26 -0700

    docs: add comment about right focus

--- src/simulation/math/orbital-elements/OrbitalRadius.ts
+++ src/simulation/math/orbital-elements/OrbitalRadius.ts
@@ -6,6 +6,7 @@ export function getRadiusAtTrueAnomaly(
   semiMajorAxis: number,
   eccentricity: number
 ) {
+  // Calculates the radius at a particular angle, using the elliptical formula in polar form, relative to the right focus
   const semiLatusRectum = getSemiLatusRectumFromEccentricity(
     semiMajorAxis,
     eccentricity
