acbc1b91ccea84bc31951ba6b567872fb82f3265
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-27 15:06:23 -0700

    feat: add Arrow component

---
+++ src/simulation/components/Orbit/arrows/Arrow.tsx
@@ -0,0 +1,38 @@
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { ArrowHelper, ColorRepresentation, Vector3, Vector3Tuple } from 'three';
+
+type ArrowProps = {
+  color: ColorRepresentation;
+  direction: Vector3;
+  origin: Vector3;
+  length: 
+};
+export const Arrow = forwardRef<ArrowHelper, ArrowProps>(function Arrow(
+  props: ArrowProps,
+  fwdRef
+) {
+  const arrowRef = useRef<ArrowHelper>(null!);
+  useImperativeHandle(
+    fwdRef,
+    () => {
+      return arrowRef.current;
+    },
+    [arrowRef]
+  );
+
+  
+
+  return (
+    <>
+      <arrowHelper
+        ref={(arrow) => {
+          if (!arrow) return;
+          arrowRef.current = arrow;
+          arrow.setColor(props.color);
+          arrow.position.set(...props.origin.toArray());
+          arrow.setDirection(props.direction);
+        }}
+      />
+    </>
+  );
+});
