a60689379204c93da08d4f5974f0b320966cb2da
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-28 13:14:53 -0700

    feat: add earth page

---
+++ src/pages/earth.tsx
@@ -0,0 +1,53 @@
+import { type NextPage } from 'next';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import Link from 'next/link';
+import { Suspense, useRef } from 'react';
+import { LoadingFallback } from '~/components/LoadingFallback';
+import { FullscreenLayout } from '~/components/layout/FullscreenLayout';
+//import Scene from '~/components/Scene';
+
+const Scene = dynamic(
+  () => import('~/components/scenes/Scene').then((mod) => mod.default),
+  {
+    ssr: false,
+  }
+);
+const SunEarth = dynamic(
+  () =>
+    import('~/simulation/components/SolarSystem/SunEarth').then(
+      (mod) => mod.default
+    ),
+  {
+    ssr: false,
+  }
+);
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Solar System</title>
+        <meta name="description" content="" />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/favicon/lightmode/favicon.svg"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/lightmode/favicon.png"
+        />
+      </Head>
+
+      <FullscreenLayout>
+        <Scene>
+          <SunEarth />
+        </Scene>
+      </FullscreenLayout>
+    </>
+  );
+};
+
+export default Page;
