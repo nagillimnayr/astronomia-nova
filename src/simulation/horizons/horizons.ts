8707c40c74e51393aeb6fb7f1ef3a5fb21d5e326
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-28 19:29:18 -0700

    feat: add code for calling functions from commandline

--- src/simulation/horizons/horizons.ts
+++ src/simulation/horizons/horizons.ts
@@ -1,5 +1,6 @@
 import fs from 'fs-extra';
 import { parseHorizonsElements } from './parseHorizonsElements';
+import { getElementsJ2000 } from './getElementsJ2000';
 
 const url = 'https://ssd.jpl.nasa.gov/api/horizons.api';
 
@@ -18,47 +19,56 @@ const TLIST = `'2000-Jan-01%2012:00:00'`;
 // const QUANTITIES = 'EC';
 const CSV_FORMAT = 'NO';
 
-export function horizons(code: string, outputFile?: string) {
-  const query = `${url}?format=${format}&COMMAND=${code}&OBJ_DATA=${OBJ_DATA}&MAKE_EPHEM=${MAKE_EPHEM}&EPHEM_TYPE=${EPHEM_TYPE}&CENTER=${CENTER}&REF_PLANE=${REF_PLANE}&TLIST=${TLIST}&CSV_FORMAT=${CSV_FORMAT}`;
+// export function horizons(code: string, outputFile?: string) {
+//   const query = `${url}?format=${format}&COMMAND=${code}&OBJ_DATA=${OBJ_DATA}&MAKE_EPHEM=${MAKE_EPHEM}&EPHEM_TYPE=${EPHEM_TYPE}&CENTER=${CENTER}&REF_PLANE=${REF_PLANE}&TLIST=${TLIST}&CSV_FORMAT=${CSV_FORMAT}`;
 
-  fetch(query)
-    .then((response) => {
-      console.log('status code:', response.status);
-      if (!response.ok) {
-        throw new Error(`error! status code: ${response.status}`);
-      }
+//   fetch(query)
+//     .then((response) => {
+//       console.log('status code:', response.status);
+//       if (!response.ok) {
+//         throw new Error(`error! status code: ${response.status}`);
+//       }
 
-      response
-        .text()
-        .then((value) => {
-          const parsedData = parseHorizonsElements(value);
-          console.log(parsedData);
-          if (outputFile) {
-            fs.writeFile('raw_' + outputFile, value)
-              .then(() => {
-                console.log('data written to:', outputFile);
-              })
-              .catch((reason) => {
-                console.error('error: ', reason);
-              });
-          }
-        })
-        .catch((reason) => {
-          console.error('error: ', reason);
-        });
-    })
+//       response
+//         .text()
+//         .then((value) => {
+//           const parsedData = parseHorizonsElements(value);
+//           console.log(parsedData);
+//           if (outputFile) {
+//             fs.writeFile('raw_' + outputFile, value)
+//               .then(() => {
+//                 console.log('data written to:', outputFile);
+//               })
+//               .catch((reason) => {
+//                 console.error('error: ', reason);
+//               });
+//           }
+//         })
+//         .catch((reason) => {
+//           console.error('error: ', reason);
+//         });
+//     })
 
-    .catch((reason) => {
-      console.error('error: ', reason);
-    });
+//     .catch((reason) => {
+//       console.error('error: ', reason);
+//     });
+// }
+
+async function horizons(code: string) {
+  try {
+    const elements = await getElementsJ2000(code);
+    console.log(elements);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-if (process.argv.length >= 4) {
+if (process.argv.length >= 3) {
   const code = process.argv[2];
-  const outputFile = process.argv[3];
+  // const outputFile = process.argv[3];
 
-  if (code && outputFile) {
-    horizons(code, outputFile);
+  if (code) {
+    await horizons(code);
   }
 } else {
   console.log('not enough arguments');
