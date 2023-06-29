778e3be54ab23a9d2b3c2d39adf2e7ff9745f15c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-28 19:21:04 -0700

    feat: add module for horizons element codes

---
+++ src/simulation/horizons/elementCodes.ts
@@ -0,0 +1,28 @@
+export type ElementCode =
+  | 'EC'
+  | 'QR'
+  | 'IN'
+  | 'OM'
+  | 'W'
+  | 'Tp'
+  | 'N'
+  | 'MA'
+  | 'TA'
+  | 'A'
+  | 'AD'
+  | 'PR';
+
+export const ALL_ELEMENT_CODES: ReadonlyArray<ElementCode> = [
+  'EC',
+  'QR',
+  'IN',
+  'OM',
+  'W',
+  'Tp',
+  'N',
+  'MA',
+  'TA',
+  'A',
+  'AD',
+  'PR',
+];
