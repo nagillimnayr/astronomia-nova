e0ce6ef6a7fcd1c27e7bb6bf97ac08fa66f0ae30
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-30 11:12:46 -0700

    feat: add type aliases for Element and Vector tables

---
+++ src/simulation/horizons/types/ElementTable.ts
@@ -0,0 +1,16 @@
+export type ElementTable = {
+  name: string;
+  date: string;
+  eccentricity: number;
+  periapsis: number;
+  inclination: number;
+  longitudeOfAscendingNode: number;
+  argumentOfPeriapsis: number;
+  timeOfPeriapsis: number;
+  meanMotion: number;
+  meanAnomaly: number;
+  trueAnomaly: number;
+  semiMajorAxis: number;
+  apoapsis: number;
+  siderealOrbitPeriod: number;
+};
