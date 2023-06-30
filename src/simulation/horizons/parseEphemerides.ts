fa6be34e302a2ae35f99c3e91c94b75a4f0fe6e4
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-30 11:12:28 -0700

    feat: add functions for saving and loading ephemeris data

--- src/simulation/horizons/parseEphemerides.ts
+++ src/simulation/horizons/parseEphemerides.ts
@@ -1,29 +1,12 @@
-import { Vector3Tuple } from 'three';
+import type { Vector3Tuple } from 'three';
 import { KM_TO_M } from '../utils/constants';
-import { ElementCode } from './elementCodes';
-import { VectorCode } from './vectorCodes';
+import type { ElementCode } from './elementCodes';
+import type { VectorCode } from './vectorCodes';
 
-export type ElementTable = {
-  eccentricity: number;
-  periapsis: number;
-  inclination: number;
-  longitudeOfAscendingNode: number;
-  argumentOfPeriapsis: number;
-  timeOfPeriapsis: number;
-  meanMotion: number;
-  meanAnomaly: number;
-  trueAnomaly: number;
-  semiMajorAxis: number;
-  apoapsis: number;
-  siderealOrbitPeriod: number;
-};
-
-type VectorTable = {
-  position: Vector3Tuple;
-  velocity: Vector3Tuple;
-  range: number;
-  rangeRate: number;
-};
+import type { ElementTable } from './types/ElementTable';
+import type { VectorTable } from './types/VectorTable';
+import _ from 'lodash';
+import { object } from 'zod';
 
 export function parseEphemeris(
   text: string,
@@ -33,69 +16,110 @@ export function parseEphemeris(
   const regexp = new RegExp(regexStr);
   const matches = text.match(regexp);
   if (!matches || matches.length < 2 || !matches[1]) {
-    console.error('no match found');
-    return 0;
+    console.error(`code ${code} was not found`);
+    console.log('text:', text);
+    console.log('matches:', matches);
+    throw new Error('no match found!');
   }
   const ephemeris = parseFloat(matches[1]) ?? 0;
   return ephemeris;
 }
 
+export function parseEphemerisDate(text: Readonly<string>) {
+  const regexp = /A\.D\.\s(.*)\sTDB/;
+  const matches = text.match(regexp);
+  if (!matches || matches.length < 2 || !matches[1]) {
+    console.error('text:', text);
+    console.error('matches:', matches);
+    throw new Error('no match found!');
+  }
+  // get the substring containing the date info
+  const dateStr = matches[1];
+
+  // split the date substring on ' ' into year-month-day / time
+  const [yearMonthDay, time] = _.split(dateStr, /\s/);
+
+  // split the year/month/day on '-'
+  const [year, month, day] = _.split(yearMonthDay, /\-/);
+
+  // construct a date object
+  const date = new Date();
+
+  return date;
+}
+export function parseEphemerisName(text: Readonly<string>) {
+  const regexp = /Target body name:\s*(\S*)\s/;
+  const matches = text.match(regexp);
+  if (!matches || matches.length < 2 || !matches[1]) {
+    console.error('text:', text);
+    console.error('matches:', matches);
+    throw new Error('no match found!');
+  }
+  const targetName = matches[1];
+  return targetName;
+}
+
 export function parseElements(text: Readonly<string>): ElementTable {
   // get text with elements data
-  const matches = text.match(/SOE([^]*)\$\$EOE/);
+  const matches = text.match(/SOE\n([^]*)\$\$EOE/);
   if (!matches || matches.length < 2) {
     console.log('error! no match found in:', text);
     throw new Error('no match found!');
   }
-  const elementsStr = matches[1];
-  if (!elementsStr) {
+
+  // get the substring that contains the ephemeris data
+  const substr = matches[1];
+  if (!substr) {
     const errorMsg = 'error: no element data found';
-    console.error(errorMsg);
     throw new Error(errorMsg);
   }
 
   return {
-    eccentricity: parseEphemeris(elementsStr, 'EC'),
-    periapsis: parseEphemeris(elementsStr, 'QR'),
-    inclination: parseEphemeris(elementsStr, 'IN'),
-    longitudeOfAscendingNode: parseEphemeris(elementsStr, 'OM'),
-    argumentOfPeriapsis: parseEphemeris(elementsStr, 'W'),
-    timeOfPeriapsis: parseEphemeris(elementsStr, 'Tp'),
-    meanMotion: parseEphemeris(elementsStr, 'N'),
-    meanAnomaly: parseEphemeris(elementsStr, 'MA'),
-    trueAnomaly: parseEphemeris(elementsStr, 'TA'),
-    semiMajorAxis: parseEphemeris(elementsStr, 'A'),
-    apoapsis: parseEphemeris(elementsStr, 'AD'),
-    siderealOrbitPeriod: parseEphemeris(elementsStr, 'PR'),
+    name: parseEphemerisName(text),
+    date: parseEphemerisDate(substr),
+    eccentricity: parseEphemeris(substr, 'EC'),
+    periapsis: parseEphemeris(substr, 'QR'),
+    inclination: parseEphemeris(substr, 'IN'),
+    longitudeOfAscendingNode: parseEphemeris(substr, 'OM'),
+    argumentOfPeriapsis: parseEphemeris(substr, 'W'),
+    timeOfPeriapsis: parseEphemeris(substr, 'Tp'),
+    meanMotion: parseEphemeris(substr, 'N'),
+    meanAnomaly: parseEphemeris(substr, 'MA'),
+    trueAnomaly: parseEphemeris(substr, 'TA'),
+    semiMajorAxis: parseEphemeris(substr, 'A'),
+    apoapsis: parseEphemeris(substr, 'AD'),
+    siderealOrbitPeriod: parseEphemeris(substr, 'PR'),
   };
 }
 
 export function parseVectors(text: Readonly<string>): VectorTable {
   // get text with vector data
 
-  const matches = text.match(/SOE([^]*)\$\$EOE/);
+  const matches = text.match(/SOE\n([^]*)\$\$EOE/);
   if (!matches || matches.length < 2) {
     console.log('error! no match found in:', text);
     throw new Error('no match found!');
   }
 
-  const vectorStr = matches[1];
-  if (!vectorStr) {
-    const errorMsg = 'error: no element data found';
-    console.error(errorMsg);
+  // get the substring that contains the ephemeris data
+  const substr = matches[1];
+  if (!substr) {
+    const errorMsg = 'error: no vector data found';
     throw new Error(errorMsg);
   }
 
-  const x = parseEphemeris(vectorStr, 'X') * KM_TO_M; // (m)
-  const y = parseEphemeris(vectorStr, 'Y') * KM_TO_M; // (m)
-  const z = parseEphemeris(vectorStr, 'Z') * KM_TO_M; // (m)
-  const vx = parseEphemeris(vectorStr, 'VX') * KM_TO_M; // (m/s)
-  const vy = parseEphemeris(vectorStr, 'VY') * KM_TO_M; // (m/s)
-  const vz = parseEphemeris(vectorStr, 'VZ') * KM_TO_M; // (m/s)
-  const range = parseEphemeris(vectorStr, 'RG') * KM_TO_M; // (m)
-  const rangeRate = parseEphemeris(vectorStr, 'RR') * KM_TO_M; // (m/s)
+  const x = parseEphemeris(substr, 'X') * KM_TO_M; // (m)
+  const y = parseEphemeris(substr, 'Y') * KM_TO_M; // (m)
+  const z = parseEphemeris(substr, 'Z') * KM_TO_M; // (m)
+  const vx = parseEphemeris(substr, 'VX') * KM_TO_M; // (m/s)
+  const vy = parseEphemeris(substr, 'VY') * KM_TO_M; // (m/s)
+  const vz = parseEphemeris(substr, 'VZ') * KM_TO_M; // (m/s)
+  const range = parseEphemeris(substr, 'RG') * KM_TO_M; // (m)
+  const rangeRate = parseEphemeris(substr, 'RR') * KM_TO_M; // (m/s)
 
   return {
+    name: parseEphemerisName(text),
+    date: parseEphemerisDate(substr),
     position: [x, y, z],
     velocity: [vx, vy, vz],
     range,
