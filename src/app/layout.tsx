2e71450848bb8d3682290c0b530f6fa5c3ab44e2
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-06 10:27:57 -0700

    fix: fix path to favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,17 @@ import '~/styles/globals.css';
 import { Metadata } from 'next';
 import SiteLayout from '~/components/layout/site-layout/SiteLayout';
 import Providers from '~/components/layout/site-layout/providers/providers';
-import type { Props } from '~/components/props/Props';
+import type { CommonProps } from '~/components/props/Props';
 
-// export const metadata: Metadata = {
-//   icons: {
-//     icon: '/favicon.ico',
-//   },
-// };
+export const metadata: Metadata = {
+  icons: {
+    icon: '/favicon/darkmode/favicon.svg',
+  },
+};
 
 // Layouts must accept a children prop.
 // This will be populated with nested layouts or pages
-export default function RootLayout({ children }: Props) {
+export default function RootLayout({ children }: CommonProps) {
   return (
     <html lang="en">
       <body suppressHydrationWarning>
