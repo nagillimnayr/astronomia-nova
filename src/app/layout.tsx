40604f6819c9e400c8f458aaf0631813ea4e1737
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-06 00:45:20 -0700

    build: suppress hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,18 @@ import SiteLayout from '~/components/layout/site-layout/SiteLayout';
 import Providers from '~/components/layout/site-layout/providers/providers';
 import type { Props } from '~/components/props/Props';
 
-export const metadata: Metadata = {
-  icons: {
-    icon: '/favicon.ico',
-  },
-};
+// export const metadata: Metadata = {
+//   icons: {
+//     icon: '/favicon.ico',
+//   },
+// };
 
 // Layouts must accept a children prop.
 // This will be populated with nested layouts or pages
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className="bg-background text-foreground">
+      <body suppressHydrationWarning>
         <Providers>
           <SiteLayout>{children}</SiteLayout>
         </Providers>
