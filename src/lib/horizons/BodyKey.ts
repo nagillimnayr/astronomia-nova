43c70d548bdef83004ae69595487a247ed211ffc
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-10 19:05:32 -0700

    feat: add more body ids

--- src/lib/horizons/BodyKey.ts
+++ src/lib/horizons/BodyKey.ts
@@ -4,11 +4,40 @@ export type BodyKey =
   | 'Venus'
   | 'Earth'
   | 'Moon'
+
+  // Martian system
   | 'Mars'
   | 'Phobos'
   | 'Deimos'
+
+  // Jovian system
   | 'Jupiter'
+  | 'Io'
+  | 'Europa'
+  | 'Ganymede'
+  | 'Callisto'
+
+  // Saturnian system
   | 'Saturn'
+  | 'Mimas'
+  | 'Enceladus'
+  | 'Tethys'
+  | 'Dione'
+  | 'Rhea'
+  | 'Titan'
+  | 'Hyperion'
+  | 'Iapetus'
+
+  // Saturnian system
   | 'Uranus'
+  | 'Ariel'
+  | 'Umbriel'
+  | 'Titania'
+  | 'Oberon'
+
+  // Saturnian system
   | 'Neptune'
+  | 'Triton'
+
+  //
   | 'Pluto';
