4eff1c2513b1ea28759e07e792125740f780e9b4
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-15 15:01:59 -0700

    feat: change DIST_MULT to 1e9

--- src/simulation/utils/constants.ts
+++ src/simulation/utils/constants.ts
@@ -5,8 +5,8 @@ export const AU = 1.495978707e11; // Astronomical Unit in m
 export const SOLAR_MASS = 1.989e30; // Solar Mass in kg
 export const DAY = 86400; // Day in seconds
 export const YEAR = 360.25; // Year in Days
-export const DIST_MULT = 1e10; // Distance multiplier (10 unit = 1x10^11m = 100,000,000km)
-// export const DIST_MULT = 1e9; // Distance multiplier (100 unit = 1x10^11m = 100,000,000km)
+// export const DIST_MULT = 1e10; // Distance multiplier (10 unit = 1x10^11m = 100,000,000km)
+export const DIST_MULT = 1e9; // Distance multiplier (100 unit = 1x10^11m = 100,000,000km)
 export const KM_TO_M = 1000; // User enters value in km and this converts it to m
 export const MASS_MULT = 1e24;
 export const EARTH_RADIUS = 6371.0084e3;
