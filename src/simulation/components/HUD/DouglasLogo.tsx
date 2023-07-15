69a31ba542c413c5e9b6c0d2e556bba915807eaa
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-14 23:58:56 -0700

    feat: set DouglasLogo to select-none

--- src/simulation/components/HUD/DouglasLogo.tsx
+++ src/simulation/components/HUD/DouglasLogo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const DouglasLogo = () => {
   return (
-    <div className="absolute bottom-2 left-2 w-[122px]">
+    <div className="absolute bottom-2 left-2 w-[122px] select-none">
       <Image
         src={'/logo/Version3_reverse.png'}
         alt=""
