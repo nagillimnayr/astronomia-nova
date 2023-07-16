7e349917f7ab4ac416eb778426e0629136612d38
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-16 13:25:57 -0700

    feat: add scale transition on hover

--- src/simulation/components/BillboardCircle.tsx
+++ src/simulation/components/BillboardCircle.tsx
@@ -5,10 +5,9 @@ import { Html } from '@react-three/drei';
 export function BillboardCircle() {
   return (
     <object3D>
-      <Html
-        center
-        className="aspect-square w-4 cursor-pointer rounded-full border-2 border-white bg-transparent hover:w-6"
-      />
+      <Html center className="h-fit w-fit bg-transparent">
+        <div className="aspect-square w-4 origin-center cursor-pointer rounded-full border-2 border-white bg-transparent transition-transform hover:scale-150 " />
+      </Html>
     </object3D>
   );
 }
