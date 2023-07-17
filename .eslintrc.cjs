14b0fa1bb09ec5543e34826584faaa9e852f1528
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-16 18:52:55 -0700

    build: add 'capitalized-comments' rule to eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,7 @@ const config = {
   plugins: ['@typescript-eslint'],
   extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   rules: {
+    'capitalized-comments': ['warn', 'always'],
     '@typescript-eslint/consistent-type-imports': [
       'warn',
       {
