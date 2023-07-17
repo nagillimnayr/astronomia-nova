8577433dfebdbcb891ab990a41f626a0657a97a1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-16 23:52:00 -0700

    feat: add click handler to close button

--- src/simulation/components/HUD/DetailsPanel/DetailsPanel.tsx
+++ src/simulation/components/HUD/DetailsPanel/DetailsPanel.tsx
@@ -1,15 +1,26 @@
 import KeplerBody from '@/simulation/classes/KeplerBody';
-import { MutableRefObject } from 'react';
+import { MutableRefObject, useCallback } from 'react';
 import { useSelectionStore } from '@/simulation/state/zustand/selection-store';
 import { useFloating } from '@floating-ui/react';
 
 export function DetailsPanel() {
   const body = useSelectionStore((state) => state.selected);
 
-  if (!body) return null; // If nothing selected, display nothing.
+  const handleCloseClick = useCallback(() => {
+    useSelectionStore.getState().deselect();
+  }, []);
 
+  if (!body) return null; // If nothing selected, display nothing.
   return (
     <div className="absolute right-0 top-0 flex h-80 w-60 flex-col items-center justify-start gap-2 rounded-sm border bg-muted p-2 text-muted-foreground">
+      {/** Close button. */}
+      <button className="absolute right-0 top-0 h-fit w-fit p-1">
+        <span
+          onClick={handleCloseClick}
+          className="icon-[mdi--close-box-outline] pointer-events-auto aspect-square text-xl text-muted-foreground transition-opacity hover:cursor-pointer hover:opacity-75"
+        />
+      </button>
+
       <header className="flex w-full flex-row items-center justify-center">
         <h4 className="text-xl">{body.name}</h4>
       </header>
