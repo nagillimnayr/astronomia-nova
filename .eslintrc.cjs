ca632cb45a1ce6424520450e8532892c735e95b8
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-16 20:47:22 -0700

    build: disable eslint capitalized-comments rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,6 @@ const config = {
   plugins: ['@typescript-eslint'],
   extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    'capitalized-comments': ['warn', 'always'],
     '@typescript-eslint/consistent-type-imports': [
       'warn',
       {
