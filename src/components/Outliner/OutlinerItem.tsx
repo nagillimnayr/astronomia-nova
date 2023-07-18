81691a1870da83740c3a640b5a6aa587996808c7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-17 18:37:34 -0700

    feat: add collapsible animations

--- src/components/Outliner/OutlinerItem.tsx
+++ src/components/Outliner/OutlinerItem.tsx
@@ -20,8 +20,8 @@ const OutlinerItem = ({ body }: OutlinerItemProps) => {
       <span className="m-0 h-fit w-full p-0">
         <span className="m-0 inline-flex h-fit w-full items-center justify-start p-0">
           {/** Trigger to toggle collapsible content open/closed. */}
-          <Collapsible.Trigger className="m-0 flex aspect-square h-full min-h-fit flex-col items-start justify-center overflow-hidden whitespace-nowrap p-0 hover:bg-subtle">
-            <ChevronRightIcon strokeWidth={1} />
+          <Collapsible.Trigger className="m-0 flex aspect-square h-full min-h-fit flex-col items-start justify-center overflow-hidden whitespace-nowrap rounded-full p-0 transition-all hover:bg-subtle  data-[state=open]:rotate-90">
+            <span className="icon-[mdi--chevron-right]" />
           </Collapsible.Trigger>
 
           {/** Button to select body. */}
@@ -36,16 +36,13 @@ const OutlinerItem = ({ body }: OutlinerItemProps) => {
         </span>
 
         {/** Subtree of orbiting bodies. */}
-        <Collapsible.Content className="m-0 flex h-fit max-h-fit min-h-fit w-full flex-col items-stretch justify-start data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down">
+        <Collapsible.Content className="m-0 flex w-full flex-col  overflow-y-hidden transition-all data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down">
           {/** Add some left margin to indent each subtree from its parent, left padding to line up the left border with the list marker. */}
-          <ul className="ml-2 border-l border-gray-600 pl-5 marker:text-gray-600">
+          <ul className="ml-2 border-l border-gray-600 pl-1 marker:text-gray-600">
             {body.orbitingBodies.map((child, index) => {
               // Recursively traverse the tree to construct the tree view.
               return (
-                <li
-                  key={index}
-                  className="bg-subtle/50 w-full list-outside list-disc"
-                >
+                <li key={index} className="w-full list-outside list-none">
                   <OutlinerItem body={child} />
                 </li>
               );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,10 +82,20 @@ export default {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'collapsible-down': {
+          from: { height: '0' },
+          to: { height: '100%' },
+        },
+        'collapsible-up': {
+          from: { height: '100%' },
+          to: { height: '0' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'collapsible-down': 'collapsible-down 0.5s ease-out',
+        'collapsible-up': 'collapsible-up 0.5s ease-out',
       },
       typography: {
         DEFAULT: {
