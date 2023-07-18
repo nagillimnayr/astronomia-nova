7b656703a17bbef938eafa6f26ba146bd1e750a5
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-17 19:05:53 -0700

    feat: update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 };
 export default function NotFound() {
   return (
-    <section className="flex h-full items-center p-16 font-sans dark:bg-gray-900 dark:text-gray-100">
+    <section className="flex h-full w-full items-center p-16 font-sans text-gray-100">
       <div className="container mx-auto my-8 flex flex-col items-center justify-center px-5">
         <div className="max-w-md text-center">
           <h2 className="mb-8 text-9xl font-extrabold dark:text-gray-600">
