9304afb785b4d5b8bd1de371feab084b792448ed
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-18 15:06:12 -0700

    feat: add camera-store

---
+++ src/state/mobx/domain-state/camera-store.ts
@@ -0,0 +1,99 @@
+import { makeAutoObservable } from 'mobx';
+import { type RootStore } from '../root/root-store';
+import type KeplerBody from '@/simulation/classes/KeplerBody';
+import { makeLoggable } from 'mobx-log';
+import { type Object3D, Vector3 } from 'three';
+import { CameraControls } from '@react-three/drei';
+
+// global vector to be reused instead of creating new vector inside of update loop
+const targetWorldPos = new Vector3();
+
+type ViewState = 'space' | 'surface';
+
+const initialState = {
+  controls: null!,
+  focusTarget: null,
+  viewState: 'space' as ViewState,
+};
+
+export class CameraState {
+  private _rootStore: RootStore = null!;
+  private _controls: CameraControls = initialState.controls;
+  private _focusTarget: Object3D | null = initialState.focusTarget;
+  private _viewState: ViewState = initialState.viewState;
+
+  constructor(rootStore: RootStore) {
+    // Set MobX annotations.
+    makeAutoObservable<this, '_rootStore'>(this, {
+      _rootStore: false,
+    });
+    makeLoggable(this);
+
+    this._rootStore = rootStore;
+  }
+
+  get rootStore() {
+    return this._rootStore;
+  }
+
+  get controls() {
+    return this._controls;
+  }
+
+  setControls(controls: CameraControls) {
+    this._controls = controls;
+    this._controls.mouseButtons.right = 8;
+  }
+
+  get focusTarget() {
+    return this._focusTarget;
+  }
+
+  setFocus(target: Object3D) {
+    this._focusTarget = target;
+  }
+
+  get viewState() {
+    return this._viewState;
+  }
+
+  setViewState(viewState: ViewState) {
+    this._viewState = viewState;
+  }
+
+  updateCamera() {
+    switch (this._viewState) {
+      case 'space': {
+        this.updateSpaceView();
+        break;
+      }
+
+      case 'surface': {
+        this.updateSurfaceView();
+        break;
+      }
+    }
+  }
+  private updateSpaceView() {
+    if (!this._controls) {
+      console.error('camera controls are null');
+      return;
+    }
+    if (!this._focusTarget) {
+      return;
+    }
+
+    // Get world position of focus target.
+    this._focusTarget.getWorldPosition(targetWorldPos);
+
+    // Update controls to follow target.
+    this._controls
+      .moveTo(...targetWorldPos.toArray(), false)
+      .catch((reason) => {
+        console.log('error updating camera controls: ', reason);
+      });
+  }
+  private updateSurfaceView() {
+    // Do something...}
+  }
+}
