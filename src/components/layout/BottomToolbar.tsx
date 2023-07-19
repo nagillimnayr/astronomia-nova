afb45c42643daf4bc26742eaa9bbfebc172b8831
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-18 23:58:44 -0700

    feat: add BottomToolbar

---
+++ src/components/layout/BottomToolbar.tsx
@@ -0,0 +1,11 @@
+import { cn } from '@/lib/cn';
+
+const BottomToolbar = () => {
+  return (
+    <div className={cn('grid h-full w-full border bg-muted')}>
+      <>{/* */}</>
+    </div>
+  );
+};
+
+export { BottomToolbar };
