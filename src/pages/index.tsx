b8eca3cb8d511742a028ad2e4b563c432a33fb15
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-21 16:02:31 -0700

    refactor: delete unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,16 +16,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Astronomia Nova</title>
         <meta name="description" content="" />
-        {/* <link
-          rel="icon"
-          type="image/svg+xml"
-          href="/favicon/lightmode/favicon.svg"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon/lightmode/favicon.png"
-        /> */}
       </Head>
 
       <main className="h-full w-full">
