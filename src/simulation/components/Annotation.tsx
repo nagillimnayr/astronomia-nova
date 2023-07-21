12711e2a97089cd05e980ece7907f78915629ae9
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-20 20:26:56 -0700

    feat: remove BBAnchor

--- src/simulation/components/Annotation.tsx
+++ src/simulation/components/Annotation.tsx
@@ -1,6 +1,7 @@
 import { Html, BBAnchor } from '@react-three/drei';
 import { useSnapshot } from 'valtio';
 import { debugState } from '../state/DebugState';
+import { DoubleSide } from 'three';
 
 type AnnotationProps = {
   annotation: string;
@@ -9,15 +10,13 @@ const Annotation = (props: AnnotationProps) => {
   const debugSnap = useSnapshot(debugState);
 
   return debugSnap.annotations ? (
-    <BBAnchor anchor={[0, -1, 0]}>
-      <object3D>
-        <Html center className="pointer-events-none min-h-fit min-w-fit">
-          <div className="flex min-h-fit min-w-fit translate-y-1/2 select-none flex-row  rounded-lg px-2 text-white">
-            {props.annotation}
-          </div>
-        </Html>
-      </object3D>
-    </BBAnchor>
+    <>
+      <Html center className="pointer-events-none min-h-fit min-w-fit">
+        <div className="flex min-h-fit min-w-fit translate-y-1/2 select-none flex-row  rounded-lg px-2 text-white">
+          {props.annotation}
+        </div>
+      </Html>
+    </>
   ) : (
     <></>
   );
