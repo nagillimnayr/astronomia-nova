9cfac88b744176388bd49214d8b35ce9350e59c2
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-22 11:25:27 -0700

    fix: fix build errors

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -34,17 +34,21 @@ export const Button = ({
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
       {...props}
     >
       {label}
       <style jsx>{`
         button {
-          background-color: ${backgroundColor};
+          background-color: ${backgroundColor ?? 'transparent'};
         }
       `}</style>
     </button>
