82c6b3bee1c14e32bd2ecf82e790c98e083985c5
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-22 15:21:00 -0700

    feat: add breakpoint for column size

--- src/components/layout/site-layout/SiteLayout.tsx
+++ src/components/layout/site-layout/SiteLayout.tsx
@@ -18,7 +18,7 @@ const SiteLayout = ({ children }: SiteLayoutProps) => {
         ...fontVariables,
         `relative left-0 top-0 m-0 h-full max-h-full min-h-screen w-screen bg-background p-0 font-sans text-foreground`,
         'grid',
-        'grid-cols-[16rem_1fr_16rem]',
+        'grid-cols-[12rem_1fr_12rem] xl:grid-cols-[16rem_1fr_16rem]',
         'grid-rows-[5rem_1fr_5rem]',
         'items-stretch justify-stretch'
       )}
