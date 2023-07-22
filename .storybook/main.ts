10232d8f6bc1efa6a6aadd7cf4b6fe036d33bad6
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-22 15:53:49 -0700

    build: setup storybook styling addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,10 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {},
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
