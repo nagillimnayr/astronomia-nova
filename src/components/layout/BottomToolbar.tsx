5df6314a5302953494cf8450b0028237d425e133
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-23 16:19:39 -0700

    feat: add toggle bar to bottom of page

--- src/components/layout/BottomToolbar.tsx
+++ src/components/layout/BottomToolbar.tsx
@@ -1,9 +1,16 @@
 import { cn } from '@/lib/cn';
+import { Toolbar } from '@/simulation/components/HUD/Toolbar/Toolbar';
 
 const BottomToolbar = () => {
   return (
-    <div className={cn('grid h-full w-full border bg-muted')}>
-      <>{/* */}</>
+    <div
+      className={cn(
+        'grid h-full w-full grid-cols-[100px_minmax(300px,_1fr)_minmax(300px,_1fr)_minmax(300px,_1fr)_100px] grid-rows-1 place-items-center border bg-muted'
+      )}
+    >
+      <div className="col-span-1 col-end-[-2] flex h-full w-full items-center justify-center">
+        <Toolbar />
+      </div>
     </div>
   );
 };
