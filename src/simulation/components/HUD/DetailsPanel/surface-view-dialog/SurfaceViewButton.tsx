e2282bd810471a47410b3bb7e343977875bf1f34
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-23 07:10:52 -0700

    feat: add click handler

--- src/simulation/components/HUD/DetailsPanel/surface-view-dialog/SurfaceViewButton.tsx
+++ src/simulation/components/HUD/DetailsPanel/surface-view-dialog/SurfaceViewButton.tsx
@@ -2,7 +2,7 @@ import { type ClassNameValue } from 'tailwind-merge';
 import * as AlertDialog from '@radix-ui/react-alert-dialog';
 import { cn } from '@/lib/cn';
 import { SurfaceViewDialog } from './SurfaceViewDialog';
-import { useContext } from 'react';
+import { useCallback, useContext } from 'react';
 import { RootStoreContext } from '@/state/mobx/root/root-store-context';
 import { observer } from 'mobx-react-lite';
 
@@ -13,12 +13,18 @@ type Props = {
 };
 const SurfaceViewButton = observer(
   ({ children, className, defaultOpen }: Props) => {
+    const { uiState, cameraState, surfaceState } = useContext(RootStoreContext);
+    const handleClick = useCallback(() => {
+      cameraState.setFocus(uiState.getSelected()!);
+    }, [cameraState, uiState]);
+
     return (
       <AlertDialog.Root defaultOpen={defaultOpen ?? false}>
         {/** Button to trigger dialog box popup. */}
         <AlertDialog.Trigger asChild>
           {/** Surface view button. */}
           <button
+            onClick={handleClick}
             className={cn(
               'pointer-events-auto flex flex-row items-center justify-center rounded-md border-2 px-2 py-1 font-sans hover:bg-subtle hover:text-subtle-foreground',
               className
