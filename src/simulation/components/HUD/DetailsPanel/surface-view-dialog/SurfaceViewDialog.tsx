bf25f6228286d0528cf6a714bab2cd2a90bc71b6
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-22 23:18:20 -0700

    feat: adjust positioning

--- src/simulation/components/HUD/DetailsPanel/surface-view-dialog/SurfaceViewDialog.tsx
+++ src/simulation/components/HUD/DetailsPanel/surface-view-dialog/SurfaceViewDialog.tsx
@@ -10,16 +10,16 @@ const SurfaceViewDialog = observer(() => {
 
   return (
     <AlertDialog.Portal container={uiState.screenPortal}>
-      <AlertDialog.Overlay className="prose fixed left-1/2 top-1/2 flex h-60 w-96 max-w-[50vw] -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-lg bg-card p-4 font-sans prose-headings:m-0">
-        <AlertDialog.Title className="text-center text-3xl">
+      <AlertDialog.Overlay className="prose fixed left-1/2 top-1/2 flex h-48 w-96 max-w-[50vw] -translate-x-1/2 -translate-y-1/2 flex-col items-center overflow-hidden rounded-lg bg-card p-3 font-sans prose-headings:m-0">
+        <AlertDialog.Title className="m-0 text-center text-3xl">
           Surface View
         </AlertDialog.Title>
         <AlertDialog.Description></AlertDialog.Description>
 
-        <AlertDialog.Content className=" flex h-full w-full flex-col items-center justify-center gap-2 font-sans">
-          <div className="pointer-events-auto my-auto flex min-h-fit min-w-fit flex-row items-center justify-center gap-6 border border-black p-2">
+        <AlertDialog.Content className=" flex h-full w-full flex-col items-center justify-center font-sans">
+          <div className="pointer-events-auto flex min-h-fit min-w-fit flex-row items-center justify-center gap-6">
             {/** Inputs for latitude and longitude. */}
-            <Label>
+            <Label className="mb-2">
               Latitude
               <Input
                 type="number"
@@ -33,10 +33,9 @@ const SurfaceViewDialog = observer(() => {
                 }}
               />
             </Label>
-            <Label>
+            <Label className="mb-2">
               Longitude
               <Input
-                className="select-user pointer-events-auto"
                 type="number"
                 value={surfaceState.longitude}
                 min={0}
@@ -49,7 +48,7 @@ const SurfaceViewDialog = observer(() => {
               />
             </Label>
           </div>
-          <div className="mt-auto flex w-full flex-row items-center justify-center gap-8">
+          <div className=" flex w-full flex-row items-center justify-center gap-8">
             {/** Cancel button. */}
             <AlertDialog.Cancel asChild>
               <button className="col-span-1 col-start-2 h-fit w-16 min-w-fit  place-items-center rounded-md border border-muted px-2 py-1 text-xl hover:bg-muted ">
