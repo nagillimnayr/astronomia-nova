91c9fc457349731c8c5402857903003413e5edb1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-23 09:58:48 -0700

    feat: adjust positioning

--- src/simulation/components/HUD/DetailsPanel/surface-view-dialog/SurfaceViewDialog.tsx
+++ src/simulation/components/HUD/DetailsPanel/surface-view-dialog/SurfaceViewDialog.tsx
@@ -10,15 +10,15 @@ const SurfaceViewDialog = observer(() => {
 
   return (
     <AlertDialog.Portal container={uiState.screenPortal}>
-      <AlertDialog.Overlay className="prose fixed left-1/2 top-1/2 flex h-48 w-96 max-w-[50vw] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-start overflow-hidden rounded-lg bg-card p-3 font-sans prose-headings:m-0">
-        <AlertDialog.Title className="m-0 text-center text-3xl">
+      <AlertDialog.Overlay className="prose fixed left-1/2 top-1/2 flex h-48 w-96 max-w-[50vw] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-start overflow-hidden rounded-lg bg-card px-14 py-3 font-sans prose-headings:m-0">
+        <AlertDialog.Title className="m-0 text-center text-3xl ">
           Surface View
         </AlertDialog.Title>
         <AlertDialog.Description className="my-1"></AlertDialog.Description>
 
-        <AlertDialog.Content className="mt-0 grid h-full w-full grid-cols-[1fr_2fr_2fr_1fr] grid-rows-2 place-items-stretch gap-x-6 gap-y-4 font-sans">
+        <AlertDialog.Content className="mt-0 grid h-full w-full grid-cols-2 grid-rows-2 place-items-stretch gap-x-6 gap-y-4 font-sans">
           {/** Inputs for latitude and longitude. */}
-          <div className="col-span-1 col-start-2 row-span-1 row-start-1">
+          <div className="col-span-1 col-start-1 row-span-1 row-start-1">
             <Label className="mb-2">
               Latitude
               <Input
@@ -35,7 +35,7 @@ const SurfaceViewDialog = observer(() => {
               />
             </Label>
           </div>
-          <div className="col-span-1 col-start-3 row-span-1 row-start-1">
+          <div className="col-span-1 col-start-2 row-span-1 row-start-1">
             <Label className="mb-2">
               Longitude
               <Input
@@ -56,18 +56,18 @@ const SurfaceViewDialog = observer(() => {
           {/** Cancel button. */}
           <AlertDialog.Cancel
             asChild
-            className="col-span-1 col-start-2 row-span-1 row-start-2"
+            className="col-span-1 col-start-1 row-span-1 row-start-2"
           >
-            <button className="col-span-1 col-start-2 h-fit w-full min-w-fit  place-items-center rounded-md border border-muted px-2 py-1 text-xl hover:bg-muted ">
+            <button className=" h-fit w-full min-w-fit  place-items-center rounded-md border border-muted px-2 py-1 text-xl hover:bg-muted ">
               Cancel
             </button>
           </AlertDialog.Cancel>
           {/** Confirm button. */}
           <AlertDialog.Action
             asChild
-            className="col-span-1 col-start-3 row-span-1 row-start-2"
+            className="col-span-1 col-start-2 row-span-1 row-start-2"
           >
-            <button className="col-span-1 col-end-[-2] h-fit  w-full min-w-fit place-items-center rounded-md  border border-muted  px-2 py-1 text-xl hover:bg-muted">
+            <button className="h-fit  w-full min-w-fit place-items-center rounded-md  border border-muted  px-2 py-1 text-xl hover:bg-muted">
               Confirm
             </button>
           </AlertDialog.Action>
