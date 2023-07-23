c4345f71a46ec5397ff5795c05025e0dfadd13b1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-22 23:33:05 -0700

    refactor: remove unused code

--- src/simulation/components/HUD/DetailsPanel/surface-view-dialog/SurfaceViewDialog.tsx
+++ src/simulation/components/HUD/DetailsPanel/surface-view-dialog/SurfaceViewDialog.tsx
@@ -3,20 +3,20 @@ import { Label } from '@/components/ui/label';
 import { RootStoreContext } from '@/state/mobx/root/root-store-context';
 import * as AlertDialog from '@radix-ui/react-alert-dialog';
 import { observer } from 'mobx-react-lite';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 
 const SurfaceViewDialog = observer(() => {
   const { uiState, surfaceState } = useContext(RootStoreContext);
 
   return (
     <AlertDialog.Portal container={uiState.screenPortal}>
-      <AlertDialog.Overlay className="nv-0 prose fixed left-1/2 top-1/2 flex h-48 w-96 max-w-[50vw] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-start overflow-hidden rounded-lg bg-card p-3 font-sans prose-headings:m-0">
+      <AlertDialog.Overlay className="prose fixed left-1/2 top-1/2 flex h-48 w-96 max-w-[50vw] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-start overflow-hidden rounded-lg bg-card p-3 font-sans prose-headings:m-0">
         <AlertDialog.Title className="m-0 text-center text-3xl">
           Surface View
         </AlertDialog.Title>
         <AlertDialog.Description className="my-1"></AlertDialog.Description>
 
-        <AlertDialog.Content className=" mt-0 grid h-full w-full grid-cols-[1fr_2fr_2fr_1fr] grid-rows-2 place-items-stretch gap-x-6 gap-y-4 font-sans">
+        <AlertDialog.Content className="mt-0 grid h-full w-full grid-cols-[1fr_2fr_2fr_1fr] grid-rows-2 place-items-stretch gap-x-6 gap-y-4 font-sans">
           {/** Inputs for latitude and longitude. */}
           <div className="col-span-1 col-start-2 row-span-1 row-start-1">
             <Label className="mb-2">
