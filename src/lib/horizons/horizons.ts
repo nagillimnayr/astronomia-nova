80409fb1ca8f97a41e4a28a3505142246c312686
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-24 16:59:02 -0700

    feat: add idSets

--- src/lib/horizons/horizons.ts
+++ src/lib/horizons/horizons.ts
@@ -1,18 +1,25 @@
 import { getEphemerides } from './getEphemerides';
 import { saveEphemerides } from './saveEphemerides';
 
-async function horizons(code: string) {
-  const ephemerides = await getEphemerides(code);
-  return ephemerides;
-}
+const idSets = [
+  { id: '199', centerId: '500@10' },
+  { id: '299', centerId: '500@10' },
+  { id: '399', centerId: '500@10' },
+  { id: '301', centerId: '500@399' },
+  { id: '499', centerId: '500@10' },
+  { id: '599', centerId: '500@10' },
+  { id: '699', centerId: '500@10' },
+  { id: '799', centerId: '500@10' },
+  { id: '899', centerId: '500@10' },
+];
 
 if (process.argv.length >= 3) {
-  const code = process.argv[2];
+  const id = process.argv[2];
   // const outputFile = process.argv[3];
 
-  if (code) {
+  if (id) {
     try {
-      const ephemerides = await horizons(code);
+      const ephemerides = await getEphemerides(id);
       await saveEphemerides(ephemerides);
       console.log('elements:', ephemerides.elements);
       console.log('vectors:', ephemerides.vectors);
@@ -23,9 +30,8 @@ if (process.argv.length >= 3) {
   }
 } else {
   // console.log('not enough arguments');
-  for (let i = 1; i <= 8; i++) {
-    const code = `${99 + i * 100}`;
-    const ephemerides = await horizons(code);
+  for (const { id, centerId } of idSets) {
+    const ephemerides = await getEphemerides(id, centerId);
     await saveEphemerides(ephemerides);
   }
 }
