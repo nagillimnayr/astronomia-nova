ba3c9a243649d5c677ba40af7bce019b09abb3aa
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-24 12:18:36 -0700

    fix: set button to asChild

--- src/components/layout/site-layout/Navigation/nav-menu/NavMenuLink.tsx
+++ src/components/layout/site-layout/Navigation/nav-menu/NavMenuLink.tsx
@@ -1,19 +1,22 @@
 import { cn } from '@/lib/cn';
 import type { LinkProps } from '@/components/props/Props';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 const NavMenuLink = ({ children, className, href }: LinkProps) => {
   return (
-    <button
+    <Button
+      variant={'outline'}
+      asChild
       className={cn(
-        'pointer-events-auto transition-colors hover:bg-subtle',
+        'pointer-events-auto bg-muted transition-colors hover:bg-subtle',
         className
       )}
     >
       <Link href={href} className={cn('h-full w-full')}>
         {children}
       </Link>
-    </button>
+    </Button>
   );
 };
 
