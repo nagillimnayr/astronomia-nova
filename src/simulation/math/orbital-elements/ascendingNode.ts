0f95f8faa58a76db10c0b817c692e4482eea3fee
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-25 14:39:39 -0700

    feat: add ascending node function

--- src/simulation/math/orbital-elements/ascendingNode.ts
+++ src/simulation/math/orbital-elements/ascendingNode.ts
@@ -1,9 +1,27 @@
 import { Vector3 } from 'three';
+import { radToDeg } from 'three/src/math/MathUtils';
 
+const _orbitPlaneNormal = new Vector3();
 const _ascendingNode = new Vector3();
 export function calculateAscendingNode(
   specificAngularMomentum: Vector3,
   zAxis: Vector3 // Z-Axis of the reference frame.
 ) {
-  return _ascendingNode.crossVectors(specificAngularMomentum, zAxis);
+  _orbitPlaneNormal.copy(specificAngularMomentum).normalize();
+  return _ascendingNode.crossVectors(_orbitPlaneNormal, zAxis).toArray();
+}
+
+export function calculateLongitudeOfAscendingNode(
+  ascendingNode: Vector3,
+  xAxis: Vector3 // X-Axis of the reference frame.
+) {
+  const ascNodeMag = ascendingNode.length();
+  const longitudeOfAscendingNode = radToDeg(
+    Math.acos(ascendingNode.dot(xAxis) / ascNodeMag)
+  );
+
+  if (ascNodeMag < 0) {
+    return 360 - longitudeOfAscendingNode;
+  }
+  return longitudeOfAscendingNode;
 }
