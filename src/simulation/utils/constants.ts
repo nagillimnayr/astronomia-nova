ca3976ac659e7f903d872911f097db7081d5074d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-24 19:54:30 -0700

    feat: increase scale

--- src/simulation/utils/constants.ts
+++ src/simulation/utils/constants.ts
@@ -1,14 +1,14 @@
-import { square, pi } from 'mathjs';
-
 export const GRAV_CONST = 6.674e-11; // Gravitational Constant m^3/kg/s^2;
 export const AU = 1.495978707e11; // Astronomical Unit in m
 export const SOLAR_MASS = 1.989e30; // Solar Mass in kg
 export const DAY = 86400; // Day in seconds
 export const YEAR = 360.25; // Year in Days
 // export const DIST_MULT = 1e10; // Distance multiplier (10 unit = 1x10^11m = 100,000,000km)
-export const DIST_MULT = 1e9; // Distance multiplier (100 unit = 1x10^11m = 100,000,000km)
+// export const DIST_MULT = 1e9; // Distance multiplier (100 unit = 1x10^11m = 100,000,000km)
+export const DIST_MULT = 1e7; // Distance multiplier
 export const KM_TO_M = 1000; // User enters value in km and this converts it to m
 export const MASS_MULT = 1e24;
 export const EARTH_RADIUS = 6371.0084e3;
+export const SUN_RADIUS = 696340e3 / DIST_MULT;
 
-export const PI_SQUARED = square(pi);
+export const PI_SQUARED = Math.pow(Math.PI, 2);
