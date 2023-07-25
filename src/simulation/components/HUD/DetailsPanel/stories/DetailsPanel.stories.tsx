2569eb1006b27d3686f6a824e552167e79939f7d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-24 19:58:26 -0700

    feat: add DetailsPanel stories

---
+++ src/simulation/components/HUD/DetailsPanel/stories/DetailsPanel.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DetailsPanel } from '../DetailsPanel';
+
+const meta: Meta<typeof DetailsPanel> = {
+  title: 'HUD/DetailsPanel',
+  component: DetailsPanel,
+};
+
+export default meta;
+type Story = StoryObj<typeof DetailsPanel>;
+
+export const Primary: Story = {
+  render: () => {
+    return (
+      <div className="font-sans">
+        <DetailsPanel />
+      </div>
+    );
+  },
+};
