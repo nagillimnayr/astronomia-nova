f439f1b6ea985bd68170161cac89af08fbf775a6
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-25 07:41:15 -0700

    docs: add comment about reference frame

--- src/simulation/math/orbital-elements/ascendingNode.ts
+++ src/simulation/math/orbital-elements/ascendingNode.ts
@@ -3,7 +3,7 @@ import { Vector3 } from 'three';
 const _ascendingNode = new Vector3();
 export function calculateAscendingNode(
   specificAngularMomentum: Vector3,
-  zAxis: Vector3
+  zAxis: Vector3 // Z-Axis of the reference frame.
 ) {
   return _ascendingNode.crossVectors(specificAngularMomentum, zAxis);
 }
