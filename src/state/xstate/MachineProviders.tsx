02293af4f6269f9b885954babbc8d794c56ff633
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-26 14:09:22 -0700

    feat: add camera-machine to GlobalStateContext

--- src/state/xstate/MachineProviders.tsx
+++ src/state/xstate/MachineProviders.tsx
@@ -2,25 +2,34 @@ import { createContext, type PropsWithChildren } from 'react';
 import { useInterpret } from '@xstate/react';
 import { toggleMachine } from './toggle-machine/toggle-machine';
 import { type InterpreterFrom } from 'xstate';
+import { cameraMachine } from './camera-machine/camera-machine';
 
 export const GlobalStateContext = createContext({
+  // Toggle machines.
   trajectoryVis: {} as InterpreterFrom<typeof toggleMachine>,
   annotationVis: {} as InterpreterFrom<typeof toggleMachine>,
   markerVis: {} as InterpreterFrom<typeof toggleMachine>,
   velArrowVis: {} as InterpreterFrom<typeof toggleMachine>,
+  cameraService: {} as InterpreterFrom<typeof cameraMachine>,
 });
 
 export const MachineProviders = ({ children }: PropsWithChildren) => {
+  // Toggle machines.
   const trajectoryVis = useInterpret(toggleMachine);
   const annotationVis = useInterpret(toggleMachine);
   const markerVis = useInterpret(toggleMachine);
   const velArrowVis = useInterpret(toggleMachine);
 
+  // Camera machine.
+  const cameraService = useInterpret(cameraMachine);
+
   const globalServices = {
+    // Toggle machines.
     trajectoryVis,
     annotationVis,
     markerVis,
     velArrowVis,
+    cameraService,
   };
   return (
     <GlobalStateContext.Provider value={globalServices}>
