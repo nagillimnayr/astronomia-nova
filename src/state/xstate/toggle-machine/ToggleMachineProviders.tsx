a7ea37f034c99708a44019c25468a5528bc4248c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-25 20:11:28 -0700

    feat: add VelocityArrow visibility state

--- src/state/xstate/toggle-machine/ToggleMachineProviders.tsx
+++ src/state/xstate/toggle-machine/ToggleMachineProviders.tsx
@@ -3,13 +3,22 @@ import { useInterpret } from '@xstate/react';
 import { toggleMachine } from './toggle-machine';
 import { type InterpreterFrom } from 'xstate';
 
+// Trajectory visibility.
 export const TrajectoryVisContext = createContext(
   {} as InterpreterFrom<typeof toggleMachine>
 );
+// Annotation visibility.
 export const AnnotationVisContext = createContext(
   {} as InterpreterFrom<typeof toggleMachine>
 );
+
+// Marker visibility.
 export const MarkerVisContext = createContext(
+  {} as InterpreterFrom<typeof toggleMachine>
+);
+
+// Velocity arrow visibility.
+export const VelArrowContext = createContext(
   {} as InterpreterFrom<typeof toggleMachine>
 );
 
@@ -17,11 +26,14 @@ export const ToggleMachineProviders = ({ children }: PropsWithChildren) => {
   const trajectoryVis = useInterpret(toggleMachine);
   const annotationVis = useInterpret(toggleMachine);
   const markerVis = useInterpret(toggleMachine);
+  const velArrowVis = useInterpret(toggleMachine);
   return (
     <TrajectoryVisContext.Provider value={trajectoryVis}>
       <AnnotationVisContext.Provider value={annotationVis}>
         <MarkerVisContext.Provider value={markerVis}>
-          {children}
+          <VelArrowContext.Provider value={velArrowVis}>
+            {children}
+          </VelArrowContext.Provider>
         </MarkerVisContext.Provider>
       </AnnotationVisContext.Provider>
     </TrajectoryVisContext.Provider>
