8ef890d019f5142dacc6006ffa038c201c0a80a0
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-25 20:23:34 -0700

    refactor: delete unused code

--- src/simulation/components/SolarSystem/SunEarth.tsx
+++ src/simulation/components/SolarSystem/SunEarth.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef } from 'react';
+import React, { useRef } from 'react';
 import Body from '../Body/Body';
 import type KeplerBody from '../../classes/KeplerBody';
 import KeplerTreeContext from '../../context/KeplerTreeContext';
@@ -10,12 +10,8 @@ import {
   EffectComposer,
   Outline,
 } from '@react-three/postprocessing';
-import { useEventListener } from 'usehooks-ts';
 // import { keplerTreeState } from '../../state/keplerTreeState';
 import { Orbit } from '../Orbit/Orbit';
-import { RetrogradeContext } from '../Retrograde/RetrogradeContext';
-import { BodyMesh } from '../Body/BodyMesh';
-import { Vector3 } from 'three';
 
 export type UpdateFn = (deltaTime: number) => void;
 const SunEarth = () => {
@@ -27,41 +23,23 @@ const SunEarth = () => {
   // use ref to store root of tree
   const rootRef = useRef<KeplerBody>(null!);
 
-  const assignAsRoot = (body: KeplerBody) => {
-    if (!body) {
-      return;
-    }
-    // keplerTreeState.setRoot(body);
-  };
-
   return (
-    <KeplerTreeContext.Provider value={assignAsRoot}>
+    <KeplerTreeContext.Provider value={null}>
       <CelestialSphere>
-        <Selection>
-          <EffectComposer autoClear={false} multisampling={8}>
-            <Outline
-              blur
-              edgeStrength={100}
-              visibleEdgeColor={0xffffff}
-              width={1000}
-            />
-          </EffectComposer>
-
-          <Body
-            ref={rootRef}
-            params={{
-              name: 'Sun',
-              mass: SOLAR_MASS,
-              color: 0xfdee00,
-              meanRadius: 1.5,
-              initialPosition: [0, 0, 0],
-              initialVelocity: [0, 0, 0],
-            }}
-            texture={sunTexture}
-          >
-            <Orbit name={'Earth'} texture={earthTexture}></Orbit>
-          </Body>
-        </Selection>
+        <Body
+          ref={rootRef}
+          params={{
+            name: 'Sun',
+            mass: SOLAR_MASS,
+            color: 0xfdee00,
+            meanRadius: 1.5,
+            initialPosition: [0, 0, 0],
+            initialVelocity: [0, 0, 0],
+          }}
+          texture={sunTexture}
+        >
+          <Orbit name={'Earth'} texture={earthTexture}></Orbit>
+        </Body>
       </CelestialSphere>
     </KeplerTreeContext.Provider>
   );
