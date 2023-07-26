8c209d5842975f9de51b91e69e90c6563887763f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-26 14:10:01 -0700

    feat: add camera to Observer

--- src/simulation/components/observer/Observer.tsx
+++ src/simulation/components/observer/Observer.tsx
@@ -1,7 +1,17 @@
+import { PerspectiveCamera, useHelper } from '@react-three/drei';
+import { useRef } from 'react';
+import { CameraHelper, type Camera } from 'three';
+
 const Observer = () => {
+  const cameraRef = useRef<Camera>(null!);
+  useHelper(cameraRef, CameraHelper);
   return (
     <>
-      <object3D></object3D>
+      <object3D>
+        <PerspectiveCamera ref={cameraRef} />
+      </object3D>
     </>
   );
 };
+
+export { Observer };
