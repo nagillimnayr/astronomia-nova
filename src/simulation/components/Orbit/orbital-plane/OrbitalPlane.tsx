10ae7ce6dd9218d695a584670c588e17ad4c844e
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-27 19:25:43 -0700

    feat: add OrbitalPlane

---
+++ src/simulation/components/Orbit/orbital-plane/OrbitalPlane.tsx
@@ -0,0 +1,35 @@
+import KeplerBody from '@/simulation/classes/KeplerBody';
+import { DIST_MULT } from '@/simulation/utils/constants';
+import { Circle } from '@react-three/drei';
+import { MutableRefObject } from 'react';
+
+type Props = {
+  bodyRef: MutableRefObject<KeplerBody>;
+  semiMajorAxis: number;
+  semiMinorAxis: number;
+  linearEccentricity: number;
+};
+const OrbitalPlane = ({
+  bodyRef,
+  semiMajorAxis,
+  semiMinorAxis,
+  linearEccentricity,
+}: Props) => {
+  return (
+    <>
+      {bodyRef.current && bodyRef.current.name === 'Earth' ? (
+        <>
+          <Circle
+            args={[semiMajorAxis / DIST_MULT, 1024]}
+            position={[-linearEccentricity / DIST_MULT, 0, 0]}
+            scale={[1, 1, semiMinorAxis / semiMajorAxis]}
+          >
+            <meshBasicMaterial color={'white'} opacity={0.2} />
+          </Circle>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export { OrbitalPlane };
