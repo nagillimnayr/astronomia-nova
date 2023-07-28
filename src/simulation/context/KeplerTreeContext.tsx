d315b3b1626a19c575548d6bff24979cca1112c3
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-28 00:38:41 -0700

    refactor: change ref

--- src/simulation/context/KeplerTreeContext.tsx
+++ src/simulation/context/KeplerTreeContext.tsx
@@ -1,8 +1,7 @@
 import { type MutableRefObject, createContext } from 'react';
 import type KeplerBody from '../classes/KeplerBody';
 
-const KeplerTreeContext = createContext<MutableRefObject<KeplerBody> | null>(
-  null
-);
+const KeplerTreeContext =
+  createContext<MutableRefObject<KeplerBody | null> | null>(null);
 
 export default KeplerTreeContext;
