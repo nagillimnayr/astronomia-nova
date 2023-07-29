217a4ae4942ebdb66931018e84fd66f0fc459e47
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-28 19:14:38 -0700

    feat: add additional context

--- src/state/xstate/ui-machine/ui-machine.ts
+++ src/state/xstate/ui-machine/ui-machine.ts
@@ -2,20 +2,22 @@ import { assign, createMachine } from 'xstate';
 import { type MutableRefObject } from 'react';
 
 type Context = {
-  screenPortalRef: MutableRefObject<HTMLElement>;
-  camViewPortalRef: MutableRefObject<HTMLElement>;
+  screenPortalRef: MutableRefObject<HTMLDivElement>;
+  camViewPortalRef: MutableRefObject<HTMLDivElement>;
 };
 
 type Events =
   | { type: 'TOGGLE' }
   | {
-      type: 'ASSIGN_SCREEN_PORTAL';
-      screenPortalRef: MutableRefObject<HTMLElement>;
+      type: 'ASSIGN_SCREEN_PORTAL_REF';
+      screenPortalRef: MutableRefObject<HTMLDivElement>;
     }
+  | { type: 'SET_SCREEN_PORTAL'; screenPortal: HTMLDivElement }
   | {
-      type: 'ASSIGN_CAM_VIEW_PORTAL';
-      camViewPortalRef: MutableRefObject<HTMLElement>;
-    };
+      type: 'ASSIGN_CAM_VIEW_PORTAL_REF';
+      camViewPortalRef: MutableRefObject<HTMLDivElement>;
+    }
+  | { type: 'SET_CAM_VIEW_PORTAL'; camViewPortal: HTMLDivElement };
 
 // Reusable sub-state.
 const toggleStates = {
@@ -46,11 +48,31 @@ export const uiMachine = createMachine(
     }),
     // Context assignment events.
     on: {
-      ASSIGN_SCREEN_PORTAL: {
-        actions: 'assignScreenPortalRef',
+      ASSIGN_SCREEN_PORTAL_REF: {
+        actions: [
+          'assignScreenPortalRef',
+          () => console.log('Assigning screen portal ref'),
+        ],
       },
-      ASSIGN_CAM_VIEW_PORTAL: {
-        actions: 'assignCamViewPortalRef',
+      SET_SCREEN_PORTAL: {
+        actions: [
+          (context, event) => {
+            context.screenPortalRef.current = event.screenPortal;
+          },
+        ],
+      },
+      ASSIGN_CAM_VIEW_PORTAL_REF: {
+        actions: [
+          'assignCamViewPortalRef',
+          () => console.log('Assigning cam view portal ref'),
+        ],
+      },
+      SET_CAM_VIEW_PORTAL: {
+        actions: [
+          (context, event) => {
+            context.camViewPortalRef.current = event.camViewPortal;
+          },
+        ],
       },
     },
 
@@ -58,6 +80,17 @@ export const uiMachine = createMachine(
     // Parallel states:
     states: {
       idle: {},
+      camView: {
+        initial: 'pending',
+        states: {
+          pending: {
+            on: {
+              SET_CAM_VIEW_PORTAL: { target: 'assigned' },
+            },
+          },
+          assigned: {},
+        },
+      },
       // outliner: {
       //   ...toggleStates,
       // },
