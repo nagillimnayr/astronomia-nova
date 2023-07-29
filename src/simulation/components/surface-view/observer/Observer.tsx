7934643fa355d2113d06d5278246c6839ecad166
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-28 19:13:58 -0700

    feat: increase fov

--- src/simulation/components/surface-view/observer/Observer.tsx
+++ src/simulation/components/surface-view/observer/Observer.tsx
@@ -10,20 +10,23 @@ import { degToRad } from 'three/src/math/MathUtils';
 const Observer = () => {
   const { cameraService } = useContext(GlobalStateContext);
   const cameraRef = useRef<PerspectiveCamera>(null!);
-  const cameraHelperRef = useHelper(cameraRef, CameraHelper);
+  useHelper(cameraRef, CameraHelper);
   return (
     <>
       <object3D>
-        <object3D rotation={[degToRad(0), 0, degToRad(90)]}>
+        <object3D rotation={[0, 0, -degToRad(90)]}>
           <PerspectiveCam
             ref={(cam) => {
+              if (!cam) return;
               const camera = cam as PerspectiveCamera;
               cameraRef.current = camera;
               // Assign camera to state context.
+              console.log('surfaceCam', camera);
               cameraService.send({ type: 'ASSIGN_SURFACE_CAMERA', camera });
             }}
-            near={1e-8}
-            fov={5}
+            near={1e-5}
+            far={1e5}
+            fov={15}
           />
         </object3D>
         <axesHelper args={[3e-2]} />
