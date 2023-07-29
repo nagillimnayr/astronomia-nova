f8ada793143c98332ef381785f9877644fd936f1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-28 21:31:09 -0700

    feat: add CameraController to Scene

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -11,19 +11,27 @@ import { LoadingFallback } from '../LoadingFallback';
 
 import { GlobalStateContext } from '@/state/xstate/MachineProviders';
 import { useFrame, useThree } from '@react-three/fiber';
+import { PointerLockControls } from '@react-three/drei';
+import { CameraController } from '@/simulation/components/camera-controller/CameraController';
+import { useKeyPressed } from '@react-hooks-library/core';
 
 const Scene = ({ children }: PropsWithChildren) => {
   const { cameraService, uiService } = useContext(GlobalStateContext);
   const getThree = useThree(({ get }) => get);
-  // useFrame(({ gl, camera, scene }) => {
-  //   gl.render(scene, camera);
-  // });
+
   useEffect(() => {
-    const scene = getThree().scene;
-    cameraService.send({ type: 'ASSIGN_SCENE', scene });
+    // const scene = getThree().scene;
+    cameraService.send({ type: 'ASSIGN_THREE', get: getThree });
   }, [cameraService, getThree]);
+
+  useKeyPressed(' ', () => {
+    const { camera, controls } = getThree();
+    console.log('camera:', camera);
+    console.log('controls:', controls);
+  });
   return (
     <>
+      <CameraController />
       <Simulation>{children}</Simulation>
     </>
   );
