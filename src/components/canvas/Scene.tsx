5f49ef9054db69413aa7fc3d91a5e658de2605e6
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-28 19:13:03 -0700

    feat: assign scene to state context

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,16 +1,31 @@
 import Simulation from '@/simulation/components/Simulation';
 
-import { type PropsWithChildren, Suspense, useContext, useRef } from 'react';
+import {
+  type PropsWithChildren,
+  Suspense,
+  useContext,
+  useRef,
+  useEffect,
+} from 'react';
 import { LoadingFallback } from '../LoadingFallback';
 
 import { GlobalStateContext } from '@/state/xstate/MachineProviders';
+import { useFrame, useThree } from '@react-three/fiber';
 
 const Scene = ({ children }: PropsWithChildren) => {
   const { cameraService, uiService } = useContext(GlobalStateContext);
+  const getThree = useThree(({ get }) => get);
+  // useFrame(({ gl, camera, scene }) => {
+  //   gl.render(scene, camera);
+  // });
+  useEffect(() => {
+    const scene = getThree().scene;
+    cameraService.send({ type: 'ASSIGN_SCENE', scene });
+  }, [cameraService, getThree]);
   return (
-    <Suspense fallback={<LoadingFallback />}>
+    <>
       <Simulation>{children}</Simulation>
-    </Suspense>
+    </>
   );
 };
 
