cc30de68e31b6b7a6c1737b412ca7e48f7b404be
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-30 13:42:58 -0700

    refactor: move constants into lib folder

---
+++ src/lib/utils/constants.ts
@@ -0,0 +1,13 @@
+export const GRAV_CONST = 6.674e-11; // Gravitational Constant m^3/kg/s^2;
+export const AU = 1.495978707e11; // Astronomical Unit in m
+// export const SOLAR_MASS = 1.989e30; // Solar Mass in kg
+export const SOLAR_MASS = 1988500e24; // Solar Mass in kg
+export const DAY = 86400; // Day in seconds
+export const YEAR = 360.25; // Year in Days
+
+export const KM_TO_M = 1000; // User enters value in km and this converts it to m
+export const MASS_MULT = 1e24;
+export const EARTH_RADIUS = 6371.0084e3;
+export const SUN_RADIUS = 696340e3;
+
+export const PI_SQUARED = Math.pow(Math.PI, 2);
