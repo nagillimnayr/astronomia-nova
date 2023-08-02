cde8e0ddff7e35028688f30918b5dcbda49921ba
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-01 17:34:06 -0700

    feat: add events

--- src/state/xstate/toggle-machine/toggle-machine.ts
+++ src/state/xstate/toggle-machine/toggle-machine.ts
@@ -1,20 +1,24 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 import { createMachine } from 'xstate';
 
+type Context = {};
+
+type Events = { type: 'TOGGLE' } | { type: 'ENABLE' } | { type: 'DISABLE' };
+
 export const toggleMachine = createMachine({
   tsTypes: {} as import('./toggle-machine.typegen').Typegen0,
   schema: {
-    context: {},
-    events: {} as { type: 'TOGGLE' },
+    context: {} as Context,
+    events: {} as Events,
   },
   id: 'toggle',
   initial: 'active',
   states: {
     active: {
-      on: { TOGGLE: 'inactive' },
+      on: { TOGGLE: 'inactive', DISABLE: 'inactive' },
     },
     inactive: {
-      on: { TOGGLE: 'active' },
+      on: { TOGGLE: 'active', ENABLE: 'active' },
     },
   },
   predictableActionArguments: true,
