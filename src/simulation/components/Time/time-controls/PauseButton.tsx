757951f216967e4c89ab70d024bec11202b13e37
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-02 18:00:07 -0700

    refactor: use xState

--- src/simulation/components/Time/time-controls/PauseButton.tsx
+++ src/simulation/components/Time/time-controls/PauseButton.tsx
@@ -12,30 +12,23 @@ import IconButton from '@/components/IconButton';
 import Icon from '@mdi/react';
 import { mdiPlay, mdiPause } from '@mdi/js';
 import { useTimeStore } from '@/simulation/state/zustand/time-store';
+import { GlobalStateContext } from '@/state/xstate/MachineProviders';
+import { timeMachine } from '../../../../state/xstate/time-machine/time-machine';
+import { useSelector } from '@xstate/react';
 
 const PauseButton = () => {
-  // const [isPaused, setPaused] = useState<boolean>(true);
-
-  // get isPaused from external store and subscribe to changes
-  const isPaused = useTimeStore((state) => state.isPaused);
-  const pause = useTimeStore((state) => state.pause);
-  const unpause = useTimeStore((state) => state.unpause);
-
-  // const pause = useCallback(() => {
-  //   timeState.pause();
-  //   // setPaused(true);
-  // }, []);
-  // const unpause = useCallback(() => {
-  //   timeState.unpause();
-  //   // setPaused(false);
-  // }, []);
+  const { rootActor } = useContext(GlobalStateContext);
+  const timeActor = useSelector(rootActor, ({ context }) => context.timeActor);
+  // Check whether paused or not.
+  const isPaused = useSelector(timeActor, (state) => state.matches('paused'));
 
   return (
     <button
       className="bg-translucent btn-icon box-content p-1 "
       onClick={(e) => {
         e.stopPropagation();
-        isPaused ? unpause() : pause();
+
+        timeActor.send(isPaused ? 'UNPAUSE' : 'PAUSE');
       }}
     >
       <Icon path={isPaused ? mdiPlay : mdiPause} size={1.5} />
