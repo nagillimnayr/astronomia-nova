d6cba859ceb1eb05f69cdec735ef0911640fda99
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-03 10:00:39 -0700

    fix: fix inconsistencies with styling

--- src/components/gui/Slider.tsx
+++ src/components/gui/Slider.tsx
@@ -7,7 +7,7 @@ export const Slider = ({ className, ...props }: Props) => {
   return (
     <RadixSlider.Root
       className={cn(
-        'relative flex h-5 w-[200px] touch-none select-none items-center ',
+        'relative flex h-5 w-[200px] touch-none select-none items-center hover:cursor-pointer ',
         className
       )}
       {...props}
@@ -17,7 +17,7 @@ export const Slider = ({ className, ...props }: Props) => {
       </RadixSlider.Track>
       <RadixSlider.Thumb
         className={cn(
-          'focus-outline pointer-events-auto block aspect-square  w-4 rounded-full bg-white shadow-lg  shadow-black transition-all duration-300  hover:cursor-grab hover:bg-black hover:outline hover:outline-1 hover:outline-white focus:bg-black focus:shadow-lg focus:shadow-black focus:outline-1 focus:outline-white'
+          'focus-outline pointer-events-auto block aspect-square  w-4 rounded-full bg-white shadow-lg  shadow-black transition-all duration-500 hover:bg-black hover:shadow-lg hover:outline hover:outline-1 hover:outline-white focus:bg-black focus:shadow-lg focus:shadow-black focus:outline focus:outline-1 focus:outline-white'
         )}
       />
     </RadixSlider.Root>
