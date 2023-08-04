474b67bbb6cb0e68bdd922e029fc8c44dbc19235
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-04 15:09:51 -0700

    feat: add mapMachine

---
+++ src/state/xstate/map-machine/map-machine.ts
@@ -0,0 +1,84 @@
+import KeplerBody from '@/simulation/classes/kepler-body';
+import { KeplerOrbit } from '@/simulation/classes/kepler-orbit';
+import { assign, createMachine, log } from 'xstate';
+
+type Context = {
+  bodyMap: Map<string, KeplerBody>;
+  orbitMap: Map<string, KeplerOrbit>;
+};
+
+type Events =
+  | { type: 'ADD_BODY'; body: KeplerBody }
+  | { type: 'ADD_ORBIT'; orbit: KeplerOrbit }
+  | { type: 'REMOVE_BODY'; name: string }
+  | { type: 'REMOVE_ORBIT'; name: string }
+  | { type: 'REMOVE'; name: string };
+
+export const mapMachine = createMachine(
+  {
+    predictableActionArguments: true,
+    tsTypes: {} as import('./map-machine.typegen').Typegen0,
+    schema: {
+      context: {} as Context,
+      events: {} as Events,
+    },
+    id: 'map-machine',
+
+    context: () => ({
+      bodyMap: new Map<string, KeplerBody>(),
+      orbitMap: new Map<string, KeplerOrbit>(),
+    }),
+
+    on: {
+      ADD_BODY: {
+        cond: 'validateAddBody',
+        actions: ['addBody'],
+      },
+      ADD_ORBIT: {
+        cond: 'validateAddOrbit',
+        actions: ['addOrbit'],
+      },
+      REMOVE_BODY: {
+        cond: 'validateRemoveBody',
+        actions: ['removeBody'],
+      },
+      REMOVE_ORBIT: {
+        cond: 'validateRemoveOrbit',
+        actions: ['removeOrbit'],
+      },
+      REMOVE: {
+        cond: 'validateRemoveBody',
+        actions: ['removeBody', 'removeOrbit'],
+      },
+    },
+  },
+  {
+    actions: {
+      addBody: assign({
+        bodyMap: ({ bodyMap }, { body }) => bodyMap.set(body.name, body),
+      }),
+      addOrbit: assign({
+        orbitMap: ({ orbitMap }, { orbit }) => orbitMap.set(orbit.name, orbit),
+      }),
+      removeBody: assign({
+        bodyMap: ({ bodyMap }, { name }) => {
+          bodyMap.delete(name);
+          return bodyMap;
+        },
+      }),
+      removeOrbit: assign({
+        orbitMap: ({ orbitMap }, { name }) => {
+          orbitMap.delete(name);
+          return orbitMap;
+        },
+      }),
+    },
+    guards: {
+      validateAddBody: (context, event) => context.bodyMap.has(event.body.name),
+      validateAddOrbit: (context, event) =>
+        context.orbitMap.has(event.orbit.name),
+      validateRemoveBody: (context, event) => context.bodyMap.has(event.name),
+      validateRemoveOrbit: (context, event) => context.orbitMap.has(event.name),
+    },
+  }
+);
