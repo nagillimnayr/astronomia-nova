3bd3d009a87f56a6878d927382c308ce3b230aa4
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-06 13:26:25 -0700

    feat: remove space button

--- src/components/layout/BottomToolbar.tsx
+++ src/components/layout/BottomToolbar.tsx
@@ -2,7 +2,6 @@ import { cn } from '@/lib/cn';
 import { ToggleBar } from '@/simulation/components/HUD/ToggleBar/ToggleBar';
 import { OpacitySliders } from '@/simulation/components/HUD/opacity/OpacitySliders';
 import { MachineContext } from '@/state/xstate/MachineProviders';
-import { useContext } from 'react';
 
 const BottomToolbar = () => {
   const { uiActor, cameraActor, selectionActor } = MachineContext.useSelector(
@@ -15,17 +14,6 @@ const BottomToolbar = () => {
         'grid h-full w-full grid-cols-[100px_minmax(300px,_1fr)_minmax(300px,_1fr)_minmax(300px,_1fr)_100px] grid-rows-1 place-items-center border bg-muted'
       )}
     >
-      <div className="col-span-1 col-start-2 flex h-full w-full items-center justify-center">
-        <button
-          className="rounded-md border px-2 py-1 text-lg transition-colors hover:bg-subtle"
-          onClick={() => {
-            cameraActor.send('TO_SPACE');
-          }}
-        >
-          Space
-        </button>
-      </div>
-
       <div className="col-span-1 col-start-3 flex h-full w-full items-center justify-center">
         <OpacitySliders />
       </div>
