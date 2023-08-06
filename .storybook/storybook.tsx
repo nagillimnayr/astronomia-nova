dd6d9365abd81639bac45ba799f62064833da6a7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-05 19:43:57 -0700

    fix: fix build errors

--- .storybook/storybook.tsx
+++ .storybook/storybook.tsx
@@ -4,6 +4,7 @@ import { PropsWithChildren } from 'react';
 import * as fonts from '../src/lib/fonts';
 import { RootStoreContext } from '@/state/mobx/root/root-store-context';
 import { RootStoreProvider } from '@/components/layout/site-layout/providers/root-store-provider';
+import { MachineContext } from '@/state/xstate/MachineProviders';
 const fontVariables = [
   fonts.atomicAge.variable,
   fonts.orbitron.variable,
@@ -11,14 +12,11 @@ const fontVariables = [
 ];
 
 const Storybook = ({ children }: PropsWithChildren) => {
-  const { uiState } = React.useContext(RootStoreContext);
+  const { uiActor } = MachineContext.useSelector(({ context }) => context);
+  const screenPortalRef = React.useRef<HTMLDivElement>(null!);
+  uiActor.send({ type: 'ASSIGN_SCREEN_PORTAL_REF', screenPortalRef });
   return (
-    <div
-      className={cn(...fontVariables)}
-      ref={(div) => {
-        uiState.setScreenPortal(div);
-      }}
-    >
+    <div className={cn(...fontVariables)} ref={screenPortalRef}>
       <div className="font-sans">{children}</div>
     </div>
   );
