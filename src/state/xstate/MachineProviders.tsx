5fbe9185563bb014e12c868e635239ae807f6d8f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-07 11:16:23 -0700

    refactor: add Initializer

--- src/state/xstate/MachineProviders.tsx
+++ src/state/xstate/MachineProviders.tsx
@@ -1,38 +1,25 @@
-import { createContext, type PropsWithChildren } from 'react';
-import { createActorContext, useInterpret } from '@xstate/react';
-import { type InterpreterFrom } from 'xstate';
-// import { cameraMachine } from './camera-machine/camera-machine';
-// import { selectionMachine } from './selection-machine/selection-machine';
-// import { uiMachine } from './ui-machine/ui-machine';
+import { useRef, type PropsWithChildren } from 'react';
+import { createActorContext } from '@xstate/react';
 
-// import { visibilityMachine } from './visibility-machine/visibility-machine';
 import { rootMachine } from './root-machine/root-machine';
 
 export const MachineContext = createActorContext(rootMachine, {
   id: 'rootActor',
 });
 
-// export const GlobalStateContext = createContext({
-//   // Root machine:
-//   rootActor: {} as InterpreterFrom<typeof rootMachine>,
-
-//   // Visibility machine:
-//   visibilityService: {} as InterpreterFrom<typeof visibilityMachine>,
-
-//   // UI machine:
-//   uiService: {} as InterpreterFrom<typeof uiMachine>,
-
-//   // Camera machine:
-//   cameraService: {} as InterpreterFrom<typeof cameraMachine>,
-
-//   // Selection machine:
-//   selectionService: {} as InterpreterFrom<typeof selectionMachine>,
-// });
-
 export const MachineProviders = ({ children }: PropsWithChildren) => {
   return (
-    // <GlobalStateContext.Provider value={globalServices}>
-    <MachineContext.Provider>{children}</MachineContext.Provider>
-    // </GlobalStateContext.Provider>
+    <MachineContext.Provider>
+      <Initializer />
+      {children}
+    </MachineContext.Provider>
   );
+};
+
+const Initializer = () => {
+  const { uiActor } = MachineContext.useSelector(({ context }) => context);
+  // Create refs and assign them to global state.
+  const screenPortalRef = useRef<HTMLDivElement>(null!);
+  uiActor.send({ type: 'ASSIGN_SCREEN_PORTAL_REF', screenPortalRef });
+  return <></>;
 };
