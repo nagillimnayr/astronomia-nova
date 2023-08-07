e70400483e8f379f3a06480909205edee3316993
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-07 11:43:40 -0700

    refactor: wrap in StrictMode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,17 @@ import '@/styles/globals.css';
 import Providers from '@/components/layout/site-layout/providers/providers';
 import SiteLayout from '@/components/layout/site-layout/SiteLayout';
 import { trpc } from '@/lib/trpc/trpc';
+import { StrictMode } from 'react';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Providers>
-      <SiteLayout>
-        <Component {...pageProps} />
-      </SiteLayout>
-    </Providers>
+    <StrictMode>
+      <Providers>
+        <SiteLayout>
+          <Component {...pageProps} />
+        </SiteLayout>
+      </Providers>
+    </StrictMode>
   );
 };
 
