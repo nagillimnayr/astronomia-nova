e44b1cd3b2e16802ac2eace822a2aac36d77b7fa
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-08 15:24:17 -0700

    fix: remove screenPortalRef

--- .storybook/storybook.tsx
+++ .storybook/storybook.tsx
@@ -12,19 +12,9 @@ const fontVariables = [
 ];
 
 const Storybook = ({ children }: PropsWithChildren) => {
-  const { uiActor } = MachineContext.useSelector(({ context }) => context);
-
   return (
     <div className={cn(...fontVariables)}>
-      <div
-        ref={(screenPortal) => {
-          if (!screenPortal) return;
-          uiActor.send({ type: 'SET_SCREEN_PORTAL', screenPortal });
-        }}
-        className="h-screen w-screen font-sans"
-      >
-        {children}
-      </div>
+      <div className="h-screen w-screen font-sans">{children}</div>
     </div>
   );
 };
