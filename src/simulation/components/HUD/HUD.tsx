6666fbfcabf363d660d041ebda14b1266e21ccdf
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-07 22:48:58 -0700

    fix: disable text selection for HUD

--- src/simulation/components/HUD/HUD.tsx
+++ src/simulation/components/HUD/HUD.tsx
@@ -5,6 +5,8 @@ import DouglasLogo from './DouglasLogo';
 import { CamViewPortal } from './CamView/CamViewPortal';
 import Outliner from './Outliner/Outliner';
 import { VRButton } from '@react-three/xr';
+import { SettingsButton } from './settings/SettingsButton';
+import { SettingsMenu } from './settings/SettingsMenu';
 
 type Props = {
   className: string;
@@ -14,11 +16,25 @@ export const HUD = ({ className }: Props) => {
     <>
       <div
         className={cn(
-          'pointer-events-none absolute z-[3] h-full min-h-full w-full min-w-full ',
+          'pointer-events-none absolute z-[3] h-full min-h-full w-full min-w-full select-none ',
           'grid grid-cols-[minmax(0,_20px)_repeat(5,_minmax(0,_1fr))_minmax(0,_20px)] grid-rows-[minmax(0,_20px)_repeat(5,_minmax(0,_1fr))_minmax(0,_20px)]',
           className
         )}
       >
+        {/** Settings Button */}
+        <div className="relative z-40 col-end-[-2] row-start-2 h-full w-full">
+          <div className="pointer-events-auto absolute right-0 top-0 m-0 inline-flex h-fit w-fit items-center justify-center p-0">
+            {/* <SettingsButton /> */}
+            <SettingsMenu />
+          </div>
+        </div>
+
+        {/** Settings Menu */}
+        {/* <div className="relative z-50 col-end-[-2] row-start-2 h-full w-full">
+          <div className="pointer-events-auto absolute right-1/2 top-0 m-0 inline-flex h-fit w-fit translate-x-1/2 items-center justify-center p-0"></div>
+          <SettingsMenu />
+        </div> */}
+
         {/** Outliner. */}
         <div className={'relative col-start-2 row-start-[3] row-end-[-3]'}>
           <div className="pointer-events-auto relative left-1/2 top-0 h-fit min-h-fit w-52 min-w-fit -translate-x-1/2 rounded-lg border-2 bg-muted p-3">
@@ -53,7 +69,7 @@ export const HUD = ({ className }: Props) => {
         <div className={'relative col-end-[-2] row-end-[-2]'}>
           <div className="pointer-events-auto absolute bottom-0 right-0 h-fit w-fit">
             <VRButton
-              className="rounded-lg border-2 border-white p-2 transition-all hover:bg-subtle hover:bg-opacity-20"
+              className="select-none rounded-lg border-2 border-white p-2 transition-all hover:bg-subtle hover:bg-opacity-20"
               style={{}}
             />
           </div>
