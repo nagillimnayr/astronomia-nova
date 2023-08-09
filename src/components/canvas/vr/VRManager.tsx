372c0dbf37abc72ea39f1d9382e8fcbd3f5cac05
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-09 14:56:12 -0700

    feat: add VR Manager

---
+++ src/components/canvas/vr/VRManager.tsx
@@ -0,0 +1,24 @@
+import { useController, useXR, useXREvent } from '@react-three/xr';
+import { PlayerControls } from './PlayerControls';
+
+export const VRManager = () => {
+  const { player, isPresenting, session, controllers } = useXR();
+  const rightController = useController('right');
+
+  useXREvent(
+    'squeeze',
+    (event) => {
+      // player.position.set(-1e4, 0, 0);
+      console.log('player position:', player.position.toArray());
+      console.log('player', player);
+      console.log('event', event);
+    },
+    { handedness: 'right' }
+  );
+
+  return (
+    <>
+      <PlayerControls />
+    </>
+  );
+};
