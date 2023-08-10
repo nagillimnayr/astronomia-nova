cb0b5d170993dfb16715a2be2afe3b370532f6cb
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-09 17:11:36 -0700

    feat: assign session on session start

--- src/components/canvas/vr/VRCanvas.tsx
+++ src/components/canvas/vr/VRCanvas.tsx
@@ -2,13 +2,23 @@ import { Canvas } from '@react-three/fiber';
 import { Controllers, VRButton, XR } from '@react-three/xr';
 import { VRScene } from './VRScene';
 import { CameraControls } from '@react-three/drei';
+import { FAR, NEAR } from '../scene-constants';
+import { MachineContext } from '@/state/xstate/MachineProviders';
 
 export const VRCanvas = () => {
+  const { vrActor } = MachineContext.useSelector(({ context }) => context);
   return (
     <>
       <div className="relative z-10 h-full w-full ">
         <Canvas>
-          <XR>
+          <XR
+            onSessionStart={(event) => {
+              const session = event.target;
+
+              // Assign session object to external state machine and start session state.
+              vrActor.send({ type: 'START_SESSION', session });
+            }}
+          >
             <Controllers />
             <VRScene />
             <CameraControls makeDefault />
