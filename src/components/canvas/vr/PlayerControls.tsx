4882f021a0868a46da2addb9b20b59b8d2e676f5
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-09 17:11:48 -0700

    feat: assign player and controllers to vrMachine

--- src/components/canvas/vr/PlayerControls.tsx
+++ src/components/canvas/vr/PlayerControls.tsx
@@ -1,16 +1,44 @@
-import { useKeyPressed } from '@react-hooks-library/core';
+import { MachineContext } from '@/state/xstate/MachineProviders';
+import { useKeyDown, useKeyPressed } from '@react-hooks-library/core';
 import { useThree } from '@react-three/fiber';
 import { useController, useXR, useXREvent } from '@react-three/xr';
+import { useEffect } from 'react';
+import { Vector3 } from 'three';
 import { degToRad } from 'three/src/math/MathUtils';
 
+const _fwd = new Vector3();
 const moveSpeed = 0.05;
-const rotateSpeed = 0.05;
+const rotateSpeed = 1;
 
 export const PlayerControls = () => {
+  const { vrActor } = MachineContext.useSelector(({ context }) => context);
   const { player, controllers, isPresenting, session } = useXR();
   const leftController = useController('left');
   const rightController = useController('right');
   // rightController?.inputSource.gamepad?.mapping.
+
+  useEffect(() => {
+    if (
+      !player ||
+      !session ||
+      !isPresenting ||
+      !leftController ||
+      !rightController
+    )
+      return;
+    console.log('initializing player controls!');
+    // Assign player.
+    vrActor.send({ type: 'ASSIGN_PLAYER', player });
+    // Assign controllers.
+    vrActor.send({
+      type: 'ASSIGN_LEFT_CONTROLLER',
+      controller: leftController,
+    });
+    vrActor.send({
+      type: 'ASSIGN_RIGHT_CONTROLLER',
+      controller: rightController,
+    });
+  }, [isPresenting, leftController, player, rightController, session, vrActor]);
 
   const getThree = useThree(({ get }) => get);
 
@@ -26,40 +54,40 @@ export const PlayerControls = () => {
   });
   useKeyPressed('w', () => {
     if (!isPresenting) return;
-    player.position.z -= moveSpeed;
+    player.translateZ(-moveSpeed);
   });
 
   useKeyPressed('a', () => {
     if (!isPresenting) return;
-    player.position.x -= moveSpeed;
+    player.translateX(-moveSpeed);
   });
 
   useKeyPressed('s', () => {
     if (!isPresenting) return;
-    player.position.z += moveSpeed;
+    player.translateZ(moveSpeed);
   });
 
   useKeyPressed('d', () => {
     if (!isPresenting) return;
-    player.position.x += moveSpeed;
+    player.translateX(moveSpeed);
   });
 
-  useKeyPressed('ArrowUp', () => {
+  useKeyDown('ArrowUp', () => {
     if (!isPresenting) return;
     player.rotateX(-degToRad(rotateSpeed));
   });
 
-  useKeyPressed('ArrowLeft', () => {
+  useKeyDown('ArrowLeft', () => {
     if (!isPresenting) return;
     player.rotateY(degToRad(rotateSpeed));
   });
 
-  useKeyPressed('ArrowRight', () => {
+  useKeyDown('ArrowRight', () => {
     if (!isPresenting) return;
     player.rotateY(-degToRad(rotateSpeed));
   });
 
-  useKeyPressed('ArrowDown', () => {
+  useKeyDown('ArrowDown', () => {
     if (!isPresenting) return;
     player.rotateX(degToRad(rotateSpeed));
   });
