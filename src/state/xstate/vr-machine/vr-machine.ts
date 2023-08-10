8c54a03473f06aab6394d81913f050e1bd291921
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-10 16:49:02 -0700

    feat: add initialization actions

--- src/state/xstate/vr-machine/vr-machine.ts
+++ src/state/xstate/vr-machine/vr-machine.ts
@@ -38,19 +38,19 @@ export const vrMachine = createMachine(
         cond: (context, event) => {
           return context.player !== event.player;
         },
-        actions: ['logEvent', 'assignPlayer'],
+        actions: ['logEvent', 'assignPlayer', 'initializePlayer'],
       },
       ASSIGN_LEFT_CONTROLLER: {
         cond: (context, event) => {
           return context.leftController !== event.controller;
         },
-        actions: ['logEvent', 'assignLeftController'],
+        actions: ['logEvent', 'assignLeftController', 'initializeController'],
       },
       ASSIGN_RIGHT_CONTROLLER: {
         cond: (context, event) => {
           return context.rightController !== event.controller;
         },
-        actions: ['logEvent', 'assignRightController'],
+        actions: ['logEvent', 'assignRightController', 'initializeController'],
       },
     },
 
@@ -97,8 +97,14 @@ export const vrMachine = createMachine(
       initializeSession: (context, event) => {
         const { session } = context;
         if (!session) throw new Error('Error! XRSession is null');
-
+        console.log('session input sources:', session.inputSources);
+        session.addEventListener('select', (event) => console.log(event));
+        session.addEventListener('selectstart', (event) => console.log(event));
+        session.addEventListener('selectend', (event) => console.log(event));
         try {
+          console.log('Initializing XRSession!');
+          console.log('near:', session.renderState.depthNear);
+          console.log('far:', session.renderState.depthFar);
           // Initialize the near and far clip planes.
           void session.updateRenderState({
             depthNear: NEAR,
@@ -106,6 +112,29 @@ export const vrMachine = createMachine(
           });
         } catch (err) {
           console.error('Error! failed to init XR session render state:', err);
+        }
+      },
+      initializePlayer: ({ player }) => {
+        if (!player) return;
+        // Set starting position.
+        // player.position.set(0, 0, 750);
+      },
+      initializeController: ({ session }, { controller }) => {
+        if (!session) return;
+        const inputSource = controller.inputSource;
+
+        const profiles = inputSource.profiles;
+        console.log('handedness', inputSource.handedness);
+        console.log('profiles:', profiles);
+        const gamepad = inputSource.gamepad;
+        if (!gamepad) return;
+        console.log('gamepad:', gamepad);
+        console.log('gamepad connected?:', gamepad.connected);
+        controller.addEventListener('connected', (event) => {
+          console.log(`controller connected!`, event);
+        });
+        if ('gamepad' in gamepad) {
+          console.log('gamepad.gamepad', gamepad.gamepad);
         }
       },
     },
