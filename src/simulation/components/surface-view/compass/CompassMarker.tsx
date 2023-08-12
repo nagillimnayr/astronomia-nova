7edc6ac875dfebeb96d6e5a968f28e789c45ff8a
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-12 14:30:42 -0700

    feat: add vector-utils

--- src/simulation/components/surface-view/compass/CompassMarker.tsx
+++ src/simulation/components/surface-view/compass/CompassMarker.tsx
@@ -1,10 +1,11 @@
 import { METER } from '@/simulation/utils/constants';
+import { getLocalUpInWorldCoords } from '@/simulation/utils/vector-utils';
 import { Line, Text } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
 import { Object3D, Quaternion, Vector3, Vector3Tuple } from 'three';
 
-const _up = new Vector3(0);
+const _up = new Vector3();
 const _objWorldPos = new Vector3();
 const _parentWorldPos = new Vector3();
 const _center: Vector3Tuple = [0, 0, 0];
@@ -33,13 +34,7 @@ export const CompassMarker = ({ children, position }: Props) => {
           if (!parent) return;
 
           // Get the object's up vector in world coords.
-          obj.getWorldPosition(_objWorldPos);
-          _up.set(0, 1, 0); // Reset y unit vector.
-          // Get y unit vector in world coords. (This will set _up as the object's up vector relative to the world origin. Equivalent to adding the local up vector to the object's world coords.)
-          obj.localToWorld(_up);
-          // To get the direction of the vector relative to the object, we can subtract the object's world coords.
-          _up.sub(_objWorldPos); // We now have the object's up vector in world coords.
-          obj.up.copy(_up); // Set the up vector so that it will be properly oriented after rotating it.
+          obj.up.set(...getLocalUpInWorldCoords(obj)); // Set the up vector so that it will be properly oriented after rotating it.
 
           // Rotate object to face its parent's origin.
           parent.getWorldPosition(_parentWorldPos);
---
+++ src/simulation/utils/vector-utils.ts
@@ -0,0 +1,27 @@
+import { type Object3D, Vector3, type Vector3Tuple } from 'three';
+
+const _up = new Vector3();
+const _objWorldPos = new Vector3();
+
+/**
+ * @description Returns the object's local y-axis unit vector in world coordinates.
+ *
+ *
+ * @author Ryan Milligan
+ * @date 12/08/2023
+ *
+ * @export
+ * @param {Object3D} obj
+ * @returns {*}  {Vector3Tuple}
+ */
+export function getLocalUpInWorldCoords(obj: Object3D): Vector3Tuple {
+  // Get the object's world coords.
+  obj.getWorldPosition(_objWorldPos);
+  _up.set(0, 1, 0); // Reset to y unit vector.
+  // Get local y unit vector in world coords. (This will set _up as the object's up vector relative to the world origin. Equivalent to adding the local up vector to the object's world coords.)
+  obj.localToWorld(_up);
+  // To get the direction of the vector relative to the object, we can subtract the object's world coords.
+  _up.sub(_objWorldPos);
+  // We now have the object's up vector in world coords.
+  return _up.toArray();
+}
