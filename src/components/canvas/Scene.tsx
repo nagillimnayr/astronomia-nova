4f104e0d4cd5a6214cc46c46291c80c8b357f961
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-12 16:48:05 -0700

    refactor: delete unused code

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -2,22 +2,9 @@ import Simulation from '@/simulation/components/Simulation';
 
 import { type PropsWithChildren, useContext, useEffect } from 'react';
 
-import { MachineContext } from '@/state/xstate/MachineProviders';
-import { useThree } from '@react-three/fiber';
 import { CameraController } from '@/simulation/components/camera-controller/CameraController';
-import { useKeyPressed } from '@react-hooks-library/core';
 
 const Scene = ({ children }: PropsWithChildren) => {
-  const { cameraActor, uiActor } = MachineContext.useSelector(
-    ({ context }) => context
-  );
-  const getThree = useThree(({ get }) => get);
-
-  useEffect(() => {
-    // const scene = getThree().scene;
-    cameraActor.send({ type: 'ASSIGN_THREE', get: getThree });
-  }, [cameraActor, getThree]);
-
   return (
     <>
       <CameraController />
