1492f92ba4bf913163fc44105bf28f6ffc399fd8
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-14 10:25:24 -0700

    refactor: add negative axes

--- src/simulation/utils/constants.ts
+++ src/simulation/utils/constants.ts
@@ -3,6 +3,9 @@ import { Vector3 } from 'three';
 export const X_AXIS: Readonly<Vector3> = new Vector3(1, 0, 0);
 export const Y_AXIS: Readonly<Vector3> = new Vector3(0, 1, 0);
 export const Z_AXIS: Readonly<Vector3> = new Vector3(0, 0, 1);
+export const X_AXIS_NEG: Readonly<Vector3> = new Vector3(-1, 0, 0);
+export const Y_AXIS_NEG: Readonly<Vector3> = new Vector3(0, -1, 0);
+export const Z_AXIS_NEG: Readonly<Vector3> = new Vector3(0, 0, -1);
 
 export const GRAV_CONST = 6.674e-11; // Gravitational Constant m^3/kg/s^2;
 export const AU = 1.495978707e11; // Astronomical Unit in m
