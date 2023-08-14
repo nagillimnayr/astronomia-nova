19047db8a4a52e668a5b354a83a503a797b91dfd
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-14 12:37:38 -0700

    feat: add loadPhysicalData procedure

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,26 +1,23 @@
 import { z } from 'zod';
 import { procedure, router } from '../trpc';
-import { loadEphemerides } from '@/lib/horizons/loadEphemerides';
+import {
+  loadEphemerides,
+  loadPhysicalData,
+} from '@/lib/horizons/loadEphemerides';
 
 export const appRouter = router({
-  hello: procedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
-
   // Load ephemerides procedure. (Query)
   // Takes a string 'name' as input.
   loadEphemerides: procedure
     .input(z.object({ name: z.string() }))
     .query(async ({ input }) => {
       return await loadEphemerides(input.name);
+    }),
+
+  loadPhysicalData: procedure
+    .input(z.object({ name: z.string() }))
+    .query(async ({ input }) => {
+      return await loadPhysicalData(input.name);
     }),
 });
 // export type definition of API
