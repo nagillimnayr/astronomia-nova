e8af86df8ce1a1701071d5fa812c2d0357c68ef5
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-14 21:43:35 -0700

    refactor: delete unused code

--- src/simulation/components/camera-controller/CameraController.tsx
+++ src/simulation/components/camera-controller/CameraController.tsx
@@ -15,37 +15,12 @@ declare module '@react-three/fiber' {
   }
 }
 
-const MIN_POLAR_ANGLE: Readonly<number> = degToRad(1);
-const MAX_POLAR_ANGLE: Readonly<number> = degToRad(179);
-
 export const CameraManager = () => {
   const { cameraActor } = MachineContext.useSelector(({ context }) => context);
   const controllerRef = useRef<CameraController>(null!);
 
   return (
     <>
-      {/* <CameraControls
-        makeDefault={true}
-        minDistance={1e-5}
-        polarAngle={degToRad(60)}
-        minPolarAngle={MIN_POLAR_ANGLE}
-        maxPolarAngle={MAX_POLAR_ANGLE}
-        ref={(controls) => {
-          if (!controls) {
-            return;
-          }
-
-          console.assert(
-            '__r3f' in controls,
-            'Error: controls are not of type BaseInstance'
-          );
-          // Assign controls context in camera state machine.
-          cameraActor.send({
-            type: 'ASSIGN_CONTROLS',
-            controls: controls as unknown as CameraControls & BaseInstance,
-          });
-        }}
-      ></CameraControls> */}
       <cameraController
         ref={(controller) => {
           if (!controller) return;
