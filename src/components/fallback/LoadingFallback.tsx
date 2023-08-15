83e1539a52d940a10209c424d67c68f23f755323
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-14 17:05:44 -0700

    fix: use css module for fallback

--- src/components/fallback/LoadingFallback.tsx
+++ src/components/fallback/LoadingFallback.tsx
@@ -1,11 +1,14 @@
-import './fallback.css';
+import { cn } from '@/lib/cn';
+import styles from './fallback.module.css';
 
 export const LoadingFallback = () => {
   return (
-    <div className="grid h-full w-full place-items-center text-foreground">
+    <div
+      className={cn('grid h-full w-full place-items-center text-foreground')}
+    >
       {/* <span className="icon-[svg-spinners--ring-resize] aspect-square text-5xl" />
       <span className="icon-[svg-spinners--ring-resize] aspect-square text-5xl" /> */}
-      <div className="loader">
+      <div className={styles.loader}>
         Loading
         <span></span>
       </div>
