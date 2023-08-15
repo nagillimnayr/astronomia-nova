5634e559e94af00c6f8201b3251053c4b9923115
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-14 18:48:08 -0700

    feat: add arrow controls for advancing day

--- src/simulation/components/KeyPressControl.tsx
+++ src/simulation/components/KeyPressControl.tsx
@@ -1,5 +1,5 @@
 import { MachineContext } from '@/state/xstate/MachineProviders';
-import { useKeyPressed } from '@react-hooks-library/core';
+import { useEventListener, useKeyPressed } from '@react-hooks-library/core';
 import { CameraControls } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
 import { Vector3 } from 'three';
@@ -10,6 +10,7 @@ const _diff = new Vector3();
 
 export const KeyPressControl = () => {
   const getThree = useThree(({ get }) => get);
+  const rootActor = MachineContext.useActorRef();
   const { mapActor } = MachineContext.useSelector(({ context }) => context);
 
   useKeyPressed(' ', (evt) => {
@@ -18,6 +19,30 @@ export const KeyPressControl = () => {
     console.log('camera:', camera);
     console.log('camera parent:', camera.parent);
   });
+
+  useKeyPressed('ArrowLeft', (event) => {
+    console.log(event);
+    // rootActor.send({ type: 'ADVANCE_DAY', reverse: true });
+  });
+
+  useKeyPressed('ArrowRight', (event) => {
+    console.log(event);
+    // rootActor.send({ type: 'ADVANCE_DAY' });
+  });
+
+  useEventListener('keydown', (event) => {
+    switch (event.key) {
+      case 'ArrowLeft': {
+        rootActor.send({ type: 'ADVANCE_DAY', reverse: true });
+        break;
+      }
+      case 'ArrowRight': {
+        rootActor.send({ type: 'ADVANCE_DAY' });
+        break;
+      }
+    }
+  });
+
   return (
     <>
       <></>
