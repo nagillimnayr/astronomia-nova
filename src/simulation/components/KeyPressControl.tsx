7e30884f5f6b2a4e1515b5f904fa8da9b71c9a7b
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-14 21:43:51 -0700

    refactor: disable arrow controls

--- src/simulation/components/KeyPressControl.tsx
+++ src/simulation/components/KeyPressControl.tsx
@@ -11,35 +11,22 @@ const _diff = new Vector3();
 export const KeyPressControl = () => {
   const getThree = useThree(({ get }) => get);
   const rootActor = MachineContext.useActorRef();
-  const { mapActor } = MachineContext.useSelector(({ context }) => context);
-
-  useKeyPressed(' ', (evt) => {
-    evt.preventDefault();
-    const { camera, controls, scene } = getThree();
-    console.log('camera:', camera);
-    console.log('camera parent:', camera.parent);
-  });
-
-  useKeyPressed('ArrowLeft', (event) => {
-    console.log(event);
-    // rootActor.send({ type: 'ADVANCE_DAY', reverse: true });
-  });
-
-  useKeyPressed('ArrowRight', (event) => {
-    console.log(event);
-    // rootActor.send({ type: 'ADVANCE_DAY' });
-  });
 
   useEventListener('keydown', (event) => {
     switch (event.key) {
-      case 'ArrowLeft': {
-        rootActor.send({ type: 'ADVANCE_DAY', reverse: true });
-        break;
+      case ' ': {
+        const { camera, controls, scene } = getThree();
+        console.log('camera:', camera);
+        console.log('camera parent:', camera.parent);
       }
-      case 'ArrowRight': {
-        rootActor.send({ type: 'ADVANCE_DAY' });
-        break;
-      }
+      // case 'ArrowLeft': {
+      //   rootActor.send({ type: 'ADVANCE_DAY', reverse: true });
+      //   break;
+      // }
+      // case 'ArrowRight': {
+      //   rootActor.send({ type: 'ADVANCE_DAY' });
+      //   break;
+      // }
     }
   });
 
