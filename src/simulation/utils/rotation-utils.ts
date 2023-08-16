fcc9c744f5085b8ff3e4a5f42a54feb62d0cfb20
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-16 15:54:04 -0700

    refactor: improve normalizeAngle

--- src/simulation/utils/rotation-utils.ts
+++ src/simulation/utils/rotation-utils.ts
@@ -1,12 +1,19 @@
 import { TWO_PI } from './constants';
 
+/**
+ * @description Adjust the angle to be within the range [0, 2PI).
+ *
+ * @author Ryan Milligan
+ * @date 16/08/2023
+ * @export
+ * @param {number} angle
+ * @returns {*}
+ */
 export function normalizeAngle(angle: number) {
-  let adjustedAngle = angle;
-  while (adjustedAngle < TWO_PI) {
+  let adjustedAngle = angle % TWO_PI;
+  while (adjustedAngle < 0) {
     adjustedAngle += TWO_PI;
   }
-  while (adjustedAngle > TWO_PI) {
-    adjustedAngle -= TWO_PI;
-  }
+
   return adjustedAngle;
 }
