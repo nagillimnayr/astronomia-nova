176acec5bb3a8600ef7b4fe54510b527b9981255
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-16 11:03:17 -0700

    feat: pass getters to state machines

--- src/components/canvas/vr/VRManager.tsx
+++ src/components/canvas/vr/VRManager.tsx
@@ -1,31 +1,34 @@
 import { useController, useXR, useXREvent } from '@react-three/xr';
-import { PlayerControls } from './PlayerControls';
+import { VRControls } from './VRControls';
 import { VRPlayer } from './VRPlayer';
 import { MachineContext } from '@/state/xstate/MachineProviders';
 import { useEffect } from 'react';
+import { useThree } from '@react-three/fiber';
 
 export const VRManager = () => {
-  const { player, isPresenting, session, controllers, get } = useXR();
+  const getXR = useXR(({ get }) => get);
   const rightController = useController('right');
-  const { cameraActor } = MachineContext.useSelector(({ context }) => context);
-
-  useEffect(() => {
-    cameraActor.send({ type: 'ASSIGN_GET_XR', getXR: get });
-  }, [cameraActor, get]);
-
-  useXREvent(
-    'squeeze',
-    (event) => {
-      console.log('player position:', player.position.toArray());
-      console.log('player', player);
-      console.log('event', event);
-    },
-    { handedness: 'right' }
+  const { cameraActor, vrActor } = MachineContext.useSelector(
+    ({ context }) => context
   );
 
+  const getThree = useThree(({ get }) => get);
+  useEffect(() => {
+    cameraActor.send({ type: 'ASSIGN_GET_XR', getXR });
+    vrActor.send({ type: 'ASSIGN_GET_XR', getXR });
+  }, [cameraActor, getXR, vrActor]);
+  useEffect(() => {
+    cameraActor.send({ type: 'ASSIGN_GET_THREE', getThree: getThree });
+    vrActor.send({ type: 'ASSIGN_GET_THREE', getThree: getThree });
+  }, [cameraActor, getThree, vrActor]);
+  useEffect(() => {
+    const xr = getThree().gl.xr;
+    if (!xr) return;
+    vrActor.send({ type: 'ASSIGN_XR_MANAGER', xr });
+  }, [getThree, vrActor]);
   return (
     <>
-      <PlayerControls />
+      <VRControls />
       <VRPlayer />
     </>
   );
