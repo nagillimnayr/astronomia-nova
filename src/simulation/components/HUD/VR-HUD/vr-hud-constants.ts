16b5fe371b215981761bbe2ba941b461ef13a8f0
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-16 19:36:13 -0700

    feat: add VR HUD constants

---
+++ src/simulation/components/HUD/VR-HUD/vr-hud-constants.ts
@@ -0,0 +1,41 @@
+export const GOLDEN_RATIO = 1.618;
+export const color = Object.freeze({
+  background: '#09090b',
+  foreground: '#ffffff',
+
+  muted: '#18181b',
+  mutedForeground: '#ffffff',
+
+  subtle: '#2c2c30',
+  subtleForeground: '#ffffff',
+
+  card: '#2c2c30',
+  cardForeground: '#ffffff',
+
+  border: '#2a2a2a',
+  input: '#282829',
+});
+
+export const text = Object.freeze({
+  sm: 16,
+  md: 24,
+  base: 32,
+  lg: 48,
+  xl: 64,
+});
+
+export const borderRadius = Object.freeze({
+  sm: 16,
+  md: 24,
+  base: 32,
+  lg: 48,
+  xl: 64,
+});
+
+export const border = Object.freeze({
+  sm: 1,
+  md: 2,
+  base: 4,
+  lg: 6,
+  xl: 8,
+});
