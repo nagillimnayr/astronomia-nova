867d4a747cf1401890908ec1e652c12b47911bc3
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-16 19:36:03 -0700

    feat: add new loading spinner

--- src/components/fallback/LoadingFallback.tsx
+++ src/components/fallback/LoadingFallback.tsx
@@ -1,17 +1,34 @@
 import { cn } from '@/lib/cn';
 import styles from './fallback.module.css';
+import { ClassNameValue } from 'tailwind-merge';
 
-export const LoadingFallback = () => {
-  return (
+type FallbackProps = {
+  primary?: boolean;
+  className?: ClassNameValue;
+};
+export const LoadingFallback = ({
+  primary = false,
+  className,
+}: FallbackProps) => {
+  return primary ? (
     <div
       className={cn('grid h-full w-full place-items-center text-foreground')}
     >
-      {/* <span className="icon-[svg-spinners--ring-resize] aspect-square text-5xl" />
-      <span className="icon-[svg-spinners--ring-resize] aspect-square text-5xl" /> */}
-      <div className={styles.loader}>
-        Loading
-        <span></span>
+      <div className={cn('aspect-square w-12 rounded-full ', className)}>
+        <span>Loading</span>
       </div>
     </div>
+  ) : (
+    <>
+      <div
+        className={cn(
+          'relative flex aspect-square w-12 animate-spin items-center justify-center rounded-full border-b-2 border-l-2 border-gray-100 bg-transparent',
+          // 'animate-spin',
+          className
+        )}
+      >
+        <div className="absolute top-0 aspect-square w-3 -translate-y-1/2 rounded-full bg-white" />
+      </div>
+    </>
   );
 };
