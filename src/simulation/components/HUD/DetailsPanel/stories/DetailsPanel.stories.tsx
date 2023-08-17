1b30a33d2188cbc7ecaf6067ede6055509b0ed67
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-17 12:23:12 -0700

    feat add mock data to story

--- src/simulation/components/HUD/DetailsPanel/stories/DetailsPanel.stories.tsx
+++ src/simulation/components/HUD/DetailsPanel/stories/DetailsPanel.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { DetailsPanel } from '../DetailsPanel';
+import { useEffect } from 'react';
+import KeplerBody from '@/simulation/classes/kepler-body';
+import { MachineContext } from '@/state/xstate/MachineProviders';
 
 const meta: Meta<typeof DetailsPanel> = {
   title: 'HUD/DetailsPanel',
@@ -12,9 +15,38 @@ type Story = StoryObj<typeof DetailsPanel>;
 export const Default: Story = {
   render: () => {
     return (
-      <div className="font-sans">
-        <DetailsPanel />
-      </div>
+      <>
+        <DetailsPanelStory />
+      </>
     );
   },
+};
+
+const params = {
+  name: 'Earth',
+  mass: 5.97219e24,
+  meanRadius: 6371010,
+  obliquity: 23.4392911,
+  siderealRotationRate: 0.00007292115,
+  siderealRotationPeriod: 86164.10063718943,
+};
+const DetailsPanelStory = () => {
+  const { selectionActor } = MachineContext.useSelector(
+    ({ context }) => context
+  );
+
+  useEffect(() => {
+    // Instantiate KeplerBody.
+    const body = new KeplerBody(params);
+    body.name = params.name;
+    // Select the newly create body.
+    selectionActor.send({ type: 'SELECT', selection: body });
+  }, [selectionActor]);
+  return (
+    <>
+      <div className="grid h-full w-full place-items-center font-sans">
+        <DetailsPanel />
+      </div>
+    </>
+  );
 };
