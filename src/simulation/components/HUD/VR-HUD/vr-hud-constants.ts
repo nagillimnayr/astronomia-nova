6f44ad2f4286e28ee180471cfd14ad7b4eff8b8c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-18 10:49:24 -0700

    feat: lighten colors

--- src/simulation/components/HUD/VR-HUD/vr-hud-constants.ts
+++ src/simulation/components/HUD/VR-HUD/vr-hud-constants.ts
@@ -1,20 +1,25 @@
 export const GOLDEN_RATIO = 1.618;
 export const colors = Object.freeze({
   // background: '#09090b',
-  background: '#2c2c30',
-  foreground: '#ffffff',
+  background: '#4b5563', // gray600
+  foreground: '#ffffff', // white
 
-  muted: '#18181b',
-  mutedForeground: '#ffffff',
-
-  subtle: '#2c2c30',
-  subtleForeground: '#ffffff',
-
-  card: '#2c2c30',
-  cardForeground: '#ffffff',
-
-  border: '#2a2a2a',
+  // border: '#2a2a2a',
+  border: '#6b7280', // gray500
   input: '#282829',
+
+  gray200: '#e5e7eb',
+  gray300: '#d1d5db',
+  gray400: '#9ca3af',
+  gray500: '#6b7280',
+  gray600: '#4b5563', // decent
+  gray700: '#374151',
+  gray800: '#1f2937',
+  gray900: '#111827',
+
+  slate500: '#64748b',
+  slate600: '#475569',
+  slate700: '#334155',
 });
 
 export const text = Object.freeze({
