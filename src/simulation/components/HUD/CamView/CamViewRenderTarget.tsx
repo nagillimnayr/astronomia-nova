89ded99b3d8621b1a84d4c9a73d40c264dc429a2
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-17 21:40:31 -0700

    fix: fix build error

--- src/simulation/components/HUD/CamView/CamViewRenderTarget.tsx
+++ src/simulation/components/HUD/CamView/CamViewRenderTarget.tsx
@@ -13,20 +13,15 @@ import { Scene } from 'three';
 
 const CamViewRenderTarget = () => {
   const { cameraActor } = MachineContext.useSelector(({ context }) => context);
-  const surfaceCamera = useSelector(
-    cameraActor,
-    (state) => state.context.surfaceCamera
-  );
+
   const getThree = useSelector(cameraActor, (state) => state.context.getThree);
 
   const renderTarget = useFBO(window.innerWidth / 4, window.innerHeight / 4);
 
   useFrame(({ gl }) => {
-    if (!surfaceCamera || !getThree) return;
     gl.autoClear = true;
     const scene = getThree().scene;
     gl.setRenderTarget(renderTarget);
-    gl.render(scene, surfaceCamera);
     gl.setRenderTarget(null);
   });
 
