afd5e79cd3714ad13893a5ea3b92c89d2c53cef0
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-18 20:55:09 -0700

    build: add glslify loader

--- next.config.mjs
+++ next.config.mjs
@@ -46,6 +46,39 @@ const nextConfig = {
   //   defaultLocale: 'en',
   // },
   transpilePackages: ['three', 'drei'],
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // We're in the browser build, so we can safely exclude the sharp module
+      config.externals.push('sharp');
+    }
+    // // audio support
+    // config.module.rules.push({
+    //   test: /\.(ogg|mp3|wav|mpe?g)$/i,
+    //   exclude: config.exclude,
+    //   use: [
+    //     {
+    //       loader: require.resolve('url-loader'),
+    //       options: {
+    //         limit: config.inlineImageLimit,
+    //         fallback: require.resolve('file-loader'),
+    //         publicPath: `${config.assetPrefix}/_next/static/images/`,
+    //         outputPath: `${isServer ? '../' : ''}static/images/`,
+    //         name: '[name]-[hash].[ext]',
+    //         esModule: config.esModule || false,
+    //       },
+    //     },
+    //   ],
+    // });
+
+    // shader support
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    });
+
+    return config;
+  },
 };
 
 export default withMDX(nextConfig);
