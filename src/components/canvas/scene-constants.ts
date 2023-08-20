34b1a380f58fd388533cd71147f9513a556b7d30
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-20 15:10:10 -0700

    feat: set far plane

--- src/components/canvas/scene-constants.ts
+++ src/components/canvas/scene-constants.ts
@@ -1,10 +1,11 @@
 import {
   DIST_MULT,
   METER,
+  SIMULATION_RADIUS,
   SOLAR_SYSTEM_RADIUS,
 } from '@/simulation/utils/constants';
 
 export const NEAR_CLIP = 1e-5;
-export const FAR_CLIP = (4 * SOLAR_SYSTEM_RADIUS) / DIST_MULT;
+export const FAR_CLIP = 2 * SIMULATION_RADIUS;
 
 export const SURFACE_NEAR_CLIP = METER / 10;
