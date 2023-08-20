76dbd8a497b263c50073f7e0a95014d4c0f9e538
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-20 14:12:34 -0700

    feat: increase far plane

--- src/components/canvas/scene-constants.ts
+++ src/components/canvas/scene-constants.ts
@@ -5,6 +5,6 @@ import {
 } from '@/simulation/utils/constants';
 
 export const NEAR_CLIP = 1e-5;
-export const FAR_CLIP = (2 * SOLAR_SYSTEM_RADIUS) / DIST_MULT;
+export const FAR_CLIP = (4 * SOLAR_SYSTEM_RADIUS) / DIST_MULT;
 
 export const SURFACE_NEAR_CLIP = METER / 10;
