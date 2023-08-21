ed234f36197606e5ba3c17afe239dc35f22790e5
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-21 10:30:17 -0700

    feat: add Loader

--- src/components/canvas/CanvasWrapper.tsx
+++ src/components/canvas/CanvasWrapper.tsx
@@ -9,7 +9,7 @@ import { HUD } from '@/simulation/components/HUD/HUD';
 import { MachineContext } from '@/state/xstate/MachineProviders';
 import Scene from './Scene';
 import { VRManager } from './vr/VRManager';
-import { Hud, Stats } from '@react-three/drei';
+import { Hud, Loader, Preload, Stats } from '@react-three/drei';
 import { VRHUD } from '@/simulation/components/HUD/VR-HUD/VRHUD';
 
 const CanvasWrapper = ({ children }: PropsWithChildren) => {
@@ -18,95 +18,102 @@ const CanvasWrapper = ({ children }: PropsWithChildren) => {
   );
   const container = useRef<HTMLDivElement>(null!);
   return (
-    <div className="relative z-0 flex h-full w-full flex-col justify-center border">
-      <HUD className="z-10" />
-      <Suspense fallback={<LoadingFallback />}>
-        {/** Wrap the canvas in a div to create a separate stacking context. This is necessary because the <Html> components from Drei and portalled out of the canvas and become sibling elements of the canvas. They have an absurdly large z-index, so they will be rendered over top of any of their siblings. Wrapping the canvas in this way ensures that they share a stacking context only with each other and the canvas, and prevents them from clipping through the HUD or the rest of the UI. */}
-        <div ref={container} className="relative z-0 h-full w-full">
-          <div className="relative z-0 h-full w-full">
-            <Canvas
-              className="z-[0]"
-              // eventSource={container}
-              gl={{
-                logarithmicDepthBuffer: true,
-                alpha: true,
-                localClippingEnabled: true,
-                preserveDrawingBuffer: true,
-              }}
-              linear /* Textures will appear washed out unless this is set. */
-              flat
-              ref={(canvas) => {
-                if (!canvas) return;
-                // Assign canvas context in camera state machine. This is necessary so that event listeners can be attached for the camera controller.
-                cameraActor.send({ type: 'ASSIGN_CANVAS', canvas });
-              }}
-              onCreated={(state) => {
-                // Filter intersections so that invisible objects don't trigger pointer events.
-                state.setEvents({
-                  filter: (intersections) =>
-                    intersections.filter((i) => i.object.visible),
-                });
-              }}
-            >
-              <XR
-                referenceSpace="local"
-                onSessionStart={(event) => {
-                  const session = event.target;
-
-                  // Assign session object to external state machine and start session state.
-                  vrActor.send({ type: 'START_SESSION', session });
-                  cameraActor.send({
-                    type: 'START_XR_SESSION',
-                    xrSession: session,
+    <Suspense fallback={null}>
+      <div className="relative z-0 flex h-full w-full flex-col justify-center border">
+        <HUD className="z-10" />
+        {/* <Suspense fallback={<LoadingFallback />}> */}
+        <Suspense fallback={null}>
+          {/** Wrap the canvas in a div to create a separate stacking context. This is necessary because the <Html> components from Drei and portalled out of the canvas and become sibling elements of the canvas. They have an absurdly large z-index, so they will be rendered over top of any of their siblings. Wrapping the canvas in this way ensures that they share a stacking context only with each other and the canvas, and prevents them from clipping through the HUD or the rest of the UI. */}
+          <div ref={container} className="relative z-0 h-full w-full">
+            <div className="relative z-0 h-full w-full">
+              <Canvas
+                className="z-[0]"
+                // eventSource={container}
+                gl={{
+                  logarithmicDepthBuffer: true,
+                  alpha: true,
+                  localClippingEnabled: true,
+                  preserveDrawingBuffer: true,
+                }}
+                linear /* Textures will appear washed out unless this is set. */
+                flat
+                ref={(canvas) => {
+                  if (!canvas) return;
+                  // Assign canvas context in camera state machine. This is necessary so that event listeners can be attached for the camera controller.
+                  cameraActor.send({ type: 'ASSIGN_CANVAS', canvas });
+                }}
+                onCreated={(state) => {
+                  // Filter intersections so that invisible objects don't trigger pointer events.
+                  state.setEvents({
+                    filter: (intersections) =>
+                      intersections.filter((i) => i.object.visible),
                   });
-
-                  // Create a new reference space.
-                  session
-                    .requestReferenceSpace('local')
-                    .then((refSpace) => {
-                      // Assign the new reference space to the external state machines.
-                      cameraActor.send({
-                        type: 'ASSIGN_REF_SPACE',
-                        refSpace,
-                      });
-                      vrActor.send({
-                        type: 'ASSIGN_REF_SPACE_ORIGIN',
-                        refSpace,
-                      });
-                    })
-                    .catch((reason) => console.error(reason));
-                }}
-                onSessionEnd={(event) => {
-                  // Send end session event to state machines.
-                  console.log('Ending XR session:', event);
-                  vrActor.send({ type: 'END_SESSION' });
-                  cameraActor.send({ type: 'END_XR_SESSION' });
-                }}
-                onVisibilityChange={(event) => {
-                  console.log('XR visibility change:', event);
-                }}
-                onInputSourcesChange={(event) => {
-                  console.log('XR input sources change:', event);
                 }}
               >
-                {/* <Hud renderPriority={1}> */}
-                <Controllers />
-                <VRManager />
-                <Scene>{children}</Scene>
-                {/* <Stats /> */}
-                {/* <Perf position={'bottom-left'} /> */}
-                {/* </Hud> */}
+                <XR
+                  referenceSpace="local"
+                  onSessionStart={(event) => {
+                    const session = event.target;
 
-                {/* <Hud renderPriority={2}> */}
-                {/* <VRManager /> */}
-                <VRHUD />
-                {/* </Hud> */}
-              </XR>
-            </Canvas>
+                    // Assign session object to external state machine and start session state.
+                    vrActor.send({ type: 'START_SESSION', session });
+                    cameraActor.send({
+                      type: 'START_XR_SESSION',
+                      xrSession: session,
+                    });
+
+                    // Create a new reference space.
+                    session
+                      .requestReferenceSpace('local')
+                      .then((refSpace) => {
+                        // Assign the new reference space to the external state machines.
+                        cameraActor.send({
+                          type: 'ASSIGN_REF_SPACE',
+                          refSpace,
+                        });
+                        vrActor.send({
+                          type: 'ASSIGN_REF_SPACE_ORIGIN',
+                          refSpace,
+                        });
+                      })
+                      .catch((reason) => console.error(reason));
+                  }}
+                  onSessionEnd={(event) => {
+                    // Send end session event to state machines.
+                    console.log('Ending XR session:', event);
+                    vrActor.send({ type: 'END_SESSION' });
+                    cameraActor.send({ type: 'END_XR_SESSION' });
+                  }}
+                  onVisibilityChange={(event) => {
+                    console.log('XR visibility change:', event);
+                  }}
+                  onInputSourcesChange={(event) => {
+                    console.log('XR input sources change:', event);
+                  }}
+                >
+                  <Suspense fallback={null}>
+                    {/* <Hud renderPriority={1}> */}
+                    <Controllers />
+                    <VRManager />
+                    <Scene>{children}</Scene>
+                    <Stats />
+                    <Perf position={'bottom-left'} />
+                    {/* </Hud> */}
+
+                    {/* <Hud renderPriority={2}> */}
+                    {/* <VRManager /> */}
+                    <VRHUD />
+                    {/* </Hud> */}
+                    <Preload all />
+                  </Suspense>
+                </XR>
+              </Canvas>
+            </div>
           </div>
-        </div>
-      </Suspense>
-    </div>
+        </Suspense>
+      </div>
+      <Loader />
+    </Suspense>
   );
 };
 
