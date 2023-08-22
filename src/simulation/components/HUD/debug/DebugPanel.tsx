ac13944d2ccfdb02eb30be734d9898ad84241a4d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-22 12:43:41 -0700

    feat: disable camera rotation display

--- src/simulation/components/HUD/debug/DebugPanel.tsx
+++ src/simulation/components/HUD/debug/DebugPanel.tsx
@@ -10,16 +10,18 @@ export const DebugPanel = () => {
   const [state, send] = useActor(cameraActor);
   const controller = state.context.controls;
   if (!controller) return;
+
   const camera = controller.camera;
   const camRotation = camera.rotation;
   const { x, y, z } = camRotation;
+
   return (
     <>
-      <div className="m-4 flex h-full w-full flex-col items-center justify-center rounded-md border-2 border-border bg-background text-foreground">
-        <span>{'camera rotation:'}</span>
+      <div className="m-4 flex h-full w-full flex-col items-center justify-center rounded-md border-transparent bg-transparent text-foreground">
+        {/* <span>{'camera rotation:'}</span>
         <span>
           {`{ x: ${x.toFixed(2)},   y: ${y.toFixed(2)},   z: ${z.toFixed(2)} }`}
-        </span>
+        </span> */}
       </div>
     </>
   );
