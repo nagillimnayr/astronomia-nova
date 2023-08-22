9142a1e82f4a336b46576f8a30dfb7d7104bfbfd
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-22 14:35:11 -0700

    refactor: separate debug display into different components

---
+++ src/simulation/components/HUD/VR-HUD/vr-debug/VRDebugDisplay.tsx
@@ -0,0 +1,39 @@
+import { Object3D, type Vector3Tuple, Vector3, type Group } from 'three';
+
+import { VRDebugFrustumDisplay } from './VRDebugFrustumDisplay';
+import { VRDebugPoseDisplay } from './VRDebugPoseDisplay';
+import { createPortal, useThree } from '@react-three/fiber';
+
+type VRDebugDisplayProps = {
+  position?: Vector3Tuple;
+  scale?: number;
+};
+export const VRDebugDisplay = ({
+  position = [0, 0, 0],
+  scale = 0.05,
+}: VRDebugDisplayProps) => {
+  return (
+    <>
+      <group position={position} scale={scale}>
+        <VRDebugPoseDisplay position={[0, 2.5, 0]} />
+        <VRDebugFrustumDisplay position={[0, -0.5, 0]} />
+      </group>
+    </>
+  );
+};
+
+export const VRDebugPortal = ({
+  position = [0, 0, -1],
+}: VRDebugDisplayProps) => {
+  const camera = useThree(({ camera }) => camera);
+
+  // Attach to the camera.
+  return createPortal(
+    <>
+      <>
+        <VRDebugDisplay position={position} />
+      </>
+    </>,
+    camera
+  );
+};
---
+++ src/simulation/components/HUD/VR-HUD/vr-debug/VRDebugFrustumDisplay.tsx
@@ -0,0 +1,44 @@
+import { Vector3Tuple } from 'three';
+import { Text, useCursor } from '@react-three/drei';
+import { MachineContext } from '@/state/xstate/MachineProviders';
+import { useRef, useState } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useXR } from '@react-three/xr';
+
+type VRDebugFrustumDisplayProps = {
+  position?: Vector3Tuple;
+};
+export const VRDebugFrustumDisplay = ({
+  position = [0, 0, 0],
+}: VRDebugFrustumDisplayProps) => {
+  const { cameraActor, vrActor } = MachineContext.useSelector(
+    ({ context }) => context
+  );
+
+  const [near, setNear] = useState<number>(0);
+  const [far, setFar] = useState<number>(0);
+
+  // const session = useXR(({session})=>session)
+
+  useFrame(({ gl, camera }) => {
+    const session = gl.xr.getSession();
+    if (!session) return;
+
+    const { depthNear, depthFar } = session.renderState;
+    setNear(depthNear);
+    setFar(depthFar);
+  });
+
+  return (
+    <>
+      <group position={position}>
+        <Text position={[0, 0.5, 0]} anchorX={'center'} anchorY={'middle'}>
+          {'near: ' + near.toFixed(2)}
+        </Text>
+        <Text position={[0, -0.5, 0]} anchorX={'center'} anchorY={'middle'}>
+          {'far: ' + far.toFixed(2)}
+        </Text>
+      </group>
+    </>
+  );
+};
