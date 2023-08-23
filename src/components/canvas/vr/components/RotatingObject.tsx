af10bb3f5d902b35a2a7519737da85f26a5fd46d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-22 18:52:06 -0700

    feat: add interaction

--- src/components/canvas/vr/components/RotatingObject.tsx
+++ src/components/canvas/vr/components/RotatingObject.tsx
@@ -1,25 +1,81 @@
-import { Box } from '@react-three/drei';
+import { getLocalUpInWorldCoords } from '@/simulation/utils/vector-utils';
+import { Box, Text } from '@react-three/drei';
 import { Object3DProps, useFrame } from '@react-three/fiber';
-import { PropsWithChildren, useRef } from 'react';
-import { ColorRepresentation, Mesh, Object3D } from 'three';
+import {
+  Interactive,
+  XRInteractionEvent,
+  XRInteractionHandler,
+} from '@react-three/xr';
+import { PropsWithChildren, useCallback, useRef, useState } from 'react';
+import {
+  ColorRepresentation,
+  Group,
+  Mesh,
+  Object3D,
+  Vector3,
+  Vector3Tuple,
+} from 'three';
 
 const rotRate = 1;
 
-type Props = Object3DProps;
-export const RotatingObject = ({ children, ...props }: Props) => {
-  const ref = useRef<Object3D>(null!);
+const _camWorldPos = new Vector3();
 
-  useFrame((state, delta, frame) => {
-    const obj = ref.current;
+type Props = PropsWithChildren & {
+  position?: Vector3Tuple;
+};
+export const RotatingObject = ({ children, position = [0, 0, 0] }: Props) => {
+  const groupRef = useRef<Group>(null!);
+  const objRef = useRef<Object3D>(null!);
+  const textCenterRef = useRef<Object3D>(null!);
+  const textRef = useRef<Object3D>(null!);
+
+  const [text, setText] = useState<string>('idle');
+
+  useFrame(({ camera }, delta, frame) => {
+    const obj = objRef.current;
     obj.rotateX(delta * rotRate);
     obj.rotateY(delta * rotRate);
     obj.rotateZ(delta * rotRate);
+
+    const center = textCenterRef.current;
+    center.up.set(...getLocalUpInWorldCoords(camera));
+    camera.getWorldPosition(_camWorldPos);
+    center.lookAt(_camWorldPos);
   });
+
+  // const handleHover: XRInteractionHandler = useCallback(
+  //   (event: XRInteractionEvent) => {
+  //     setText('hover');
+  //   },
+  //   []
+  // );
+  // const handleBlur: XRInteractionHandler = useCallback(
+  //   (event: XRInteractionEvent) => {
+  //     setText('blur');
+  //   },
+  //   []
+  // );
   return (
     <>
-      <object3D ref={ref} {...props}>
-        {children}
-      </object3D>
+      <Interactive
+        onBlur={() => setText('blur')}
+        onHover={() => setText('hover')}
+        // onMove={() => setText('move')}
+        onSelectStart={() => setText('selectstart')}
+        onSelectEnd={() => setText('selectend')}
+        onSqueezeStart={() => setText('squeezestart')}
+        onSqueezeEnd={() => setText('squeezeend')}
+      >
+        <group ref={groupRef} position={position}>
+          <object3D ref={objRef}>{children}</object3D>
+
+          <object3D ref={textCenterRef}>
+            <Text ref={textRef} position={[0, 0, 1]}>
+              {text}
+            </Text>
+          </object3D>
+        </group>
+      </Interactive>
     </>
   );
 };
