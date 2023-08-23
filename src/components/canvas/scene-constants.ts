b7e875d34166fe52bcf4ab602924a10b5432f2ab
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-22 18:51:44 -0700

    fix: increase far frustum plane

--- src/components/canvas/scene-constants.ts
+++ src/components/canvas/scene-constants.ts
@@ -6,6 +6,7 @@ import {
 } from '@/simulation/utils/constants';
 
 export const NEAR_CLIP = 1e-2 * METER;
-export const FAR_CLIP = 2 * SIMULATION_RADIUS;
+export const FAR_CLIP = 2 * SIMULATION_RADIUS + 1000;
 
-export const SURFACE_NEAR_CLIP = METER / 10;
+// export const SURFACE_NEAR_CLIP = METER / 10;
+export const SURFACE_NEAR_CLIP = NEAR_CLIP;
