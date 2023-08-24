78a8524725bddadfe0f5a1a50b479e3875b37d42
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-24 16:19:41 -0700

    chore: backup koestlich VR UI stories

---
+++ src/simulation/components/HUD/VR-HUD/koestlich/stories/VRSettingsMenu.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { VRCanvasDecorator } from '@/stories/decorators/VRCanvasDecorator';
+
+import { PropsWithChildren } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { MachineContext } from '@/state/xstate/MachineProviders';
+import { MockSolarSystem } from '@/stories/mocks/MockSolarSystem';
+import { CameraControls, PerspectiveCamera } from '@react-three/drei';
+import { VRSettingsMenu } from '../vr-settings-menu/VRSettingsMenu';
+import { VRSettingsButton } from '../vr-settings-menu/VRSettingsButton';
+
+const meta: Meta<typeof VRSettingsMenu> = {
+  title: 'VRHUD/VRSettingsMenu',
+  component: VRSettingsMenu,
+  decorators: [(storyFn) => <VRCanvasDecorator>{storyFn()}</VRCanvasDecorator>],
+};
+
+export default meta;
+type Story = StoryObj<typeof VRSettingsMenu>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <>
+        <VRSettingsMenuStory />
+      </>
+    );
+  },
+};
+
+const VRSettingsMenuStory = () => {
+  return (
+    <>
+      <PerspectiveCamera makeDefault position={[0, 0, 2.5]} />
+      <CameraControls makeDefault />
+      <VRSettingsButton position={[1, 1, 0]} />
+      <VRSettingsMenu defaultOpen />
+    </>
+  );
+};
