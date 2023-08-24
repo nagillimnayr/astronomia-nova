4e73e7c6e6c1be56d6bea1312a793925ff66a6ff
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-23 20:01:18 -0700

    feat: add controllers

--- src/components/canvas/vr/VRSessionOrigin.tsx
+++ src/components/canvas/vr/VRSessionOrigin.tsx
@@ -4,15 +4,27 @@ import { MachineContext } from '@/state/xstate/MachineProviders';
 import {
   ImmersiveSessionOrigin,
   NonImmersiveCamera,
+  useInputSources,
 } from '@coconut-xr/natuerlich/react';
 import { Group, PerspectiveCamera } from 'three';
 import { FAR_CLIP, NEAR_CLIP } from '@/components/canvas/scene-constants';
-import { Controllers } from '@coconut-xr/natuerlich/defaults';
+import {
+  Controllers,
+  PointerController,
+} from '@coconut-xr/natuerlich/defaults';
 import { useSelector } from '@xstate/react';
 
 export const VRSessionOrigin = () => {
   const { cameraActor } = MachineContext.useSelector(({ context }) => context);
   const playerRef = useRef<Group>(null!);
+
+  const inputSources = useInputSources();
+  const leftInput = inputSources.find(
+    (inputSource) => inputSource.handedness === 'left' && !inputSource.hand
+  );
+  const rightInput = inputSources.find(
+    (inputSource) => inputSource.handedness === 'right' && !inputSource.hand
+  );
   return (
     <>
       <ImmersiveSessionOrigin
@@ -31,6 +43,9 @@ export const VRSessionOrigin = () => {
         }}
       >
         <Controllers />
+        {/* {leftInput && <PointerController id={-1} inputSource={leftInput} />}
+
+        {rightInput && <PointerController id={-2} inputSource={rightInput} />} */}
       </ImmersiveSessionOrigin>
     </>
   );
