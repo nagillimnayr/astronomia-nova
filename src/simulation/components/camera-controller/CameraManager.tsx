a524465a831fd640e818446ea36135a231632f35
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-24 21:18:38 -0700

    chore: delete unused code

--- src/simulation/components/camera-controller/CameraManager.tsx
+++ src/simulation/components/camera-controller/CameraManager.tsx
@@ -1,23 +1,10 @@
 import { degToRad } from 'three/src/math/MathUtils';
 import { MainCamera } from './MainCamera';
-import {
-  CameraControls,
-  PerspectiveCamera as PerspectiveCam,
-} from '@react-three/drei';
+
 import { useContext, useEffect, useRef } from 'react';
 import { MachineContext } from '@/state/xstate/MachineProviders';
 import { Object3DNode, extend, useThree } from '@react-three/fiber';
 import { CameraController } from '@/lib/camera-controller/CameraController';
-import { PI_OVER_THREE, PI_OVER_TWO } from '@/simulation/utils/constants';
-import {
-  ImmersiveSessionOrigin,
-  NonImmersiveCamera,
-} from '@coconut-xr/natuerlich/react';
-import { Group, PerspectiveCamera } from 'three';
-import { FAR_CLIP, NEAR_CLIP } from '@/components/canvas/scene-constants';
-import { Controllers } from '@coconut-xr/natuerlich/defaults';
-import { useSelector } from '@xstate/react';
-import { VRSessionOrigin } from '@/components/canvas/vr/VRSessionOrigin';
 
 const startRadius = 2e11;
 const startPolar = degToRad(75);
@@ -40,6 +27,7 @@ export const CameraManager = () => {
         args={[undefined, startRadius, startAzimuth, startPolar]}
         ref={(controller) => {
           if (!controller) return;
+          if (controllerRef.current === controller) return;
           controllerRef.current = controller;
           cameraActor.send({ type: 'ASSIGN_CONTROLS', controls: controller });
         }}
