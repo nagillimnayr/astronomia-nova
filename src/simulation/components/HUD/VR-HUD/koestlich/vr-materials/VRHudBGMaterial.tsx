24df92e1b568e6ca591d60358c19344c4e90851f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-24 19:14:29 -0700

    chore: delete koestlich backups

--- src/simulation/components/HUD/VR-HUD/koestlich/vr-materials/VRHudBGMaterial.tsx
+++
@@ -1,6 +0,0 @@
-import { makeBorderMaterial } from '@coconut-xr/xmaterials';
-import { MeshBasicMaterial } from 'three';
-
-export const VRHudBGMaterial = makeBorderMaterial(MeshBasicMaterial, {
-  transparent: false,
-});
