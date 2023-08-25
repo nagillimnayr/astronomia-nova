fe67ca8ef4f8217af01432644b441f467bcc0b56
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-25 11:12:26 -0700

    feat: add Interactive

--- src/simulation/components/HUD/VR-HUD/vr-time-panel/vr-time-controls/VRTimescaleIncrementButton.tsx
+++ src/simulation/components/HUD/VR-HUD/vr-time-panel/vr-time-controls/VRTimescaleIncrementButton.tsx
@@ -1,11 +1,12 @@
 import { MachineContext } from '@/state/xstate/MachineProviders';
 import { useSelector } from '@xstate/react';
 import { border, colors, depth, iconSize, text } from '../../vr-hud-constants';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { Center, Circle, Svg, Text, useCursor } from '@react-three/drei';
 import { type Vector3Tuple } from 'three';
 import { ICON_MATERIAL_BASE } from '../../vr-ui-materials';
 import useHover from '@/hooks/useHover';
+import { Interactive } from '@react-three/xr';
 
 interface VRTimescaleIncrementButtonProps {
   position?: Vector3Tuple;
@@ -20,14 +21,27 @@ export const VRTimescaleIncrementButton = ({
   const { isHovered, setHovered, hoverEvents } = useHover();
   useCursor(isHovered, 'pointer');
 
-  const decrementTimescale = useCallback(() => {
-    timeActor.send({ type: 'DECREMENT_TIMESCALE' });
-  }, [timeActor]);
-  const incrementTimescale = useCallback(() => {
-    timeActor.send({ type: 'INCREMENT_TIMESCALE' });
-  }, [timeActor]);
+  const timescaleEvents = useMemo(
+    () => ({
+      decrementTimescale: () => {
+        timeActor.send({ type: 'DECREMENT_TIMESCALE' });
+      },
+      incrementTimescale: () => {
+        timeActor.send({ type: 'INCREMENT_TIMESCALE' });
+      },
+    }),
+    [timeActor]
+  );
+  // const decrementTimescale = useCallback(() => {
+  //   timeActor.send({ type: 'DECREMENT_TIMESCALE' });
+  // }, [timeActor]);
+  // const incrementTimescale = useCallback(() => {
+  //   timeActor.send({ type: 'INCREMENT_TIMESCALE' });
+  // }, [timeActor]);
 
-  const handleClick = reverse ? decrementTimescale : incrementTimescale;
+  const handleClick = reverse
+    ? timescaleEvents.decrementTimescale
+    : timescaleEvents.incrementTimescale;
 
   const iconSrc = reverse
     ? 'icons/MdiChevronLeft.svg'
@@ -36,20 +50,33 @@ export const VRTimescaleIncrementButton = ({
   const size = iconSize.base;
   return (
     <>
-      <Circle args={[1]} position={position} onClick={handleClick}>
-        <meshBasicMaterial color={colors.background} />
-        {/** Offset the z position of the icon from its background. */}
-        <object3D
-          position={[0, 0, depth.xs]}
-          scale={size}
+      <Interactive
+        onSelect={handleClick}
+        onHover={hoverEvents.handlePointerEnter}
+        onBlur={hoverEvents.handlePointerLeave}
+      >
+        <Circle
+          args={[1]}
+          scale={isHovered ? 1.2 : 1}
+          position={position}
           onClick={handleClick}
+          onPointerEnter={hoverEvents.handlePointerEnter}
+          onPointerLeave={hoverEvents.handlePointerLeave}
         >
-          {/** Center the Svg icon. */}
-          <Center>
-            <Svg src={iconSrc} fillMaterial={ICON_MATERIAL_BASE} />
-          </Center>
-        </object3D>
-      </Circle>
+          <meshBasicMaterial color={colors.background} />
+          {/** Offset the z position of the icon from its background. */}
+          <object3D
+            position={[0, 0, depth.xs]}
+            scale={size}
+            onClick={handleClick}
+          >
+            {/** Center the Svg icon. */}
+            <Center>
+              <Svg src={iconSrc} fillMaterial={ICON_MATERIAL_BASE} />
+            </Center>
+          </object3D>
+        </Circle>
+      </Interactive>
     </>
   );
 };
