1def18c8d2e9aecdedb9e45a1fead0f49a20015c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-26 16:06:43 -0700

    feat: add Interactive to floor

--- src/components/canvas/vr/components/Floor.tsx
+++ src/components/canvas/vr/components/Floor.tsx
@@ -1,11 +1,34 @@
-import { Grid, GridProps } from '@react-three/drei';
-import { Object3DProps } from '@react-three/fiber';
+import { PI_OVER_TWO } from '@/simulation/utils/constants';
+import { MachineContext } from '@/state/xstate/MachineProviders';
+import {
+  Grid,
+  Plane,
+  type GridProps,
+  MeshDiscardMaterial,
+} from '@react-three/drei';
+import { type Object3DProps } from '@react-three/fiber';
+import { Interactive, XRInteractionEvent } from '@react-three/xr';
 
 type Props = GridProps & Object3DProps;
 export const Floor = ({ position = [0, 0, 0], ...props }: Props) => {
+  const { vrActor } = MachineContext.useSelector(({ context }) => context);
   return (
     <>
-      <Grid infiniteGrid position={position} />
+      <Interactive
+        onHover={(event: XRInteractionEvent) => {
+          //
+          if (!event.intersection) return;
+
+          // event.intersection.point;
+        }}
+      >
+        <group position={position}>
+          <Grid infiniteGrid />
+          <Plane args={[1000, 1000]} rotation={[-PI_OVER_TWO, 0, 0]}>
+            <MeshDiscardMaterial />
+          </Plane>
+        </group>
+      </Interactive>
     </>
   );
 };
