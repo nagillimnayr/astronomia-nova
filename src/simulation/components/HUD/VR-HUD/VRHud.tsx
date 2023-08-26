d5f03bcd883fdc10db1a6d86f75a2717a87f0b9e
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-26 12:57:53 -0700

    feat: move VRDetailsPanel further to right

--- src/simulation/components/HUD/VR-HUD/VRHud.tsx
+++ src/simulation/components/HUD/VR-HUD/VRHud.tsx
@@ -45,7 +45,7 @@ export const VRHud = ({ position = [0, 0, 0], defaultOpen }: VRHudProps) => {
       <group name="VR-Hud" ref={groupRef} position={position}>
         {inVR && (
           <>
-            <VRDetailsPanel position={[1, 0, 0]} />
+            <VRDetailsPanel position={[1.5, 0, 0]} />
             <VRTimePanel position={[0, -1.5, 0]} scale={0.2} />
             {/* <VROutliner position={[-1, 0, 0]} /> */}
             {/* <VRSettingsButton position={[1, 1.25, 0]} /> */}
