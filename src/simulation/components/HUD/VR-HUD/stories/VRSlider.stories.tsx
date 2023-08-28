39764b1175dabd977f7b7e2ddbdf177056c61a0d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-28 10:31:06 -0700

    feat: display slider value

--- src/simulation/components/HUD/VR-HUD/stories/VRSlider.stories.tsx
+++ src/simulation/components/HUD/VR-HUD/stories/VRSlider.stories.tsx
@@ -1,14 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { VRCanvasDecorator } from '@/stories/decorators/VRCanvasDecorator';
-import { CameraControls, PerspectiveCamera } from '@react-three/drei';
-import { VRSlider } from '../vr-ui-components/VRSlider';
+import { CameraControls, PerspectiveCamera, Text } from '@react-three/drei';
+import { VRSlider, VRSliderProps } from '../vr-ui-components/VRSlider';
 import { colors } from '../vr-hud-constants';
-import { useMemo, useRef, useState } from 'react';
-import { Object3D, Spherical } from 'three';
+import { useCallback, useMemo, useRef, useState } from 'react';
+import { Object3D, Spherical, Vector3Tuple } from 'three';
 import { useEventListener } from '@react-hooks-library/core';
 import { useFrame, useThree } from '@react-three/fiber';
 import { clamp, degToRad } from 'three/src/math/MathUtils';
 import { PI, PI_OVER_TWO } from '@/simulation/utils/constants';
+import { KeyboardCamControls } from '@/simulation/components/camera-controller/KeyboardCamControls';
 
 const meta: Meta<typeof VRSlider> = {
   title: 'VR-UI/components/VRSlider',
@@ -30,10 +31,13 @@ type Story = StoryObj<typeof VRSlider>;
 
 const Default: Story = {
   args: {
-    position: [0, 0, 0],
+    position: [0, -0.15, 0],
     height: 0.1,
     width: 2,
     value: 0,
+    min: -100,
+    max: 100,
+    step: 2,
     thumbRadius: 0.1,
     trackColor: 'black',
     rangeColor: 'white',
@@ -43,81 +47,27 @@ const Default: Story = {
   render: (args) => {
     return (
       <>
-        <KeyboardCamControls />
-        <VRSlider {...args} min={-100} max={100} step={0.1} />
+        <VRSliderStory {...args} />
       </>
     );
   },
 };
 
-export { Default };
-
-const rotateSpeed = 1;
-const MIN_POLAR = 0;
-const MAX_POLAR = PI;
-
-const KeyboardCamControls = () => {
-  const pivotRef = useRef<Object3D>(null!);
-  const sph = useMemo(() => new Spherical(1, PI_OVER_TWO), []);
-  const [spherical, setSpherical] = useState<Spherical>(sph);
-  const getThree = useThree(({ get }) => get);
-  useEventListener('keypress', (event) => {
-    // console.log(event.key);
-    switch (event.key) {
-      case '8': {
-        spherical.phi = clamp(
-          spherical.phi + degToRad(rotateSpeed),
-          MIN_POLAR,
-          MAX_POLAR
-        );
-        break;
-      }
-      case '2': {
-        spherical.phi = clamp(
-          spherical.phi - degToRad(rotateSpeed),
-          MIN_POLAR,
-          MAX_POLAR
-        );
-        break;
-      }
-      case '4': {
-        spherical.theta -= degToRad(rotateSpeed);
-        break;
-      }
-      case '6': {
-        spherical.theta += degToRad(rotateSpeed);
-        break;
-      }
-    }
-  });
-
-  useFrame((state, delta) => {
-    const pivot = pivotRef.current;
-    const { camera, invalidate } = state;
-    pivot.rotation.set(0, 0, 0);
-    pivot.add(camera);
-    camera.position.set(0, 0, spherical.radius);
-    spherical.makeSafe();
-    pivot.rotateY(spherical.theta);
-
-    const polar = PI_OVER_TWO - spherical.phi;
-    pivot.rotateX(polar);
-    invalidate();
-  });
-
+const VRSliderStory = ({ ...args }: VRSliderProps) => {
+  const [val, setVal] = useState<number>(args.value);
+  const handleValueChange = useCallback((value: number) => {
+    console.log('val:', value);
+    setVal(value);
+  }, []);
   return (
     <>
-      <object3D
-        ref={(obj) => {
-          if (!obj) return;
-          pivotRef.current = obj;
-          const { camera } = getThree();
-          obj.add(camera);
-          camera.position.set(0, 0, spherical.radius);
-        }}
-      >
-        <PerspectiveCamera makeDefault position={[0, 0, 1]} />
-      </object3D>
+      <KeyboardCamControls />
+      <Text position={[0, 0.15, 0]} fontSize={0.35}>
+        {val.toLocaleString()}
+      </Text>
+      <VRSlider {...args} onValueChange={handleValueChange} />
     </>
   );
 };
+
+export { Default };
