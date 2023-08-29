c5bc8e5fe3251be86345065f22e6e1fdd70a9407
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-29 11:54:52 -0700

    refactor: remove RootStoreProvider

--- src/components/layout/site-layout/providers/providers.tsx
+++ src/components/layout/site-layout/providers/providers.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { MDXProvider } from '@mdx-js/react';
 import { ThemeProvider } from './theme-provider';
-import { RootStoreProvider } from './root-store-provider';
 import { type PropsWithChildren } from 'react';
 import {
   QueryClient,
@@ -18,13 +17,11 @@ const queryClient = new QueryClient();
 const Providers = ({ children }: PropsWithChildren) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      <RootStoreProvider>
-        <MachineProviders>
-          <QueryClientProvider client={queryClient}>
-            <MDXProvider components={components}>{children}</MDXProvider>
-          </QueryClientProvider>
-        </MachineProviders>
-      </RootStoreProvider>
+      <MachineProviders>
+        <QueryClientProvider client={queryClient}>
+          <MDXProvider components={components}>{children}</MDXProvider>
+        </QueryClientProvider>
+      </MachineProviders>
     </ThemeProvider>
   );
 };
