24531f3528c128fc3eacdbfd40ee1db66707c110
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-30 11:54:53 -0700

    feat: implement coordinate sliders

--- src/simulation/components/HUD/VR-HUD/vr-details-panel/VRSurfaceDialog.tsx
+++ src/simulation/components/HUD/VR-HUD/vr-details-panel/VRSurfaceDialog.tsx
@@ -14,12 +14,15 @@ import { VRHudButton } from '../vr-ui-components/VRHudButton';
 import { depth } from '../vr-hud-constants';
 import { VRSlider } from '../vr-ui-components/vr-slider/VRSlider';
 import { VRLabel } from '../vr-ui-components/VRLabel';
+import { surfaceMachine } from '@/state/xstate/surface-machine/surface-machine';
+import { type ContextFrom } from 'xstate';
+import { capitalize } from 'lodash';
 
 type VRSurfaceDialogProps = {
   position?: Vector3Tuple;
 };
 export const VRSurfaceDialog = ({ position }: VRSurfaceDialogProps) => {
-  const { uiActor, cameraActor } = MachineContext.useSelector(
+  const { uiActor, cameraActor, surfaceActor } = MachineContext.useSelector(
     ({ context }) => context
   );
   const { surfaceDialogActor } = useSelector(uiActor, ({ context }) => context);
@@ -29,17 +32,38 @@ export const VRSurfaceDialog = ({ position }: VRSurfaceDialogProps) => {
   }, [surfaceDialogActor]);
 
   const confirm = useCallback(() => {
-    cameraActor.send('TO_SURFACE');
     surfaceDialogActor.send({ type: 'CLOSE' });
+    cameraActor.send('TO_SURFACE');
   }, [cameraActor, surfaceDialogActor]);
+
+  const handleLatitudeChange = useCallback((newValue: number) => {
+    surfaceActor.send({ type: 'SET_LATITUDE', value: newValue });
+  }, []);
+  const handleLongitudeChange = useCallback((newValue: number) => {
+    surfaceActor.send({ type: 'SET_LONGITUDE', value: newValue });
+  }, []);
 
   return (
     <>
       <animated.group position={position}>
         <VRPanel width={8} height={4} />
 
-        {/** Sliders. */}
-        <CoordinateSliders position={[0, 0.5, depth.xs]} />
+        {/** Latitude Slider. */}
+        <CoordinateSlider
+          position={[0, 1.25, depth.xs]}
+          target={'latitude'}
+          onValueChange={handleLatitudeChange}
+          min={MIN_LATITUDE}
+          max={MAX_LATITUDE}
+        />
+        {/** Longitude Slider. */}
+        <CoordinateSlider
+          position={[0, 0, depth.xs]}
+          target={'longitude'}
+          onValueChange={handleLongitudeChange}
+          min={MIN_LONGITUDE}
+          max={MAX_LONGITUDE}
+        />
 
         {/** Cancel Button. */}
         <VRHudButton
@@ -58,65 +82,58 @@ export const VRSurfaceDialog = ({ position }: VRSurfaceDialogProps) => {
   );
 };
 
-type CoordinateSlidersProps = {
+type CoordinateSliderProps = {
   position?: Vector3Tuple;
+  target: keyof ContextFrom<typeof surfaceMachine>;
+  onValueChange: (newValue: number) => void;
+  min: number;
+  max: number;
 };
-const CoordinateSliders = ({ position }: CoordinateSlidersProps) => {
+const CoordinateSlider = ({
+  position,
+  target,
+  onValueChange,
+  min,
+  max,
+}: CoordinateSliderProps) => {
   const { surfaceActor } = MachineContext.useSelector(({ context }) => context);
 
-  const { latitude, longitude } = useSelector(
-    surfaceActor,
-    ({ context }) => context
-  );
+  const value = useSelector(surfaceActor, ({ context }) => context[target]);
+  const label = capitalize(target);
 
   const sliderWidth = 4;
   const sliderHeight = 0.1;
   const thumbRadius = 0.1;
   const step = 0.1;
   const fontSize = 0.35;
-  const yOffset = 2 * thumbRadius + sliderHeight + fontSize;
+  const halfWidth = sliderWidth / 2;
+  // const yOffset = 2 * thumbRadius + sliderHeight + fontSize;
+  const yOffset = sliderHeight + fontSize / 2;
   return (
     <>
       <group position={position}>
-        {/** Latitude. */}
         <VRLabel
-          position={[
-            0,
-            yOffset + thumbRadius + 1.5 * sliderHeight + fontSize / 2,
-            0,
-          ]}
-          label="Latitude"
+          position={[-halfWidth, yOffset, 0]}
+          label={label}
           fontSize={fontSize}
+          anchorX={'left'}
         />
-        <VRSlider
-          position={[0, yOffset, 0]}
-          min={MIN_LATITUDE}
-          max={MAX_LATITUDE}
-          value={0}
-          width={sliderWidth}
-          height={sliderHeight}
-          thumbRadius={thumbRadius}
-          step={step}
-        />
-        {/** Longitude. */}
         <VRLabel
-          position={[
-            0,
-            -yOffset + thumbRadius + 1.5 * sliderHeight + fontSize / 2,
-            0,
-          ]}
-          label="Longitude"
+          position={[halfWidth, yOffset, 0]}
+          label={value.toFixed(1)}
           fontSize={fontSize}
+          anchorX={'right'}
         />
         <VRSlider
           position={[0, -yOffset, 0]}
-          min={MIN_LONGITUDE}
-          max={MAX_LONGITUDE}
-          value={0}
+          min={min}
+          max={max}
+          value={value}
           width={sliderWidth}
           height={sliderHeight}
           thumbRadius={thumbRadius}
           step={step}
+          onValueChange={onValueChange}
         />
       </group>
     </>
