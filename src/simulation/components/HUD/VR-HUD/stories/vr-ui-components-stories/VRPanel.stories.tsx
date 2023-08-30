0393709e0569ab2debba5a4dc7f9db1f2c11fbbc
Ryan Milligan <ryanmilligan88@gmail.com>
2023-08-30 12:06:18 -0700

    feat: add args

--- src/simulation/components/HUD/VR-HUD/stories/vr-ui-components-stories/VRPanel.stories.tsx
+++ src/simulation/components/HUD/VR-HUD/stories/vr-ui-components-stories/VRPanel.stories.tsx
@@ -7,75 +7,63 @@ import {
   Wireframe as Wires,
 } from '@react-three/drei';
 import { VRPanel } from '../../vr-ui-components/VRPanel';
-import { GOLDEN_RATIO, colors } from '../../vr-hud-constants';
+import { colors } from '../../vr-hud-constants';
 
 const meta: Meta<typeof VRPanel> = {
   title: 'VR-UI/components/VRPanel',
   component: VRPanel,
   decorators: [(storyFn) => <VRCanvasDecorator>{storyFn()}</VRCanvasDecorator>],
+  argTypes: {
+    height: { control: { type: 'range', min: 0.1, max: 2, step: 0.1 } },
+    radius: { control: { type: 'range', min: 0.01, max: 0.5, step: 0.01 } },
+    borderWidth: {
+      control: { type: 'range', min: 0.01, max: 0.5, step: 0.01 },
+    },
+    backgroundColor: { control: { type: 'color' } },
+    borderColor: { control: { type: 'color' } },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof VRPanel>;
 
 const Default: Story = {
-  render: () => {
+  render: (args) => {
     return (
       <>
-        <VRPanelStory />
+        <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+        <CameraControls makeDefault />
+        <VRPanel {...args} />
       </>
     );
+  },
+  args: {
+    height: 1,
+    radius: 0.25,
+    borderWidth: 0.025,
+    backgroundColor: colors.background,
+    borderColor: colors.border,
   },
 };
 
 const Wireframe: Story = {
-  render: () => {
+  render: (args) => {
     return (
       <>
-        <VRPanelWireframeStory />
+        <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+        <VRPanel {...args}>
+          <Wires thickness={0.02} />
+        </VRPanel>
       </>
     );
   },
-};
-
-const VRPanelStory = () => {
-  const height = 1;
-  // const width = height * GOLDEN_RATIO;
-  const width = 2;
-  return (
-    <>
-      <PerspectiveCamera makeDefault position={[0, 0, 2]} />
-      <CameraControls makeDefault />
-      <VRPanel
-        width={width}
-        height={height}
-        radius={0.25}
-        backgroundColor={colors.background}
-        borderColor={colors.border}
-        borderWidth={0.025}
-      ></VRPanel>
-    </>
-  );
-};
-
-const VRPanelWireframeStory = () => {
-  const height = 1;
-  const width = height * GOLDEN_RATIO;
-  return (
-    <>
-      <PerspectiveCamera makeDefault position={[0, 0, 2]} />
-      <VRPanel
-        width={width}
-        height={height}
-        radius={0.2}
-        backgroundColor={colors.background}
-        borderColor={colors.border}
-        borderWidth={0.05}
-      >
-        <Wires thickness={0.02} />
-      </VRPanel>
-    </>
-  );
+  args: {
+    height: 1,
+    radius: 0.25,
+    borderWidth: 0.025,
+    backgroundColor: colors.background,
+    borderColor: colors.border,
+  },
 };
 
 export { Default, Wireframe };
