bb58a4d928acaae1bfb3ea997c9dbd90a0423f63
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-01 15:25:19 -0700

    delete unused code

--- src/simulation/components/Time/time-controls/PauseButton.tsx
+++ src/simulation/components/Time/time-controls/PauseButton.tsx
@@ -1,23 +1,9 @@
-import {
-  PlayIcon,
-  PauseIcon,
-  PlayCircleIcon,
-  PauseCircleIcon,
-} from '@heroicons/react/24/solid';
-import { useThree } from '@react-three/fiber';
-
-import { Play, Pause, PlayCircle, PauseCircle } from 'lucide-react';
 import { useCallback, useContext, useState } from 'react';
-import IconButton from '@/components/IconButton';
-import Icon from '@mdi/react';
-import { mdiPlay, mdiPause } from '@mdi/js';
-import { useTimeStore } from '@/simulation/state/zustand/time-store';
 import { MachineContext } from '@/state/xstate/MachineProviders';
 import { timeMachine } from '../../../../state/xstate/time-machine/time-machine';
 import { useSelector } from '@xstate/react';
 import { cn } from '@/lib/cn';
-import { RootStoreContext } from '@/state/mobx/root/root-store-context';
-import { ClassNameValue } from 'tailwind-merge';
+import { type ClassNameValue } from 'tailwind-merge';
 
 type Props = {
   className?: ClassNameValue;
