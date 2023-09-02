b9ee5a786a785708280f423c18905ed491532afe
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-02 12:13:30 -0700

    scale line relative to circumference of trajectory

--- src/simulation/components/Orbit/Trajectory/PseudoTrajectory.tsx
+++ src/simulation/components/Orbit/Trajectory/PseudoTrajectory.tsx
@@ -1,14 +1,15 @@
 import KeplerBody from '@/simulation/classes/kepler-body';
+import { TWO_PI } from '@/simulation/utils/constants';
 import { MachineContext } from '@/state/xstate/MachineProviders';
 import { Line } from '@react-three/drei';
 import { createPortal, useFrame } from '@react-three/fiber';
 import { useSelector } from '@xstate/react';
-import { useMemo, useRef } from 'react';
+import { distance } from 'mathjs';
+import { useEffect, useMemo, useRef } from 'react';
 import { Vector3 } from 'three';
 import { Line2 } from 'three-stdlib';
 
 const _vel = new Vector3();
-const LENGTH = 1e5;
 const NUM_OF_SEGMENTS = 1024;
 
 // This line helps to cover up a lot of the flickering of the trajectory when very close to a planet.
@@ -17,6 +18,9 @@ export const PseudoTrajectory = () => {
   const focusTarget = useSelector(
     cameraActor,
     ({ context }) => context.focusTarget
+  );
+  const onSurface = useSelector(cameraActor, (state) =>
+    state.matches('surface')
   );
 
   const points = useMemo(() => {
@@ -29,26 +33,34 @@ export const PseudoTrajectory = () => {
   const scale = useMemo(() => {
     return new Vector3(1, 1, 1);
   }, []);
-  scale.set(1, 1, LENGTH);
 
   const lineRef = useRef<Line2>(null!);
 
   useFrame(() => {
     const body = cameraActor.getSnapshot()!.context.focusTarget;
     if (!(body instanceof KeplerBody)) return;
+    if (onSurface) return;
+
     const line = lineRef.current;
     if (!line) return;
+
+    // Scale the line relative to the circumference of the trajectory.
+    const distanceToOrigin = body.position.length();
+    const circumference = TWO_PI * distanceToOrigin;
+    const length = circumference / 4096;
 
     _vel.copy(body.velocity);
     body.localToWorld(_vel);
     line.lookAt(_vel);
-    line.scale.set(1, 1, body.meanRadius * 6);
+    // line.scale.set(1, 1, body.meanRadius * 20);
+    line.scale.set(1, 1, length);
   });
 
   if (!focusTarget) return;
   return createPortal(
     <>
       <Line
+        visible={onSurface}
         ref={lineRef}
         points={points}
         lineWidth={2.1}
