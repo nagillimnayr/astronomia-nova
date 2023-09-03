7378e4e31a65d38b73153c71c434eaf15b1ed036
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-03 16:35:20 -0700

    load ring texture

--- src/simulation/components/Body/planet-ring/PlanetRing.tsx
+++ src/simulation/components/Body/planet-ring/PlanetRing.tsx
@@ -3,22 +3,24 @@ import { DIST_MULT, PI_OVER_TWO } from '@/simulation/utils/constants';
 import { Ring, useCursor, useTexture } from '@react-three/drei';
 import { Object3DNode } from '@react-three/fiber';
 import { PropsWithoutRef, useState } from 'react';
-import { DoubleSide, Object3D, Vector3Tuple } from 'three';
+import { DoubleSide, Object3D, Texture, Vector3Tuple } from 'three';
 
 type PlanetRingProps = PropsWithoutRef<
   Object3DNode<Object3D, typeof Object3D>
 > & {
   innerRadius: number;
   outerRadius: number;
+  texture?: Texture;
   rotation?: Vector3Tuple;
 };
 export const PlanetRing = ({
   innerRadius,
   outerRadius,
+  texture,
   rotation = [0, 0, 0],
   ...props
 }: PlanetRingProps) => {
-  // const ringTexture = useTexture('assets/textures/2k_saturn_ring_alpha.png');
+  const ringTexture = useTexture('assets/textures/2k_saturn_ring_alpha.png');
 
   return (
     <>
@@ -28,9 +30,10 @@ export const PlanetRing = ({
           rotation={[PI_OVER_TWO, 0, 0]}
         >
           <meshBasicMaterial
-            // map={ringTexture}
+            map={ringTexture}
             // alphaMap={ringTexture}
             side={DoubleSide}
+            // map={texture}
           />
         </Ring>
       </object3D>
