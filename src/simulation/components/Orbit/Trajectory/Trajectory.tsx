efc67999a3081e007ebc87e58b7a7f5785c7985e
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-02 17:06:56 -0700

    replace meshline with line

--- src/simulation/components/Orbit/Trajectory/Trajectory.tsx
+++ src/simulation/components/Orbit/Trajectory/Trajectory.tsx
@@ -39,6 +39,7 @@ import { flatten } from 'lodash';
 import { useSpring, animated } from '@react-spring/three';
 import { Line2 } from 'three-stdlib';
 import { anim } from '../../animated-components';
+import { useXR } from '@react-three/xr';
 
 const DIST_TO_CAM_THRESHOLD = 1e8 * METER;
 
@@ -94,8 +95,8 @@ export const Trajectory = ({
     cameraActor,
     ({ context }) => context.focusTarget
   );
-  const getThree = useThree(({ get }) => get);
-  const size = useThree(({ size }) => size);
+  // const getThree = useThree(({ get }) => get);
+  // const size = useThree(({ size }) => size);
 
   const arrowRef = useRef<ArrowHelper>(null!);
   const objRef = useRef<Object3D>(null!);
@@ -103,55 +104,55 @@ export const Trajectory = ({
 
   const [showPeriapsis, setShowPeriapsis] = useState<boolean>(false);
 
-  const points = useMemo(() => {
-    const points = new EllipseCurve(
-      -linearEccentricity / DIST_MULT,
-      0,
-      semiMajorAxis / DIST_MULT,
-      semiMinorAxis / DIST_MULT
-    )
-      .getSpacedPoints(NUM_OF_POINTS)
-      .map((vec2) => {
-        // const vec3Tuple: Vector3Tuple = [vec2.x, 0, vec2.y];
-        const vec3Tuple: Vector3Tuple = [vec2.x, vec2.y, 0];
-        return vec3Tuple;
-      });
-    return flatten(points);
-  }, [semiMajorAxis, semiMinorAxis, linearEccentricity]);
-
   // const points = useMemo(() => {
   //   const points = new EllipseCurve(
   //     -linearEccentricity / DIST_MULT,
   //     0,
   //     semiMajorAxis / DIST_MULT,
   //     semiMinorAxis / DIST_MULT
-  //   ).getSpacedPoints(NUM_OF_POINTS);
-  //   return points;
+  //   )
+  //     .getSpacedPoints(NUM_OF_POINTS)
+  //     .map((vec2) => {
+  //       // const vec3Tuple: Vector3Tuple = [vec2.x, 0, vec2.y];
+  //       const vec3Tuple: Vector3Tuple = [vec2.x, vec2.y, 0];
+  //       return vec3Tuple;
+  //     });
+  //   return flatten(points);
   // }, [semiMajorAxis, semiMinorAxis, linearEccentricity]);
 
-  const geometry = useMemo(() => {
-    return new MeshLineGeometry();
-  }, []);
+  const points = useMemo(() => {
+    const points = new EllipseCurve(
+      -linearEccentricity / DIST_MULT,
+      0,
+      semiMajorAxis / DIST_MULT,
+      semiMinorAxis / DIST_MULT
+    ).getSpacedPoints(NUM_OF_POINTS);
+    return points;
+  }, [semiMajorAxis, semiMinorAxis, linearEccentricity]);
 
-  useEffect(() => {
-    geometry.setPoints(points);
-  }, [geometry, points]);
+  // const geometry = useMemo(() => {
+  //   return new MeshLineGeometry();
+  // }, []);
 
-  const material = useMemo(() => {
-    const { size } = getThree();
-    const material = new MeshLineMaterial({
-      lineWidth: 10,
-      sizeAttenuation: 0,
-      resolution: new Vector2(size.width, size.height),
-    });
-    return material;
-  }, [getThree]);
+  // useEffect(() => {
+  //   geometry.setPoints(points);
+  // }, [geometry, points]);
 
-  useEffect(() => {
-    /** @ts-ignore */
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    material.uniforms.resolution!.value.set(size.width, size.height);
-  }, [size, material]);
+  // const material = useMemo(() => {
+  //   const { size } = getThree();
+  //   const material = new MeshLineMaterial({
+  //     lineWidth: 10,
+  //     sizeAttenuation: 0,
+  //     resolution: new Vector2(size.width, size.height),
+  //   });
+  //   return material;
+  // }, [getThree]);
+
+  // useEffect(() => {
+  //   /** @ts-ignore */
+  //   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  //   material.uniforms.resolution!.value.set(size.width, size.height);
+  // }, [size, material]);
 
   let isVisible = trajectoryVisibilityOn;
   if (bodyRef.current && focusTarget) {
@@ -183,16 +184,16 @@ export const Trajectory = ({
     <>
       <object3D visible={isVisible} ref={objRef}>
         {/** @ts-ignore */}
-        {/* <Line ref={lineRef} points={points} color={'white'} lineWidth={2} /> */}
-        <mesh material={material} geometry={geometry}>
-          {/* <meshLineGeometry points={points} /> */}
-          {/* <meshLineMaterial
+        <Line ref={lineRef} points={points} color={'white'} lineWidth={2} />
+        {/* <mesh material={material} geometry={geometry}> */}
+        {/* <meshLineGeometry points={points} /> */}
+        {/* <meshLineMaterial
           
             color={'white'}
             sizeAttenuation={0}
             lineWidth={0.01}
           /> */}
-        </mesh>
+        {/* </mesh> */}
         {/* <Segments limit={NUM_OF_POINTS} lineWidth={2}>
           {points.map((point, index, arr) => {
             const i = index + 1 < arr.length ? index + 1 : 0;
