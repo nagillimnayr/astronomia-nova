e064c4ac6fe70538269b471d7d07d986fda5e1ce
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-03 17:21:13 -0700

    set length of ray based on intersection distance

--- src/components/canvas/vr/VRControllerRay.tsx
+++ src/components/canvas/vr/VRControllerRay.tsx
@@ -1,17 +1,28 @@
 import { PI, PI_OVER_TWO } from '@/simulation/utils/constants';
 import { Line } from '@react-three/drei';
-import { createPortal } from '@react-three/fiber';
-import { useController } from '@react-three/xr';
-import { useMemo } from 'react';
-import { Euler, Vector3 } from 'three';
+import { createPortal, useFrame, useThree } from '@react-three/fiber';
+import { useController, useXR } from '@react-three/xr';
+import { useMemo, useRef } from 'react';
+import { Euler, Intersection, Object3D, Vector3 } from 'three';
+import { Line2 } from 'three-stdlib';
+import { VRHoverIndicator } from './VRHoverIndicator';
 
 const RAY_LENGTH = 1e3;
+
+const _objWorldPos = new Vector3();
+const _worldNormal = new Vector3();
+const _lookPos = new Vector3();
 
 type VRControllerRayProps = {
   handedness: XRHandedness;
 };
 export const VRControllerRay = ({ handedness }: VRControllerRayProps) => {
+  const getThree = useThree(({ get }) => get);
+  const getXR = useXR(({ get }) => get);
   const controller = useController(handedness);
+
+  const lineRef = useRef<Line2>(null!);
+  const indicatorRef = useRef<Object3D>(null!);
 
   const points = useMemo(() => {
     return [new Vector3(0, 0, 0), new Vector3(0, 0, 1)];
@@ -25,16 +36,74 @@ export const VRControllerRay = ({ handedness }: VRControllerRayProps) => {
   }, []);
   rotation.set(0, PI, 0);
 
+
+  useFrame(({ gl }, _, frame) => {
+    if (!(frame instanceof XRFrame)) return;
+    const line = lineRef.current;
+    const indicator = indicatorRef.current;
+
+    
+    // Get hover state.
+    const hoverState = getXR().hoverState[handedness];
+
+    const intersections = hoverState.values();
+    const iter = intersections.next();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const intersection: Intersection = iter.value;
+
+    if (!intersection) {
+      line.scale.setZ(RAY_LENGTH);
+      return;
+    }
+    const distance = intersection.distance;
+    // Set ray length and indicator position.
+    line.scale.setZ(distance);
+    indicator.position.set(0, 0, distance);
+
+    // Get intersection object.
+    const obj = intersection.object;
+    // Get point of intersection.
+    const point = intersection.point;
+    
+      const face = intersection.face;
+if (face) {
+        const normal = face.normal;
+        obj.getWorldPosition(_objWorldPos);
+        _worldNormal.copy(normal);
+        // Get the normal in world coordinates.
+        obj.localToWorld(_worldNormal);
+        // Subtract object's world position so that we just get the direction.
+        _worldNormal.sub(_objWorldPos);
+        // Add the direction to the point of intersection to get the position to look at.
+        _lookPos.addVectors(point, _worldNormal);
+        indicator.translateZ(0.01); // Slight offset to prevent z-fighting.
+      } else if(controller) {
+        controller.controller.getWorldPosition(_lookPos);
+      }
+    
+    
+      // Look in direction of the normal.
+      indicator.lookAt(_lookPos);
+    
+  });
+
   if (!controller) return;
   return createPortal(
     <>
+      <group>
+
       <Line
+        ref={lineRef}
         points={points}
         color={'red'}
         scale={scale}
         rotation={rotation}
         lineWidth={2}
-      />
+        />
+        <object3D ref={indicatorRef}>
+        <VRHoverIndicator handedness={handedness} />
+        </object3D>
+        </group>
     </>,
     controller.controller
   );
