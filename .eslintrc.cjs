aaad310940878afd011f470c32e612391ed0620c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-07 13:55:04 -0700

    turn off consistent-type-imports warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,7 +27,7 @@ const config = {
   rules: {
     '@typescript-eslint/no-non-null-assertion': ['off'],
     '@typescript-eslint/consistent-type-imports': [
-      'warn',
+      'off',
       {
         prefer: 'type-imports',
         fixStyle: 'inline-type-imports',
