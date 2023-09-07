a1ecdb2c707e8eea7822abc56b5754b924428f3f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-06 18:15:21 -0700

    delete unused code

--- src/simulation/components/Retrograde/RetrogradeContext.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-
-type RetrogradeContextObj = 'referenceBody' | 'otherBody' | null;
-export const RetrogradeContext = createContext<RetrogradeContextObj>(null);
