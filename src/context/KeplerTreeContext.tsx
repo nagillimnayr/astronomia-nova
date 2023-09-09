bae54f4c8fa25347a385e78c3403dc09bf515ea2
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-09 13:28:22 -0700

    change to named export

--- src/context/KeplerTreeContext.tsx
+++ src/context/KeplerTreeContext.tsx
@@ -1,7 +1,5 @@
 import { createContext, type MutableRefObject } from 'react';
-import type KeplerBody from '../components/canvas/body/kepler-body';
+import type { KeplerBody } from '@/components/canvas/body';
 
-const KeplerTreeContext =
+export const KeplerTreeContext =
   createContext<MutableRefObject<KeplerBody | null> | null>(null);
-
-export default KeplerTreeContext;
