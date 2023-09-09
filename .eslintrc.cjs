69d2eb92eb5b8e8ae187a32e22b7931911905818
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-09 12:07:54 -0700

    change consistent type imports back to warn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,7 +27,7 @@ const config = {
   rules: {
     '@typescript-eslint/no-non-null-assertion': ['off'],
     '@typescript-eslint/consistent-type-imports': [
-      'off',
+      'warn',
       {
         prefer: 'type-imports',
         fixStyle: 'inline-type-imports',
