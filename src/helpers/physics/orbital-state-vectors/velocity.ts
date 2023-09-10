aee055c8fa7eaa8a2d9edc7dcfc37904b525a794
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-10 10:50:48 -0700

    delete unused code

--- src/helpers/physics/orbital-state-vectors/velocity.ts
+++ src/helpers/physics/orbital-state-vectors/velocity.ts
@@ -1,10 +1,6 @@
-// The velocity of the orbiting body can be determined with the 'vis-viva'
-// equation $$ \displaystyle |\vec{v}| = \sqrt{GM \left( \frac{2}{r} -
-// \frac{1}{a} \right)} $$
-
-import { Vector3 } from 'three';
+import { type Vector3 } from 'three';
 import { degToRad } from 'three/src/math/MathUtils';
-import { GRAV_CONST } from '../../../constants/constants';
+import { GRAV_CONST } from '@/constants';
 
 /**
  * @summary
@@ -20,40 +16,14 @@ import { GRAV_CONST } from '../../../constants/constants';
  * @param {number} radius
  * @param {number} centralMass
  * @param {number} semiMajorAxis
- * @returns {*}  {number} orbitalSpeed
+ * @returns {number}  {number} orbitalSpeed
  */
 export function getOrbitalSpeedFromRadius(
   radius: number,
   centralMass: number,
   semiMajorAxis: number
 ): number {
-  const orbitalSpeed = Math.sqrt(
-    GRAV_CONST * centralMass * (2 / radius - 1 / semiMajorAxis)
-  );
-
-  return orbitalSpeed;
-}
-
-export function getVelocityDirectionAtRadius(
-  radius: number,
-  trueAnomaly: number,
-  semiMajorAxis: number,
-  semiMinorAxis: number
-): Vector3 {
-  // get the radius relative to the center of the ellipse
-
-  // parameterize to get the x, y coordinates at radius
-  const radians = degToRad(trueAnomaly);
-  const x = radius * Math.cos(radians);
-  const y = radius * Math.sin(radians);
-
-  // get the direction vector of the tangent line of the ellipse at the point
-  // (x, y)
-  const dirX = -y * semiMajorAxis ** 2;
-  const dirY = x * semiMinorAxis ** 2;
-
-  const velocityDirection = new Vector3(dirX, 0, -dirY).normalize();
-  return velocityDirection;
+  return Math.sqrt(GRAV_CONST * centralMass * (2 / radius - 1 / semiMajorAxis));
 }
 
 /**
