0c472b7a8b8bd623728d1754316835502c148a0f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-10 20:05:50 -0700

    remove export from props

--- src/components/dom/layout/header/SiteHeader.tsx
+++ src/components/dom/layout/header/SiteHeader.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { useRef } from 'react';
 import { type ClassNameValue } from 'tailwind-merge';
 
-export type SiteHeaderProps = {
+type SiteHeaderProps = {
   className?: ClassNameValue;
 };
 export const SiteHeader = ({ className }: SiteHeaderProps) => {
