b41d914c22b47595c38dc9e8066837faae76cdc3
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-11 14:01:52 -0700

    delete unused code

--- src/helpers/horizons/elementCodes.ts
+++ src/helpers/horizons/elementCodes.ts
@@ -11,18 +11,3 @@ export type ElementCode =
   | 'A'
   | 'AD'
   | 'PR';
-
-export const ALL_ELEMENT_CODES: ReadonlyArray<ElementCode> = [
-  'EC',
-  'QR',
-  'IN',
-  'OM',
-  'W',
-  'Tp',
-  'N',
-  'MA',
-  'TA',
-  'A',
-  'AD',
-  'PR',
-];
