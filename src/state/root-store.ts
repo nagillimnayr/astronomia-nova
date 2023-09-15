f55b15acea53ec39e631b03b464a64039025351b
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-14 21:20:21 -0700

    add orbitMap to root store

--- src/state/root-store.ts
+++ src/state/root-store.ts
@@ -1,19 +1,23 @@
+import { type KeplerOrbit } from '@/components/canvas/orbit';
 import { create } from 'zustand';
 import { subscribeWithSelector } from 'zustand/middleware';
 
 type State = {
   trajectoryThickness: number;
   trajectoryColor: boolean;
+  orbitMap: Map<string, KeplerOrbit>;
 };
 
 type Actions = {
   setTrajectoryThickness: (thickness: number) => void;
   setTrajectoryColor: (color: boolean) => void;
+  addOrbit: (orbit: KeplerOrbit) => void;
 };
 
 const initialState: State = {
   trajectoryThickness: 2,
   trajectoryColor: false,
+  orbitMap: new Map<string, KeplerOrbit>(),
 };
 
 type RootStore = State & Actions;
@@ -30,6 +34,15 @@ export const useRootStore = create<RootStore>()(
     },
     setTrajectoryColor: (color) => {
       set({ trajectoryColor: color });
+    },
+
+    addOrbit: (orbit: KeplerOrbit) => {
+      const orbitMap = get().orbitMap;
+      if (orbitMap.has(orbit.name)) return;
+      // Create new map.
+      const newOrbitMap = new Map<string, KeplerOrbit>(orbitMap);
+      newOrbitMap.set(orbit.name, orbit);
+      set({ orbitMap: newOrbitMap });
     },
   }))
 );
