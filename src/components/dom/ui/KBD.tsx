30ccd3ec98111e4ecfe95548e926917d9e7e28c7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-16 18:29:42 -0700

    add KBD component to wrap kbd tag

---
+++ src/components/dom/ui/KBD.tsx
@@ -0,0 +1,11 @@
+import { type PropsWithChildren } from 'react';
+
+export const KBD = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <kbd className="mx-1 inline-flex h-6 min-w-[1.5rem] items-center justify-center rounded-sm border-b-4 bg-neutral-800 px-1 py-0 outline outline-2 outline-border">
+        {children}
+      </kbd>
+    </>
+  );
+};
