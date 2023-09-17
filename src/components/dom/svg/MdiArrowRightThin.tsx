29ed4bc7fc7f1ba577d24632f399967918d0ec9e
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-17 11:09:23 -0700

    add icon components for arrows

---
+++ src/components/dom/svg/MdiArrowRightThin.tsx
@@ -0,0 +1,16 @@
+import React, { type SVGProps } from 'react';
+
+export function MdiArrowRightThin(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path fill="#888888" d="M14 16.94v-4H5.08l-.03-2.01H14V6.94l5 5Z"></path>
+    </svg>
+  );
+}
+export default MdiArrowRightThin;
