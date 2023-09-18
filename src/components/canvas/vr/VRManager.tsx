d3445fd2f45db01db8387b36c6e6ef730651da2d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-18 14:16:49 -0700

    get frame from xr

--- src/components/canvas/vr/VRManager.tsx
+++ src/components/canvas/vr/VRManager.tsx
@@ -3,12 +3,13 @@ import { useFrame, useThree } from '@react-three/fiber';
 import { useEffect } from 'react';
 import { useVRControls } from './vr-controls/useVRControls';
 import { VRPlayer } from './VRPlayer';
+import { useXR } from '@react-three/xr';
 
 export const VRManager = () => {
   const { cameraActor, vrActor } = MachineContext.useSelector(
     ({ context }) => context
   );
-  // const getXR = useXR(({ get }) => get);
+  const getXR = useXR(({ get }) => get);
   const getThree = useThree(({ get }) => get);
 
   useEffect(() => {
@@ -16,8 +17,9 @@ export const VRManager = () => {
     vrActor.send({ type: 'ASSIGN_GET_THREE', getThree: getThree });
   }, [cameraActor, getThree, vrActor]);
 
-  useFrame((state, delta, frame) => {
-    if (!(frame instanceof XRFrame)) return;
+  useFrame(({ gl }) => {
+    if (!getXR().isPresenting) return;
+    const frame = gl.xr.getFrame();
 
     // Pass frame to vrActor.
     vrActor.send({ type: 'UPDATE', frame });
