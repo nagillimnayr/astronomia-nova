dc010d05a2210c625a5686fb87a409e57f124840
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-25 11:08:07 -0700

    disable edit link

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { type DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>Astronomia Nova</span>,
+  logo: <strong>Astronomia Nova</strong>,
   project: {
     link: 'https://github.com/nagillimnayr/astronomia-nova',
   },
   docsRepositoryBase: 'https://github.com/nagillimnayr/astronomia-nova',
   footer: {
     text: 'Astronomia Nova Documentation',
+  },
+  editLink: {
+    text: '',
   },
 };
 
