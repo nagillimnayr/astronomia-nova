a517cb0d28e892e8a93ed15094cf9b6c5cbf0293
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-20 22:41:00 -0700

    add console log statements

--- src/components/canvas/scene/useKeyboard.tsx
+++ src/components/canvas/scene/useKeyboard.tsx
@@ -1,6 +1,12 @@
+import { PI, TWO_PI } from '@/constants';
+import { normalizeAngle } from '@/helpers/rotation-utils';
+import { getLocalUpInWorldCoords } from '@/helpers/vector-utils';
 import { MachineContext } from '@/state/xstate/MachineProviders';
 import { useEventListener } from '@react-hooks-library/core';
+import { Spherical, Vector3 } from 'three';
 import { radToDeg } from 'three/src/math/MathUtils';
+import { gsap } from 'gsap';
+import { KeplerBody } from '../body';
 
 export function useKeyboard() {
   const rootActor = MachineContext.useActorRef();
@@ -9,12 +15,68 @@ export function useKeyboard() {
   );
   useEventListener('keydown', (event) => {
     // console.log(event.code);
+
     switch (event.code) {
+      case 'KeyU': {
+        const { controls, observer, focusTarget } =
+          cameraActor.getSnapshot()!.context;
+        if (!controls || !focusTarget || !(focusTarget instanceof KeplerBody))
+          return;
+
+        const _focusUp = new Vector3();
+        const _cameraUp = new Vector3();
+
+        _focusUp.set(...getLocalUpInWorldCoords(focusTarget.meshRef.current!));
+        _cameraUp.set(...getLocalUpInWorldCoords(controls.camera));
+
+        const angle = _cameraUp.angleTo(_focusUp);
+        controls.lock();
+        gsap.to(controls.camera.rotation, {
+          z: angle,
+          duration: 3,
+          onComplete: () => {
+            controls.unlock();
+          },
+        });
+
+        break;
+      }
+      case 'KeyP': {
+        const { controls, observer } = cameraActor.getSnapshot()!.context;
+        const spherical = new Spherical();
+        const observerPos = new Vector3();
+        // const cameraPos = new Vector3();
+        if (!controls || !observer) return;
+        observer.getWorldPosition(observerPos);
+        controls.worldToLocal(observerPos);
+        spherical.setFromVector3(observerPos);
+        spherical.makeSafe();
+        controls.normalizeAzimuthalAngle();
+        const phi = spherical.phi;
+        const theta = normalizeAngle(spherical.theta);
+        const diffTheta = theta - controls.azimuthalAngle;
+
+        void controls.animateTo({
+          phi,
+          theta: Math.abs(diffTheta) < PI ? theta : theta - TWO_PI,
+        });
+
+        break;
+      }
       case 'Space': {
         const { controls } = cameraActor.getSnapshot()!.context;
         if (!controls) return;
+        const x = radToDeg(controls.rotation.x);
+        const y = radToDeg(controls.rotation.y);
+        const z = radToDeg(controls.rotation.z);
         const azimuth = radToDeg(controls.azimuthalAngle);
         console.log(`azimuthal angle: ${azimuth}`);
+        console.log(`controls angles: `, { x, y, z });
+        // controls.rotation.y = 0;
+        // controls.rotation.x = 0;
+        // controls.rotation.z = 0;
+        controls.setAzimuthalAngle(0);
+        controls.setAzimuthalAngleTarget(0);
         break;
       }
       /** Sidereal Day advancement. */
