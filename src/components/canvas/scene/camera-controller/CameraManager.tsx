fd7103ca32f2161c7795b3b5aaebd9d2a7bcedd8
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-27 14:25:53 -0700

    replace ref callback with useLayoutEffect

--- src/components/canvas/scene/camera-controller/CameraManager.tsx
+++ src/components/canvas/scene/camera-controller/CameraManager.tsx
@@ -2,7 +2,7 @@ import { CameraController } from '@/components/canvas/scene/camera-controller/Ca
 import { FAR_CLIP, NEAR_CLIP } from '@/constants/scene-constants';
 import { MachineContext } from '@/state/xstate/MachineProviders';
 import { extend, type Object3DNode, useThree } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 import { PerspectiveCamera } from 'three';
 import { degToRad } from 'three/src/math/MathUtils';
 
@@ -20,34 +20,34 @@ declare module '@react-three/fiber' {
 export const CameraManager = () => {
   const { cameraActor } = MachineContext.useSelector(({ context }) => context);
   const controllerRef = useRef<CameraController>(null!);
-  const camRef = useRef<PerspectiveCamera>(null!);
   const getThree = useThree(({ get }) => get);
+
+  useLayoutEffect(() => {
+    if (!controllerRef.current) return;
+    const { camera } = getThree();
+
+    cameraActor.send({
+      type: 'ASSIGN_CONTROLS',
+      controls: controllerRef.current,
+    });
+
+    if (camera instanceof PerspectiveCamera) {
+      // Initialize camera.
+      camera.name = 'main-camera';
+      camera.near = NEAR_CLIP;
+      camera.far = FAR_CLIP;
+      cameraActor.send({
+        type: 'ASSIGN_CAMERA',
+        camera,
+      });
+    }
+  }, [cameraActor, getThree]);
 
   return (
     <>
       <cameraController
         args={[undefined, startRadius, startAzimuth, startPolar]}
-        ref={(controller) => {
-          if (!controller) return;
-          if (controllerRef.current === controller) return;
-          controllerRef.current = controller;
-          const { camera } = getThree();
-
-          cameraActor.send({ type: 'ASSIGN_CONTROLS', controls: controller });
-
-          if (camRef.current === camera) return;
-          if (camera instanceof PerspectiveCamera) {
-            camRef.current = camera;
-            // Initialize camera.
-            camera.name = 'main-camera';
-            camera.near = NEAR_CLIP;
-            camera.far = FAR_CLIP;
-            cameraActor.send({
-              type: 'ASSIGN_CAMERA',
-              camera,
-            });
-          }
-        }}
+        ref={controllerRef}
       />
     </>
   );
