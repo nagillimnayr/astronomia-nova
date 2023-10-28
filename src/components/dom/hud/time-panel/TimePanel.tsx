697ecf251e46fbaee076b4b4cc6ecdafb4489f9c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-28 14:27:42 -0700

    move controls below time display

--- src/components/dom/hud/time-panel/TimePanel.tsx
+++ src/components/dom/hud/time-panel/TimePanel.tsx
@@ -7,10 +7,10 @@ import { TimescaleControls } from './time-controls/TimescaleControls';
 const TimePanel = () => {
   return (
     <div className="w-min-full h-min-fit pointer-events-none flex h-fit w-72 select-none flex-col items-center justify-start self-center rounded-lg border-2 border-border bg-muted p-2">
-      <TimescaleDisplay />
-      <TimescaleControls />
       <DateDisplay />
       <TimeControls />
+      <TimescaleDisplay />
+      <TimescaleControls />
     </div>
   );
 };
