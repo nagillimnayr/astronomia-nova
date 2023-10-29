5d1ae84e806e047e1e05159c9733f05c6695c489
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-29 15:06:21 -0700

    zoom in on button press

--- src/components/canvas/vr/vr-hud/vr-details-panel/VRSurfaceButton.tsx
+++ src/components/canvas/vr/vr-hud/vr-details-panel/VRSurfaceButton.tsx
@@ -78,6 +78,7 @@ export const VRSurfaceButton = ({ position, width }: VRSurfaceButtonProps) => {
       cameraActor.send({
         type: 'SET_TARGET',
         focusTarget: selected,
+        zoomIn: true,
       });
 
       // Open surface dialog.
