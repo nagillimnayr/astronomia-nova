af5ff04f95802b5e0d536eba31b5ace8f17716d5
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-29 13:25:30 -0700

    add Analytics component

--- src/components/dom/layout/SiteLayout.tsx
+++ src/components/dom/layout/SiteLayout.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { cn } from '@/helpers/cn';
 import { orbitron, roboto } from '@/helpers/fonts';
+import { Analytics } from '@vercel/analytics/react';
 
 const fontVariables = [orbitron.variable, roboto.variable];
 
@@ -8,5 +9,14 @@ type SiteLayoutProps = {
   children: React.ReactNode;
 };
 export const SiteLayout = ({ children }: SiteLayoutProps) => {
-  return <div className={cn(...fontVariables)}>{children}</div>;
+  return (
+    <>
+      <div
+        className={cn(...fontVariables, 'h-full min-h-fit w-full min-w-fit')}
+      >
+        {children}
+      </div>
+      <Analytics />
+    </>
+  );
 };
