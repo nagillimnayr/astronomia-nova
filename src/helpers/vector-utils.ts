a56edffb076abb479a8fdf3041c8f052d47dc661
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-29 13:09:35 -0700

    use world quaternion to get up vector in world coords

--- src/helpers/vector-utils.ts
+++ src/helpers/vector-utils.ts
@@ -1,6 +1,8 @@
-import { type Object3D, Vector3, type Vector3Tuple } from 'three';
+import { type Object3D, Vector3, type Vector3Tuple, Quaternion } from 'three';
 
 const _up = new Vector3();
+const _v1 = new Vector3();
+const _worldQuaternion = new Quaternion();
 const _objWorldPos = new Vector3();
 
 /**
@@ -19,17 +21,21 @@ export function getLocalUpInWorldCoords(obj: Object3D): Vector3Tuple;
 export function getLocalUpInWorldCoords(obj: Object3D, vec: Vector3): Vector3;
 export function getLocalUpInWorldCoords(obj: Object3D, vec?: Vector3) {
   // Get the object's world coords.
-  obj.getWorldPosition(_objWorldPos);
+  // obj.getWorldPosition(_objWorldPos);
+  obj.getWorldQuaternion(_worldQuaternion);
+
   _up.set(0, 1, 0); // Reset to y unit vector.
 
-  // Get local y unit vector in world coords. (This will set _up as the
-  // object's up vector relative to the world origin. Equivalent to adding the
-  // local up vector to the object's world coords.)
-  obj.localToWorld(_up);
-  // To get the direction of the vector relative to the object, we can subtract
-  // the object's world coords.
-  _up.sub(_objWorldPos);
-  // We now have the object's up vector in world coords.
+  // /** Get local y unit vector in world coords. (This will set _up as the
+  //  * object's up vector relative to the world origin. Equivalent to adding the
+  //  * local up vector to the object's world coords.) */
+  // obj.localToWorld(_up);
+  // /**  To get the direction of the vector relative to the object, we can subtract
+  //  * the object's world coords.*/
+  // _up.sub(_objWorldPos);
+  // /* We now have the object's up direction in world coords. */
+
+  _up.applyQuaternion(_worldQuaternion);
 
   if (!vec) {
     return _up.toArray();
