878f0fdace90f92232096220c1f0df5d3fcd965c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-30 10:04:20 -0700

    switch out VRButton

--- src/components/canvas/vr/EnterVRButton.tsx
+++ src/components/canvas/vr/EnterVRButton.tsx
@@ -1,12 +1,13 @@
 import { MachineContext } from '@/state/xstate/MachineProviders';
-import { toggleSession } from '@react-three/xr';
+import { toggleSession, VRButton } from '@react-three/xr';
 import { useSelector } from '@xstate/react';
 import { useCallback, useRef } from 'react';
 
 export const REF_SPACE_TYPE: Readonly<XRReferenceSpaceType> = 'local-floor';
 
 export const sessionOptions: XRSessionInit = {
-  requiredFeatures: [REF_SPACE_TYPE, 'hand-tracking'],
+  requiredFeatures: [REF_SPACE_TYPE],
+  optionalFeatures: ['hand-tracking'],
 };
 
 export const EnterVRButton = () => {
@@ -14,25 +15,40 @@ export const EnterVRButton = () => {
     ({ context }) => context
   );
 
-  const buttonRef = useRef<HTMLButtonElement>(null!);
-  const toggleVR = useCallback(async () => {
-    console.log('toggleVR clicked');
-    const session = await toggleSession('immersive-vr', {
-      sessionInit: sessionOptions,
-    });
-  }, []);
+  // const buttonRef = useRef<HTMLButtonElement>(null!);
+  // const toggleVR = useCallback(async () => {
+  //   console.log('toggleVR clicked');
+  //   const session = await toggleSession('immersive-vr', {
+  //     sessionInit: sessionOptions,
+  //   });
+  // }, []);
 
   const vrActive = useSelector(vrActor, (state) => state.matches('active'));
 
+  const text = vrActive ? 'Exit VR' : 'Enter VR';
   return (
     <>
-      <button
+      {/* <button
         ref={buttonRef}
-        className="select-none rounded-lg border-2 border-white p-2 transition-all hover:bg-subtle hover:bg-opacity-20"
+        className="min-w-fit select-none rounded-lg border-2 border-white p-2 transition-all hover:bg-subtle hover:bg-opacity-20"
         onClick={toggleVR}
       >
-        {vrActive ? 'Exit VR' : 'Enter VR'}
-      </button>
+        {text}
+      </button> */}
+      <VRButton
+        sessionInit={sessionOptions}
+        style={{
+          userSelect: 'none',
+          borderRadius: '12px',
+          width: '124px',
+          maxWidth: '124px',
+        }}
+        className={
+          'select-none border-2 border-white  p-2 transition-all hover:bg-subtle hover:bg-opacity-20'
+        }
+      >
+        {(status) => (status === 'unsupported' ? `WebXR ${status}` : text)}
+      </VRButton>
     </>
   );
 };
