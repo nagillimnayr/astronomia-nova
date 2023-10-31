9d1d6d993386979a3ed967f6761bc697716f8845
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-31 14:38:53 -0700

    set font to sans-serif

--- src/components/dom/ui/calendar.tsx
+++ src/components/dom/ui/calendar.tsx
@@ -16,37 +16,41 @@ function Calendar({
 }: CalendarProps) {
   return (
     <DayPicker
+      style={{
+        fontFamily: 'sans-serif',
+      }}
       showOutsideDays={showOutsideDays}
-      className={cn('p-3', className)}
+      className={cn('p-3 font-sans', className)}
       classNames={{
-        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
-        month: 'space-y-4',
-        caption: 'flex justify-center pt-1 relative items-center',
-        caption_label: 'text-sm font-medium',
-        nav: 'space-x-1 flex items-center',
+        months:
+          'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 font-sans',
+        month: 'space-y-4 font-sans',
+        caption: 'flex justify-center pt-1 relative items-center font-sans',
+        caption_label: 'text-sm font-medium font-sans',
+        nav: 'space-x-1 flex items-center font-sans',
         nav_button: cn(
           buttonVariants({ variant: 'outline' }),
-          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
+          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 font-sans'
         ),
-        nav_button_previous: 'absolute left-1',
-        nav_button_next: 'absolute right-1',
-        table: 'w-full border-collapse space-y-1',
-        head_row: 'flex',
+        nav_button_previous: 'absolute left-1 font-sans',
+        nav_button_next: 'absolute right-1 font-sans',
+        table: 'w-full border-collapse space-y-1 font-sans',
+        head_row: 'flex font-sans',
         head_cell:
-          'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
-        row: 'flex w-full mt-2',
-        cell: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
+          'text-muted-foreground rounded-md w-9 font-semibold  text-[0.8rem] font-sans',
+        row: 'flex w-full mt-2 font-sans',
+        cell: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20  font-sans',
         day: cn(
           buttonVariants({ variant: 'ghost' }),
-          'h-9 w-9 p-0 font-normal aria-selected:opacity-100'
+          'h-9 w-9 p-0  aria-selected:opacity-100 font-sans'
         ),
         day_selected:
-          'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
-        day_today: 'bg-accent text-accent-foreground',
-        day_outside: 'text-muted-foreground opacity-50',
-        day_disabled: 'text-muted-foreground opacity-50',
+          'bg-primary text-primary-foreground hover:bg-subtle hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground  font-sans',
+        day_today: 'bg-subtle text-accent-foreground font-sans',
+        day_outside: 'text-muted-foreground opacity-50 font-sans',
+        day_disabled: 'text-muted-foreground opacity-50 font-sans',
         day_range_middle:
-          'aria-selected:bg-accent aria-selected:text-accent-foreground',
+          'aria-selected:bg-subtle aria-selected:text-accent-foreground font-sans',
         day_hidden: 'invisible',
         ...classNames,
       }}
