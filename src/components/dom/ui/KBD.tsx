3dbfba6a7fd76b257a501c5c0f1408b7bbb69979
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-01 15:52:22 -0700

    adjust colors

--- src/components/dom/ui/KBD.tsx
+++ src/components/dom/ui/KBD.tsx
@@ -3,7 +3,7 @@ import { type PropsWithChildren } from 'react';
 export const KBD = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <kbd className="mx-1 inline-flex h-6 min-w-[1.5rem] items-center justify-center rounded-sm border border-b-4 border-gray-300 bg-neutral-800 px-1 py-0 text-white outline outline-2 outline-border">
+      <kbd className="mx-1 inline-flex h-6 min-w-[1.5rem] items-center justify-center rounded-md border border-b-[3px]  border-neutral-400 bg-neutral-800 px-1 py-0 text-center text-neutral-100 outline outline-1 outline-border dark:border-neutral-300">
         {children}
       </kbd>
     </>
