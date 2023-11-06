4961bb27e5dfc954cb524a7686ffc565fdfd85eb
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-05 18:50:33 -0800

    add useAutoAnimate

---
+++ src/components/canvas/scene/useAutoAnimateCamera.tsx
@@ -0,0 +1,38 @@
+import { DEV_ENV, J2000 } from '@/constants';
+import { MachineContext } from '@/state/xstate/MachineProviders';
+import { useEventListener } from '@react-hooks-library/core';
+
+import { parse } from 'date-fns';
+
+const LATITUDE = 0.0;
+const LONGITUDE = 0.0;
+
+const DATE = parse('2024-12-30', 'yyyy-mm-dd', J2000);
+
+export function useAutoAnimateCamera() {
+  const { cameraActor, timeActor, mapActor, selectionActor, surfaceActor } =
+    MachineContext.useSelector(({ context }) => context);
+
+  useEventListener('keydown', (event) => {
+    if (!DEV_ENV) return;
+    if (event.code !== 'KeyA') return;
+
+    const bodyMap = mapActor.getSnapshot()!.context.bodyMap;
+    const mars = bodyMap.get('Mars');
+    if (!mars) {
+      console.error('ERROR: Mars not found.');
+      return;
+    }
+
+    timeActor.send({ type: 'SET_DATE', date: DATE });
+
+    selectionActor.send({ type: 'SELECT', selection: mars });
+
+    surfaceActor.send({ type: 'SET_LATITUDE', value: LATITUDE });
+    surfaceActor.send({ type: 'SET_LONGITUDE', value: LONGITUDE });
+
+    // cameraActor.send({ type: 'SET_TARGET', focusTarget: mars, zoomIn: true });
+
+    cameraActor.send({ type: 'TO_SURFACE' });
+  });
+}
