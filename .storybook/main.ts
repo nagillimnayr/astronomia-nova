dd9d955755b247be23670123541fda3019197ffd
Ryan Milligan <ryanmilligan88@gmail.com>
2024-07-15 14:32:51 -0700

    change react-docgen-typescript to react-docgen

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -37,7 +37,7 @@ const config: StorybookConfig = {
         },
       },
     },
-    '@chromatic-com/storybook'
+    '@chromatic-com/storybook',
   ],
 
   docs: {},
@@ -62,7 +62,7 @@ const config: StorybookConfig = {
   },
 
   typescript: {
-    reactDocgen: 'react-docgen-typescript'
-  }
+    reactDocgen: 'react-docgen',
+  },
 };
 export default config;
